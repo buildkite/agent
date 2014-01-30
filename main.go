da99a3b6e7b6793be705a06d0475133887333b9f
Keith Pitt <me@keithpitt.com>
2014-01-30 20:34:17 +0800

    Something seems to be running.

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
   "os/exec"
   "path/filepath"
   "log"
+  "io"
+  "syscall"
 )
 
 const CMD_IOBUF_LEN = 512
@@ -24,15 +26,32 @@ func main() {
   c := exec.Command(absolutePath)
   c.Dir = "test"
   c.Env = []string{"BUILDBOX_COMIMT=1"}
+  c.Args[0] = "script.sh"
+  c.SysProcAttr = &syscall.SysProcAttr{Setctty: true, Setsid: true}
 
   fmt.Println("before")
 
-  pty, err := pty.Start(c)
+  output := new(io.Writer)
+
+  ptyMaster, ptySlave, err := pty.Open()
   if err != nil {
     log.Fatal(err)
   }
 
-  fmt.Println("%s", pty)
+  c.Stdout = ptySlave
+  c.Stderr = ptySlave
+
+	go func() {
+		// defer output.Close()
+		log.Printf("startPty: begin of stdout pipe")
+		io.Copy(*output, ptyMaster)
+		log.Printf("startPty: end of stdout pipe")
+	}()
+
+  err = c.Start()
+  if err != nil {
+    log.Fatal(err)
+  }
 
   err = c.Wait()
   if err != nil {
