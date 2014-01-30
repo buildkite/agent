ed3f04b8c8f62cf2e5f2b98a389cd14e8dc094c1
Mark Wolfe <mark@wolfe.id.au>
2014-01-30 19:55:54 +1100

    Just annoted the main file now shows output of pty in stdout.

--- main.go
+++ main.go
@@ -1,43 +1,61 @@
 package main
 
 import (
-  "fmt"
-  // "time"
-  "github.com/kr/pty"
-  // "github.com/buildboxhq/buildbox-agent/buildbox"
-  "os/exec"
-  "path/filepath"
-  "log"
+	"fmt"
+	// "time"
+	"github.com/kr/pty"
+	// "github.com/buildboxhq/buildbox-agent/buildbox"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
 )
 
 const CMD_IOBUF_LEN = 512
 
 func main() {
-  // b := buildbox.GetNextBuild()
-  // if b == nil {
-    // fmt.Println("No build")
-  // }
+	// b := buildbox.GetNextBuild()
+	// if b == nil {
+	// fmt.Println("No build")
+	// }
 
-  // time.Sleep(5000 * time.Millisecond)
+	// time.Sleep(5000 * time.Millisecond)
 
-  absolutePath, _ := filepath.Abs("test/script.sh")
-  c := exec.Command(absolutePath)
-  c.Dir = "test"
-  c.Env = []string{"BUILDBOX_COMIMT=1"}
+	// so run this script
+	absolutePath, _ := filepath.Abs("test/script.sh")
+	c := exec.Command(absolutePath)
+	// with cwd this folder
+	c.Dir = "test"
+	// inject env stuff for build box
+	c.Env = []string{"BUILDBOX_COMIMT=1"}
 
-  fmt.Println("before")
+	// before the process is run
+	fmt.Println("before")
 
-  pty, err := pty.Start(c)
-  if err != nil {
-    log.Fatal(err)
-  }
+	// spawn a pty
+	pty, err := pty.Start(c)
 
-  fmt.Println("%s", pty)
+	// probably not this
+	if err != nil {
+		log.Fatal(err)
+	}
+	// not really much use but yeah it is a PTY..
+	fmt.Printf("%v\n", pty)
 
-  err = c.Wait()
-  if err != nil {
-    log.Fatal(err)
-  }
+	// now we attach the output of the pty to stdout
+	// need a line buffered go routine sending
+	// this to buildbox..
+	io.Copy(os.Stdout, pty)
 
-  fmt.Println("%s", c.ProcessState)
+	// wait for the pty to finish
+	err = c.Wait()
+
+	// probably not this but something telling us the exit result
+	if err != nil {
+		log.Fatalf("left the building %s", err)
+	}
+
+	// never gets run because fatal is fatal..
+	fmt.Println("status of the process %s", c.ProcessState)
 }
