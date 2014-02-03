4cc906516f3f3578dfc4a9572058aa595892535d
Keith Pitt <me@keithpitt.com>
2014-02-03 13:03:35 +0800

    Started working on improving the CLI.

--- main.go
+++ main.go
@@ -1,56 +1,45 @@
 package main
 
 import (
-  "log"
-  "fmt"
-  "flag"
-  "os"
   "github.com/buildboxhq/buildbox-agent-go/buildbox"
 )
 
-func main() {
-  // Set the flags that we'll use
-  accessToken := flag.String("access-token", "", "The access token used to identify the agent.")
-  debug := flag.Bool("debug", false, "Runs the agent in debug mode.")
-  url := flag.String("url", "", "Specify a different API endpoint.")
-  version := flag.Bool("version", false, "Show the version of the agent")
-  exitOnComplete := flag.Bool("exit-on-complete", false, "Runs all available jobs and then exits")
-
-  // Parse the flags
-  flag.Parse()
-
-  // Are they trying to show the version?
-  if *version {
-    fmt.Printf("buildbox-agent version %s\n", buildbox.Version)
-    os.Exit(0)
-  }
-
+/*
+func start(cli *Cli) {
   // Raise an error if no access token has been set.
-  if *accessToken == "" {
-    fmt.Printf("Error: Missing access token. See 'buildbox-agent-%s --help'\n", buildbox.Version)
+  if cli.AccessToken == "" {
+    fmt.Printf("Error: Missing access token. See 'buildbox-agent-%s --help'\n", Version)
     os.Exit(1)
   }
 
   // Tell the user that debug mode has been enabled
-  if *debug {
+  if cli.Debug {
     log.Printf("Debug mode enabled")
   }
 
   // Create a new Client that we'll use to interact with the API
   var client buildbox.Client
-  client.AgentAccessToken = *accessToken
-  client.URL = *url
-  client.Debug = *debug
+  client.AgentAccessToken = cli.AccessToken
+  client.URL = cli.URL
+  client.Debug = cli.Debug
 
   // Create a new instance of the Agent
   var agent buildbox.Agent
   agent.Client = client
 
+  // Get the agent ready for action
   agent.Prepare()
 
-  if *exitOnComplete {
+  if cli.ExitOnComplete {
     agent.Work()
   } else {
     agent.Start()
   }
+}
+*/
+
+func main() {
+  // Run our cli stuffs
+  var cli buildbox.Cli
+  cli.Run()
 }
