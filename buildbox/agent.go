321b6e69468745020b554cf5a545fa0e72eee58f
Keith Pitt <me@keithpitt.com>
2014-02-03 14:23:22 +0800

    Improved the CLI

--- buildbox/agent.go
+++ buildbox/agent.go
@@ -12,33 +12,52 @@ type Agent struct {
   // The client the agent will use to communicate to
   // the API
   Client Client
+
+  // Whether to run the agent in Debug mode
+  Debug bool
+
+  // Stop the agent when all work is complete
+  ExitOnComplete bool
+
+  // The boostrap script to run
+  BootstrapScript string
 }
 
-func (a Agent) Prepare() {
+func (a Agent) Run() {
+  // Tell the user that debug mode has been enabled
+  if a.Debug {
+    log.Printf("Debug mode enabled")
+  }
+
+  // Should the client also run in Debug mode?
+  a.Client.Debug = a.Debug
+
   // TODO
   // Get agent information from API
   a.Name = "hello123"
 
   // A nice welcome message
-  log.Printf("Starting up buildbox-agent-(%s) (%s)...\n", Version, a.Name)
-}
+  log.Printf("Starting up buildbox-agent `%s` (version %s)...\n", a.Name, Version)
 
-func (a Agent) Start() {
-  // How long the agent will wait when no jobs can be found.
-  idleSeconds := 5
-  sleepTime := time.Duration(idleSeconds * 1000) * time.Millisecond
+  if a.ExitOnComplete {
+    a.work()
+  } else {
+    // How long the agent will wait when no jobs can be found.
+    idleSeconds := 5
+    sleepTime := time.Duration(idleSeconds * 1000) * time.Millisecond
 
-  for {
-    // The agent will run all the jobs in the queue, and return
-    // when there's nothing left to do.
-    a.Work()
+    for {
+      // The agent will run all the jobs in the queue, and return
+      // when there's nothing left to do.
+      a.work()
 
-    // Sleep then check again later.
-    time.Sleep(sleepTime)
+      // Sleep then check again later.
+      time.Sleep(sleepTime)
+    }
   }
 }
 
-func (a Agent) Work() {
+func (a Agent) work() {
   for {
     // Try and find some work to do
     job, err := a.Client.JobNext()
--- main.go
+++ main.go
@@ -1,45 +1,98 @@
 package main
 
 import (
+  "os"
+  "github.com/codegangsta/cli"
   "github.com/buildboxhq/buildbox-agent-go/buildbox"
 )
 
 /*
 func start(cli *Cli) {
-  // Raise an error if no access token has been set.
-  if cli.AccessToken == "" {
-    fmt.Printf("Error: Missing access token. See 'buildbox-agent-%s --help'\n", Version)
-    os.Exit(1)
-  }
-
-  // Tell the user that debug mode has been enabled
-  if cli.Debug {
-    log.Printf("Debug mode enabled")
-  }
-
-  // Create a new Client that we'll use to interact with the API
-  var client buildbox.Client
-  client.AgentAccessToken = cli.AccessToken
-  client.URL = cli.URL
-  client.Debug = cli.Debug
-
-  // Create a new instance of the Agent
-  var agent buildbox.Agent
-  agent.Client = client
-
-  // Get the agent ready for action
-  agent.Prepare()
-
-  if cli.ExitOnComplete {
-    agent.Work()
-  } else {
-    agent.Start()
-  }
 }
 */
 
+var AppHelpTemplate = `The agent performs builds and sends the results back to Buildbox.
+
+Usage:
+
+  {{.Name}} command [arguments]
+
+The comamnds are:
+
+  {{range .Commands}}{{.Name}}{{with .ShortName}}, {{.}}{{end}}{{ "\t" }}{{.Usage}}
+  {{end}}
+Use "buildbox-agent help [command]" for more information about a command.
+
+`
+
+var CommandHelpTemplate = `Usage: buildbox-agent {{.Name}} [command options] [arguments...]
+
+{{.Description}}
+
+Options:
+   {{range .Flags}}{{.}}
+   {{end}}
+`
+
+var StartHelpDescription = `When a job is ready to run it will call the "bootstrap-script"
+and pass it all the environment variables required for the job to run.
+This script is responsible for checking out the code, and running the
+actual build script defined in the project.
+
+The agent will run any jobs within a PTY (Pseudo terminal) if available.
+
+Example:
+
+buildbox-agent start --access-token a374fha7834f \
+                     --bootstrap-script ~/.buildbox/bootstrap.sh`
+
 func main() {
-  // Run our cli stuffs
-  var cli buildbox.Cli
-  cli.Run()
+  cli.AppHelpTemplate = AppHelpTemplate
+  cli.CommandHelpTemplate = CommandHelpTemplate
+
+  app := cli.NewApp()
+  app.Name = "buildbox.agent"
+  app.Version = buildbox.Version
+
+  // Define the actions for our CLI
+  app.Commands = []cli.Command{
+    {
+      Name:  "start",
+      Usage: "Starts the Buildbox agent",
+      Description: StartHelpDescription,
+      Flags: []cli.Flag {
+        cli.StringFlag{"access-token", "", "The access token used to identify the agent."},
+        cli.StringFlag{"bootstrap-script", "bootstrap.sh", "Path to the bootstrap script."},
+        cli.StringFlag{"url", "https://agent.buildbox.io/v1", "The Agent API endpoint."},
+        cli.BoolFlag{"exit-on-complete", "Runs all available jobs and then exit."},
+        cli.BoolFlag{"debug", "Enable debug mode."},
+      },
+      Action: func(c *cli.Context) {
+        if c.String("access-token") == "" {
+          print("buildbox-agent: missing access token\nSee 'buildbox-agent help start'\n")
+          os.Exit(1)
+        }
+
+        // Set the agent options
+        var agent buildbox.Agent;
+        agent.Debug = c.Bool("debug")
+        agent.ExitOnComplete = c.Bool("exit-on-complete")
+        agent.BootstrapScript = c.String("bootstrap-script")
+
+        // Client specific options
+        agent.Client.URL = c.String("url")
+
+        // Run the agent
+        agent.Run()
+      },
+    },
+  }
+
+  // Default the default action
+  app.Action = func(c *cli.Context) {
+    cli.ShowAppHelp(c)
+    os.Exit(1)
+  }
+
+  app.Run(os.Args)
 }
