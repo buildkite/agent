0b6f4f0d4082bc16384886db54037e6aed78f2dd
Keith Pitt <me@keithpitt.com>
2014-06-14 21:24:35 +0930

    Hacks to get the buildbox package working.

--- scripts/buildbox.sh
+++ scripts/buildbox.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
 
-export GOPATH="$GOPATH:`pwd`"
-
 echo '--- go version'
 go version
 
 echo '--- install dependencies'
 go get github.com/tools/godep
 godep restore
+
+# setup the current repo as a package
+mkdir -p gopath/src/github.com/buildboxhq/buildbox-agent
+ln -s buildbox gopath/src/github.com/buildboxhq/buildbox-agent/buildbox
+export GOPATH="`pwd`/gopath:$GOPATH"
 
 echo '--- building packages'
 ./scripts/build.sh
