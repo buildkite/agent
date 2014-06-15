f0442ed3fe32c771a2670fd5317a30603c537776
Keith Pitt <me@keithpitt.com>
2014-06-15 17:55:40 +0930

    Fixed build script.

--- scripts/buildbox.sh
+++ scripts/buildbox.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+# setup the current repo as a package - super hax.
+mkdir -p gopath/src/github.com/buildboxhq
+ln -s `pwd` gopath/src/github.com/buildboxhq/buildbox-agent
+export GOPATH="`pwd`/gopath:$GOPATH"
+
 echo '--- go version'
 go version
 
@@ -16,8 +21,4 @@ echo '--- tests'
 make test
 
 echo '--- building packages'
-# setup the current repo as a package - super hax.
-mkdir -p gopath/src/github.com/buildboxhq
-ln -s `pwd` gopath/src/github.com/buildboxhq/buildbox-agent
-export GOPATH="`pwd`/gopath:$GOPATH"
 make build
