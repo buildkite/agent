df32f21ab4667db8b97970e44e4436cdb705d8a1
Keith Pitt <me@keithpitt.com>
2014-08-29 11:37:10 +0800

    Added the data set and get commands to the new structure.

--- command/data_get.go
+++ command/data_get.go
@@ -1,8 +1,50 @@
 package command
 
 import (
+	"fmt"
+	"github.com/buildbox/buildbox-agent/buildbox"
 	"github.com/codegangsta/cli"
+	"os"
 )
 
 func DataGetCommandAction(c *cli.Context) {
+	// Init debugging
+	if c.Bool("debug") {
+		buildbox.LoggerInitDebug()
+	}
+
+	agentAccessToken := c.String("agent-access-token")
+	if agentAccessToken == "" {
+		fmt.Println("buildbox: missing agent access token\nSee 'buildbox data get --help'")
+		os.Exit(1)
+	}
+
+	jobId := c.String("job")
+	if jobId == "" {
+		fmt.Printf("buildbox: missing job\nSee 'buildbox data get --help'\n")
+		os.Exit(1)
+	}
+
+	// Create a client so we can register the agent
+	var client buildbox.Client
+	client.AuthorizationToken = agentAccessToken
+	client.URL = c.String("url")
+
+	// Find the job
+	job, err := client.JobFind(jobId)
+	if err != nil {
+		buildbox.Logger.Fatalf("Could not find job: %s", jobId)
+	}
+
+	// Grab the key
+	key := c.Args()[0]
+
+	// Get the data through the API
+	data, err := client.DataGet(job, key)
+	if err != nil {
+		buildbox.Logger.Fatalf("Failed to get data: %s", err)
+	}
+
+	// Output it
+	fmt.Print(data.Value)
 }
--- command/data_set.go
+++ command/data_set.go
@@ -1,8 +1,48 @@
 package command
 
 import (
+	"fmt"
+	"github.com/buildbox/buildbox-agent/buildbox"
 	"github.com/codegangsta/cli"
+	"os"
 )
 
 func DataSetCommandAction(c *cli.Context) {
+	// Init debugging
+	if c.Bool("debug") {
+		buildbox.LoggerInitDebug()
+	}
+
+	agentAccessToken := c.String("agent-access-token")
+	if agentAccessToken == "" {
+		fmt.Println("buildbox: missing agent access token\nSee 'buildbox data get --help'")
+		os.Exit(1)
+	}
+
+	jobId := c.String("job")
+	if jobId == "" {
+		fmt.Printf("buildbox: missing job\nSee 'buildbox data get --help'\n")
+		os.Exit(1)
+	}
+
+	// Create a client so we can register the agent
+	var client buildbox.Client
+	client.AuthorizationToken = agentAccessToken
+	client.URL = c.String("url")
+
+	// Find the job
+	job, err := client.JobFind(jobId)
+	if err != nil {
+		buildbox.Logger.Fatalf("Could not find job: %s", jobId)
+	}
+
+	// Grab the key and value to set
+	key := c.Args()[0]
+	value := c.Args()[1]
+
+	// Set the data through the API
+	data, err := client.DataSet(job, key, value)
+	if err != nil {
+		buildbox.Logger.Fatalf("Failed to set data: %s", err)
+	}
 }
