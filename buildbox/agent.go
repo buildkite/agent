b5b956c9f3bb5d1583747fd92cf7083e6cc04c8f
Keith Pitt <me@keithpitt.com>
2014-09-13 14:45:53 +0800

    Handle SIGKILL correctly.

--- buildbox/agent.go
+++ buildbox/agent.go
@@ -58,12 +58,51 @@ func (a *Agent) MonitorSignals() {
 	signals := make(chan os.Signal, 1)
 
 	// Monitor a heap of different signals for debugging purposes. Only
-	// some of them are used.
+	// some of them are used. Descriptions are copied from
+	// http://en.wikipedia.org/wiki/Unix_signal so when working with this
+	// code, we know exactly what each signal should do (and  alwsays
+	// forget the specifics)
+
 	signal.Notify(signals, os.Interrupt,
+		// The SIGHUP signal is sent to a process when its controlling
+		// terminal is closed. It was originally designed to notify the
+		// process of a serial line drop (a hangup). In modern systems,
+		// this signal usually means that the controlling pseudo or
+		// virtual terminal has been closed.[3] Many daemons will
+		// reload their configuration files and reopen their logfiles
+		// instead of exiting when receiving this signal.[4] nohup is a
+		// command to make a command ignore the signal.
 		syscall.SIGHUP,
+
+		// The SIGTERM signal is sent to a process to request its
+		// termination. Unlike the SIGKILL signal, it can be caught and
+		// interpreted or ignored by the process.  This allows the
+		// process to perform nice termination releasing resources and
+		// saving state if appropriate. It should be noted that SIGINT
+		// is nearly identical to SIGTERM.
 		syscall.SIGTERM,
-		syscall.SIGQUIT,
+
+		// The SIGKILL signal is sent to a process to cause it to
+		// terminate immediately (kill). In contrast to SIGTERM and
+		// SIGINT, this signal cannot be caught or ignored, and the
+		// receiving process cannot perform any clean-up upon receiving
+		// this signal.
+		syscall.SIGKILL,
+
+		// The SIGINT signal is sent to a process by its controlling
+		// terminal when a user wishes to interrupt the process. This
+		// is typically initiated by pressing Control-C, but on some
+		// systems, the "delete" character or "break" key can be
+		// used.[5]
 		syscall.SIGINT,
+
+		// The SIGQUIT signal is sent to a process by its controlling
+		// terminal when the user requests that the process quit and
+		// perform a core dump.
+		syscall.SIGQUIT,
+
+		// The SIGUSR1 and SIGUSR2 signals are sent to a process to
+		// indicate user-defined conditions.
 		syscall.SIGUSR2)
 
 	go func() {
@@ -72,8 +111,9 @@ func (a *Agent) MonitorSignals() {
 
 		Logger.Debugf("Received signal `%s`", sig.String())
 
-		// Only monitor certain signals
-		if sig != syscall.SIGINT && sig != syscall.SIGUSR2 {
+		// Ignore SIGHUP, some terminals seem to send it when they get resized,
+		// killing the process here would just be silly.
+		if sig == syscall.SIGHUP {
 			Logger.Debugf("Ignoring signal `%s`", sig.String())
 
 			// Start monitoring signals again
@@ -82,10 +122,17 @@ func (a *Agent) MonitorSignals() {
 			return
 		}
 
+		// If we've received a SIGKILL, die immediately.
+		if sig == syscall.SIGKILL {
+			Logger.Debugf("Exiting immediately", sig.String())
+
+			os.Exit(1)
+		}
+
 		// If the agent isn't running a job, exit right away
 		if a.Job == nil {
 			// Disconnect from Buildbox
-			Logger.Info("No jobs running. Disconnecting...")
+			Logger.Info("Disconnecting...")
 			a.Client.AgentDisconnect(a)
 
 			os.Exit(1)
@@ -104,7 +151,7 @@ func (a *Agent) MonitorSignals() {
 			// Die time.
 			os.Exit(1)
 		} else {
-			Logger.Info("Exiting... Waiting for job to finish before stopping. Send signal again to exit immediately.")
+			Logger.Info("Exiting... Waiting for job to finish before stopping. Send signal again to exit immediately")
 
 			a.stopping = true
 		}
@@ -147,20 +194,4 @@ func (a *Agent) Start() {
 			time.Sleep(sleepTime)
 		}
 	}
-}
-
-func (a *Agent) Run(id string) {
-	// Try and find the job
-	job, err := a.Client.JobFindAndAssign(id)
-
-	if err != nil {
-		Logger.Fatal(err)
-	}
-
-	if job.State != "scheduled" {
-		Logger.Fatalf("The agent can only run scheduled jobs. Current state is `%s`", job.State)
-	}
-
-	// Run the paticular job
-	job.Run(a)
 }
