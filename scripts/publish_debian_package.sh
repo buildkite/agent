7c9a428dddb09fff20c6b390b63f1594e3b81527
Keith Pitt <me@keithpitt.com>
2014-11-27 13:16:21 +0800

    Changes to the ENV used to publish things.

--- scripts/publish_debian_package.sh
+++ scripts/publish_debian_package.sh
@@ -13,35 +13,35 @@ CODENAME=${2}
 COMPONENT="main"
 
 # Some validations
-if [ -z "$S3_BUCKET" ]; then
-  echo "Error: Missing ENV variable S3_BUCKET"
+if [ -z "$DEB_S3_BUCKET" ]; then
+  echo "Error: Missing ENV variable DEB_S3_BUCKET"
   exit 1
 fi
 
-if [ -z "$S3_ACCESS_KEY_ID" ]; then
-  echo "Error: Missing ENV variable S3_ACCESS_KEY_ID"
+if [ -z "$DEB_S3_ACCESS_KEY_ID" ]; then
+  echo "Error: Missing ENV variable DEB_S3_ACCESS_KEY_ID"
   exit 1
 fi
 
-if [ -z "$S3_SECRET_ACCESS_KEY_ID" ]; then
-  echo "Error: Missing ENV variable S3_SECRET_ACCESS_KEY_ID"
+if [ -z "$DEB_S3_SECRET_ACCESS_KEY_ID" ]; then
+  echo "Error: Missing ENV variable DEB_S3_SECRET_ACCESS_KEY_ID"
   exit 1
 fi
 
-echo "--- Uploading $PACKAGE to $S3_BUCKET ($CODENAME $COMPONENT)"
+echo "--- Uploading $PACKAGE to $DEB_S3_BUCKET ($CODENAME $COMPONENT)"
 
 deb-s3 upload \
-  --bucket $S3_BUCKET \
+  --bucket $DEB_S3_BUCKET \
   --codename $CODENAME \
   --component $COMPONENT \
-  --access-key-id=$S3_ACCESS_KEY_ID \
-  --secret-access-key=$S3_SECRET_ACCESS_KEY_ID \
+  --access-key-id=$DEB_S3_ACCESS_KEY_ID \
+  --secret-access-key=$DEB_S3_SECRET_ACCESS_KEY_ID \
   $PACKAGE
 
-echo "All done! To install:"
+echo "✅ All done! To install:"
 echo ""
 echo "    # Add the repository to your APT sources"
-echo "    $ echo deb $S3_BUCKET $CODENAME $COMPONENT > /etc/apt/sources.list.d/buildbox-agent.list"
+echo "    $ echo deb $DEB_S3_BUCKET $CODENAME $COMPONENT > /etc/apt/sources.list.d/buildbox-agent.list"
 echo ""
 echo "    # Then import the repository key (TODO)"
 echo "    $ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys xxx"
