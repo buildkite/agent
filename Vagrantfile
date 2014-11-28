4b2856838ee5e73d9b13a78526b86d87c8b272ce
Keith Pitt <me@keithpitt.com>
2014-11-28 15:03:58 +0800

    Rewrote the debian package scripts.

--- Vagrantfile
+++ Vagrantfile
@@ -6,7 +6,9 @@ VAGRANTFILE_API_VERSION = "2"
 
 $INSTALL_FPM = <<SCRIPT
 apt-get update --fix-missing
-apt-get install -y build-essential vim curl
+
+apt-get install -y build-essential vim curl git-core
+
 command curl -sSL https://rvm.io/mpapis.asc | gpg --import -
 curl -L https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
@@ -14,6 +16,24 @@ rvm install ruby
 rvm use ruby --default
 rvm rubygems current
 gem install fpm deb-s3
+
+apt-get -y --force-yes -q install mercurial
+cd /tmp && curl -L -O https://storage.googleapis.com/golang/go1.3.3.linux-amd64.tar.gz
+tar -C /usr/local -xzf /tmp/go1.3.3.linux-amd64.tar.gz
+mkdir -own -R vagrant:vagrant /home/vagrant/.go /home/vagrant/.go/src/github.com/buildbox
+cd /home/vagrant/.go/src/github.com/buildbox
+ln -s /vagrant agent
+chown -R vagrant:vagrant /home/vagrant/.go
+echo 'export GOROOT="/usr/local/go"' >> /home/vagrant/.profile
+echo 'export GOPATH="/home/vagrant/.go"' >> /home/vagrant/.profile
+echo 'export PATH="/home/vagrant/.go/bin:/usr/local/go/bin:$PATH"' >> /home/vagrant/.profile
+cd /usr/local/go/src && GOOS=windows GOARCH=386 ./make.bash --no-clean
+cd /usr/local/go/src && GOOS=windows GOARCH=amd64 ./make.bash --no-clean
+cd /usr/local/go/src && GOOS=linux GOARCH=amd64 ./make.bash --no-clean
+cd /usr/local/go/src && GOOS=linux GOARCH=386 ./make.bash --no-clean
+cd /usr/local/go/src && GOOS=linux GOARCH=arm ./make.bash --no-clean
+cd /usr/local/go/src && GOOS=darwin GOARCH=386 ./make.bash --no-clean
+cd /usr/local/go/src && GOOS=darwin GOARCH=amd64 ./make.bash --no-clean
 SCRIPT
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
---
+++ scripts/publish-debian-package.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+set -o errexit
+
+if [[ ${#} -ne 2 ]]
+then
+  echo "Usage: ${0} [debian-package] [codename]" >&2
+  exit 1
+fi
+
+function info {
+  echo -e "\033[35m$1\033[0m"
+}
+
+PACKAGE=${1}
+CODENAME=${2}
+COMPONENT="main"
+
+# Some validations
+if [ -z "$GPG_SIGNING_KEY" ]; then
+  echo "Error: Missing ENV variable $GPG_SIGNING_KEY"
+  exit 1
+fi
+
+if [ -z "$GPG_PASSPHRASE_PASSWORD" ]; then
+  echo "Error: Missing ENV variable $GPG_PASSPHRASE_PASSWORD"
+  exit 1
+fi
+
+if [ -z "$GPG_PASSPHRASE_PATH" ]; then
+  echo "Error: Missing ENV variable $GPG_PASSPHRASE_PATH"
+  exit 1
+fi
+
+if [ -z "$DEB_S3_BUCKET" ]; then
+  echo "Error: Missing ENV variable DEB_S3_BUCKET"
+  exit 1
+fi
+
+if [ -z "$DEB_S3_ACCESS_KEY_ID" ]; then
+  echo "Error: Missing ENV variable DEB_S3_ACCESS_KEY_ID"
+  exit 1
+fi
+
+if [ -z "$DEB_S3_SECRET_ACCESS_KEY_ID" ]; then
+  echo "Error: Missing ENV variable DEB_S3_SECRET_ACCESS_KEY_ID"
+  exit 1
+fi
+
+info "Uploading $PACKAGE to $DEB_S3_BUCKET ($CODENAME $COMPONENT)"
+
+# Decrpyt the GPG_PASSPHRASE with our GPG_PASSPHRASE_PASSWORD
+GPG_PASSPHRASE=`openssl aes-256-cbc -k "$GPG_PASSPHRASE_PASSWORD" -in "$GPG_PASSPHRASE_PATH" -d`
+
+# Uploads to s3 and signs with the default key on the system
+
+deb-s3 upload \
+  --sign $GPG_SIGNING_KEY \
+  --gpg-options "\-\-passphrase $GPG_PASSPHRASE" \
+  --bucket $DEB_S3_BUCKET \
+  --codename $CODENAME \
+  --component $COMPONENT \
+  --access-key-id=$DEB_S3_ACCESS_KEY_ID \
+  --secret-access-key=$DEB_S3_SECRET_ACCESS_KEY_ID \
+  $PACKAGE
+
+echo "✅ All done! To install this package:"
+echo ""
+echo "    # Login as root"
+echo "    $ sudo su"
+echo ""
+echo "    # Add the repository to your APT sources"
+echo "    $ echo deb http://$DEB_S3_BUCKET $CODENAME $COMPONENT > /etc/apt/sources.list.d/buildbox-agent.list"
+echo ""
+echo "    # Then import the repository key"
+echo "    $ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 5a7dbc2d7109e367"
+echo ""
+echo "    # Install the agent"
+echo "    $ apt-get update"
+echo "    $ apt-get install -y buildbox-agent"
+echo ""
