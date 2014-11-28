f730d5e1269ed9353efd6cbcb38fb792e26d9155
Keith Pitt <me@keithpitt.com>
2014-11-28 16:01:18 +0800

    More fixes.

--- scripts/buildbox/publish-debian-packages.sh
+++ scripts/buildbox/publish-debian-packages.sh
@@ -11,12 +11,15 @@ function publish-package {
   ./scripts/publish-debian-package.sh $1 $CODENAME
 }
 
+# Export the function so we can use it in xargs
+export -f publish-package
+
 echo '--- Installing dependencies'
 gem install deb-s3
 rbenv rehash
 
 echo '--- Downloading package artifacts'
-~/.buildbox/bin/buildbox-agent artifact download "pkg/deb/*.deb" .
+~/.buildbox/bin/buildbox-agent artifact download "pkg/deb/*.deb" . --job ""
 
 # Loop over all the .deb files and publish them
 ls pkg/deb/*.deb | xargs publish-package
