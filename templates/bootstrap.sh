6ba23091354432680735a7619c20cefdf075332d
Keith Pitt <me@keithpitt.com>
2015-01-15 16:25:58 +0800

    Rearranged the bootstrap.sh file to be easier to read

--- templates/bootstrap.sh
+++ templates/bootstrap.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-BUILDBOX_BOOTSTRAP_VERSION="0.9"
+#
+#  _           _ _     _ _    _ _              _                 _       _
+# | |         (_) |   | | |  (_) |            | |               | |     | |
+# | |__  _   _ _| | __| | | ___| |_ ___ ______| |__   ___   ___ | |_ ___| |_ _ __ __ _ _ __
+# | '_ \| | | | | |/ _` | |/ / | __/ _ \______| '_ \ / _ \ / _ \| __/ __| __| '__/ _` | '_ \
+# | |_) | |_| | | | (_| |   <| | ||  __/      | |_) | (_) | (_) | |_\__ \ |_| | | (_| | |_) |
+# |_.__/ \__,_|_|_|\__,_|_|\_\_|\__\___|      |_.__/ \___/ \___/ \__|___/\__|_|  \__,_| .__/
+#                                                                                     | |
+#                                                                                     |_|
+# https://github.com/buildbox/agent/blob/master/templates/bootstrap.sh
+
+BUILDBOX_BOOTSTRAP_VERSION="buildkite-bootstrap version 1.0-beta.1"
+
+##############################################################
+#
+# BOOTSTRAP FUNCTIONS
+# These functions are used throughout the bootstrap.sh file
+#
+##############################################################
+
 BUILDBOX_PROMPT="\033[90m$\033[0m"
 
 function buildbox-prompt-and-run {
@@ -10,96 +29,63 @@ function buildbox-prompt-and-run {
 
 function buildbox-run {
   echo -e "$BUILDBOX_PROMPT $1"
-
   eval $1
   EVAL_EXIT_STATUS=$?
 
-  if [ $EVAL_EXIT_STATUS -ne 0 ]
-  then
+  if [[ $EVAL_EXIT_STATUS -ne 0 ]]; then
     exit $EVAL_EXIT_STATUS
   fi
 }
 
-if [ "$BUILDBOX_BIN_DIR" != "" ]
-then
-  echo "Deprecation warning: BUILDBOX_BIN_DIR has been renamed to BUILDBOX_BIN_PATH"
-  BUILDBOX_BIN_PATH=$BUILDBOX_BIN_DIR
-fi
+##############################################################
+#
+# PATH DEFAULTS
+# Come up with the paths used throughout the bootstrap.sh file
+#
+##############################################################
 
-if [ "$BUILDBOX_DIR" != "" ]
-then
-  echo "Deprecation warning: BUILDBOX_DIR has been renamed to BUILDBOX_PATH"
-  BUILDBOX_PATH=$BUILDBOX_DIR
-fi
-
-# Provide a default BUILDBOX_PATH
-if [ -z "$BUILDBOX_PATH" ]; then
-  # This will return the location of this file. We assume that the buildbox-artifact
-  # tool is in the same folder. You can of course customize the locations
-  # and edit this file.
-  BUILDBOX_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-fi
-
-# If no BUILDBOX_BIN_PATH has been provided, make one up
-if [ -z "$BUILDBOX_BIN_PATH" ]; then
-  if [ -d "$BUILDBOX_PATH/bin" ]; then
-    BUILDBOX_BIN_PATH="$BUILDBOX_PATH/bin"
-  else
-    BUILDBOX_BIN_PATH="$BUILDBOX_PATH"
-  fi
-fi
+: ${BUILDBOX_PATH:="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"}
+: ${BUILDBOX_BIN_PATH:="$BUILDBOX_PATH/bin"}
+: ${BUILDBOX_BUILD_PATH:="$BUILDBOX_PATH/builds"}
 
 # Add the $BUILDBOX_BIN to the $PATH
 export PATH="$BUILDBOX_BIN_PATH:$PATH"
 
-function buildbox-meta-data {
-  # Check to see if we're on the latest version of the agent (older agents
-  # don't have the build-data argument)
-  if buildbox-agent --help | grep build-data
-  then
-    buildbox-agent build-data set $1 $2
-  fi
-}
-
-echo '--- setup environment'
-
 # Send the bootstrap version back to Buildbox
-buildbox-meta-data "buildkite:bootstrap:version" $BUILDBOX_BOOTSTRAP_VERSION
+buildbox-agent build-data set "buildkite:bootstrap:version" $BUILDBOX_BOOTSTRAP_VERSION
 
-# Create the build directory
+# Come up with the place that the repository will be checked out to
 SANITIZED_AGENT_NAME=$(echo $BUILDBOX_AGENT_NAME | tr -d '"')
 PROJECT_FOLDER_NAME="$SANITIZED_AGENT_NAME/$BUILDBOX_PROJECT_SLUG"
+BUILDBOX_BUILD_CHECKOUT_PATH="$BUILDBOX_BUILD_PATH/$PROJECT_FOLDER_NAME"
 
-# Allow overiding the location that builds get run in
-if [ -z "$BUILDBOX_BUILD_PATH" ]; then
-  BUILDBOX_PROJECT_PATH="$BUILDBOX_PATH/builds/$PROJECT_FOLDER_NAME"
-else
-  BUILDBOX_PROJECT_PATH="$BUILDBOX_BUILD_PATH/$PROJECT_FOLDER_NAME"
-fi
+##############################################################
+#
+# REPOSITORY HANDLING
+# Creates the build folder and makes sure we're running the
+# build at the right commit.
+#
+##############################################################
 
 # Show the ENV variables if DEBUG is on
-if [ "$BUILDBOX_AGENT_DEBUG" == "true" ]
-then
+if [[ "$BUILDBOX_AGENT_DEBUG" == "true" ]]; then
+  echo '--- Buildbox environment variables'
+
   buildbox-run "env | grep BUILDBOX"
 fi
 
-buildbox-run "mkdir -p \"$BUILDBOX_PROJECT_PATH\""
-buildbox-run "cd \"$BUILDBOX_PROJECT_PATH\""
+echo '--- Preparing build'
+
+buildbox-run "mkdir -p \"$BUILDBOX_BUILD_CHECKOUT_PATH\""
+buildbox-run "cd \"$BUILDBOX_BUILD_CHECKOUT_PATH\""
 
 # Do we need to do a git checkout?
-if [ ! -d ".git" ]
-then
+if [[ ! -d ".git" ]]; then
   # If it's a first time SSH git clone it will prompt to accept the host's
   # fingerprint. To avoid this add the host's key to ~/.ssh/known_hosts ahead
   # of time:
   #   ssh-keyscan -H host.com >> ~/.ssh/known_hosts
-  buildbox-run "git clone "$BUILDBOX_REPO" . -qv"
-fi
-
-# Default empty branch names
-if [ "$BUILDBOX_BRANCH" == "" ]
-then
-  BUILDBOX_BRANCH="master"
+  buildbox-run "git clone \"$BUILDBOX_REPO\" . -qv"
 fi
 
 buildbox-run "git clean -fdq"
@@ -107,29 +93,35 @@ buildbox-run "git fetch -q"
 
 # Allow checkouts of forked pull requests on GitHub only. See:
 # https://help.github.com/articles/checking-out-pull-requests-locally/#modifying-an-inactive-pull-request-locally
-if [ "$BUILDBOX_PULL_REQUEST" != "false" ] && [ "$BUILDBOX_PROJECT_PROVIDER" == *"github"* ]
-then
+if [[ "$BUILDBOX_PULL_REQUEST" != "false" ]] && [[ "$BUILDBOX_PROJECT_PROVIDER" == *"github"* ]]; then
   buildbox-run "git fetch origin \"+refs/pull/$BUILDBOX_PULL_REQUEST/merge:\""
-elif [ "$BUILDBOX_TAG" == "" ]
-then
+elif [[ "$BUILDBOX_TAG" == "" ]]; then
+  # Default empty branch names
+  : ${BUILDBOX_BRANCH:=master}
+
   buildbox-run "git reset --hard origin/$BUILDBOX_BRANCH"
 fi
 
 buildbox-run "git checkout -qf \"$BUILDBOX_COMMIT\""
 
 # Grab author and commit information and send it back to Buildbox
-buildbox-meta-data "buildkite:git:commit" "`git show "$BUILDBOX_COMMIT" -s --format=fuller --no-color`"
-buildbox-meta-data "buildkite:git:branch" "`git branch --contains "$BUILDBOX_COMMIT" --no-color`"
+buildbox-agent build-data set "buildkite:git:commit" "`git show "$BUILDBOX_COMMIT" -s --format=fuller --no-color`"
+buildbox-agent build-data set "buildkite:git:branch" "`git branch --contains "$BUILDBOX_COMMIT" --no-color`"
 
-if [ "$BUILDBOX_SCRIPT_PATH" == "" ]
-then
+##############################################################
+#
+# RUN THE BUILD
+# Determines how to run the build, and then runs it
+#
+##############################################################
+
+if [[ "$BUILDBOX_SCRIPT_PATH" == "" ]]; then
   echo "ERROR: No script to run. Please go to \"Project Settings\" and configure your build step's \"Script to Run\""
   exit 1
 fi
 
 ## Docker
-if [ "$BUILDBOX_DOCKER" != "" ]
-then
+if [[ "$BUILDBOX_DOCKER" != "" ]]; then
   DOCKER_CONTAINER="buildbox_"$BUILDBOX_JOB_ID"_container"
   DOCKER_IMAGE="buildbox_"$BUILDBOX_JOB_ID"_image"
 
@@ -147,14 +139,13 @@ then
   # Build the Docker image, namespaced to the job
   buildbox-run "docker build -t $DOCKER_IMAGE ."
 
-  echo "--- running $BUILDBOX_SCRIPT_PATH (in Docker container $DOCKER_IMAGE)"
+  echo "--- Running $BUILDBOX_SCRIPT_PATH (in Docker container $DOCKER_IMAGE)"
 
   # Run the build script command in a one-off container
   buildbox-prompt-and-run "docker run --name $DOCKER_CONTAINER $DOCKER_IMAGE ./$BUILDBOX_SCRIPT_PATH"
 
 ## Fig
-elif [ "$BUILDBOX_FIG_CONTAINER" != "" ]
-then
+elif [[ "$BUILDBOX_FIG_CONTAINER" != "" ]]; then
   # Fig strips dashes and underscores, so we'll remove them to match the docker container names
   FIG_PROJ_NAME="buildbox"${BUILDBOX_JOB_ID//-}
   # The name of the docker container fig creates when it creates the adhoc run
@@ -179,62 +170,48 @@ then
   # Build the Docker images using Fig, namespaced to the job
   buildbox-run "fig -p $FIG_PROJ_NAME build"
 
-  echo "--- running $BUILDBOX_SCRIPT_PATH (in Fig container '$BUILDBOX_FIG_CONTAINER')"
+  echo "--- Running $BUILDBOX_SCRIPT_PATH (in Fig container '$BUILDBOX_FIG_CONTAINER')"
 
   # Run the build script command in the service specified in BUILDBOX_FIG_CONTAINER
   buildbox-prompt-and-run "fig -p $FIG_PROJ_NAME run $BUILDBOX_FIG_CONTAINER ./$BUILDBOX_SCRIPT_PATH"
 
 ## Standard
 else
-  echo "--- running $BUILDBOX_SCRIPT_PATH"
+  echo "+++ Running build script"
+  echo -e "$BUILDBOX_PROMPT ./$BUILDBOX_SCRIPT_PATH"
 
-  # Run the step's build script
   ."/$BUILDBOX_SCRIPT_PATH"
 fi
 
 # Capture the exit status for the end
 EXIT_STATUS=$?
 
-if [ "$BUILDBOX_ARTIFACT_PATHS" != "" ]
-then
-  # NOTE: In agent version 1.0 and above, the location and the name of the
-  # buildbox artifact binary changed. As of this verison, builbdox-artifact has
-  # been rolled into buildbox-agent.
-  if buildbox-agent --help | grep build-artifact
-  then
-    # If you want to upload artifacts to your own server, uncomment the lines below
-    # and replace the AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY with keys to your
-    # own bucket.
-    #
-    # export AWS_SECRET_ACCESS_KEY=yyy
-    # export AWS_ACCESS_KEY_ID=xxx
-    # export AWS_S3_ACL=private
-    # buildbox-run "buildbox-agent build-artifact upload \"$BUILDBOX_ARTIFACT_PATHS\" \"s3://name-of-your-s3-bucket/$BUILDBOX_JOB_ID\""
+##############################################################
+#
+# ARTIFACTS
+# Uploads and build artifacts associated with this build
+#
+##############################################################
 
-    # Show the output of the artifact uploder when in debug mode
-    if [ "$BUILDBOX_AGENT_DEBUG" == "true" ]
-    then
-      echo '--- uploading artifacts'
-      buildbox-run "buildbox-agent build-artifact upload \"$BUILDBOX_ARTIFACT_PATHS\""
-    else
-      buildbox-run "buildbox-agent build-artifact upload \"$BUILDBOX_ARTIFACT_PATHS\" > /dev/null 2>&1"
-    fi
-  elif [[ -e $BUILDBOX_PATH/buildbox-artifact ]]
-  then
-    # If you want to upload artifacts to your own server, uncomment the lines below
-    # and replace the AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY with keys to your
-    # own bucket.
-    # export AWS_SECRET_ACCESS_KEY=yyy
-    # export AWS_ACCESS_KEY_ID=xxx
-    # buildbox-run "buildbox-artifact upload \"$BUILDBOX_ARTIFACT_PATHS\" "s3://name-of-your-s3-bucket/$BUILDBOX_JOB_ID" --url $BUILDBOX_AGENT_API_URL > /dev/null 2>&1"
+if [[ "$BUILDBOX_ARTIFACT_PATHS" != "" ]]; then
+  # If you want to upload artifacts to your own server, uncomment the lines below
+  # and replace the AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY with keys to your
+  # own bucket.
+  #
+  # export AWS_SECRET_ACCESS_KEY=yyy
+  # export AWS_ACCESS_KEY_ID=xxx
+  # export AWS_S3_ACL=private
+  # buildbox-run "buildbox-agent build-artifact upload \"$BUILDBOX_ARTIFACT_PATHS\" \"s3://name-of-your-s3-bucket/$BUILDBOX_JOB_ID\""
 
-    # By default we silence the buildbox-artifact build output. However, if you'd like to see
-    # it in your logs, remove the: > /dev/null 2>&1 from the end of the line.
-    buildbox-run "buildbox-artifact upload \"$BUILDBOX_ARTIFACT_PATHS\" --url $BUILDBOX_AGENT_API_URL > /dev/null 2>&1"
+  # Show the output of the artifact uploder when in debug mode
+  if [[ "$BUILDBOX_AGENT_DEBUG" == "true" ]]; then
+    echo '--- Uploading artifacts'
+    buildbox-run "buildbox-agent build-artifact upload \"$BUILDBOX_ARTIFACT_PATHS\""
   else
-    echo >&2 "ERROR: buildbox-artifact could not be found in $BUILDBOX_PATH"
-    exit 1
+    buildbox-run "buildbox-agent build-artifact upload \"$BUILDBOX_ARTIFACT_PATHS\" > /dev/null 2>&1"
   fi
 fi
 
+# Be sure to exit this script with the same exit status that the users build
+# script exited with.
 exit $EXIT_STATUS
