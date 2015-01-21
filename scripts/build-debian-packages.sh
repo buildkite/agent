daafe2bb934e19acf4e3002439a14c8557f87a02
Keith Pitt <me@keithpitt.com>
2015-01-21 14:00:14 +0800

    Added ruby to the Dockerfile

--- scripts/build-debian-packages.sh
+++ scripts/build-debian-packages.sh
@@ -2,35 +2,27 @@
 set -e
 
 if [[ "$CODENAME" == "" ]]; then
-  echo "Error: Missing $CODENAME (`stable` or `unstable`)"
+  echo "Error: Missing \$CODENAME (\`stable\` or \`unstable\`)"
   exit 1
 fi
 
 function build-package {
   echo "--- Building debian package $1/$2"
 
-  # Attach the buildbox build number to debian packages we're releasing to the
+  # Attach the Buildkite build number to debian packages we're releasing to the
   # unstable chanel.
   if [ "$CODENAME" == "unstable" ]; then
-    ./scripts/build-debian-package.sh $1 $2 $BUILDBOX_BUILD_NUMBER
+    ./scripts/utils/build-debian-package.sh $1 $2 $BUILDKITE_BUILD_NUMBER
   else
-    ./scripts/build-debian-package.sh $1 $2
+    ./scripts/utils/build-debian-package.sh $1 $2
   fi
 }
 
 # Clear out any existing pkg dir
 rm -rf pkg
 
-echo '--- Installing ruby dependencies'
-bundle
-
-echo '--- Installing go dependencies'
-# setup the current repo as a package - super hax.
-mkdir -p gopath/src/github.com/buildbox
-ln -s `pwd` gopath/src/github.com/buildbox/agent
-export GOPATH="$GOPATH:`pwd`/gopath"
-
-go get github.com/tools/godep
+echo '--- Installing dependencies'
+bundle --path vendor/bundle
 godep restore
 
 # Build the packages
