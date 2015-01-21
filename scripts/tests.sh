b92e7280b935c8faec6f6784566a7abe68dd0897
Keith Pitt <me@keithpitt.com>
2015-01-21 13:36:34 +0800

    More rearranging of the scripts folder.

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 set -e
 
-# setup the current repo as a package - super hax.
-mkdir -p gopath/src/github.com/buildbox
-ln -s `pwd` gopath/src/github.com/buildbox/agent
-export GOPATH="$GOPATH:`pwd`/gopath"
-
 echo '--- Install dependencies'
-go get github.com/tools/godep
 godep restore
 
 echo '--- Running golint'
-go get github.com/golang/lint/golint
 make lint
 
 echo '--- Running tests'
