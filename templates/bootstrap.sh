a600b5f474ff74041984896bd50cb7f129fcf1e9
Keith Pitt <me@keithpitt.com>
2015-01-21 13:04:42 +0800

    Changed buildbox references in the template/bootstrap.sh file

--- templates/bootstrap.sh
+++ templates/bootstrap.sh
@@ -9,9 +9,9 @@
 # |_.__/ \__,_|_|_|\__,_|_|\_\_|\__\___|      |_.__/ \___/ \___/ \__|___/\__|_|  \__,_| .__/
 #                                                                                     | |
 #                                                                                     |_|
-# https://github.com/buildbox/agent/blob/master/templates/bootstrap.sh
+# https://github.com/buildkite/agent/blob/master/templates/bootstrap.sh
 
-BUILDBOX_BOOTSTRAP_VERSION="buildkite-bootstrap version 1.0-beta.1"
+BUILDKITE_BOOTSTRAP_VERSION="buildkite-bootstrap version 1.0-beta.1"
 
 ##############################################################
 #
@@ -20,15 +20,15 @@ BUILDBOX_BOOTSTRAP_VERSION="buildkite-bootstrap version 1.0-beta.1"
 #
 ##############################################################
 
-BUILDBOX_PROMPT="\033[90m$\033[0m"
+BUILDKITE_PROMPT="\033[90m$\033[0m"
 
-function buildbox-prompt-and-run {
-  echo -e "$BUILDBOX_PROMPT $1"
+function buildkite-prompt-and-run {
+  echo -e "$BUILDKITE_PROMPT $1"
   eval $1
 }
 
-function buildbox-run {
-  echo -e "$BUILDBOX_PROMPT $1"
+function buildkite-run {
+  echo -e "$BUILDKITE_PROMPT $1"
   eval $1
   EVAL_EXIT_STATUS=$?
 
@@ -44,20 +44,20 @@ function buildbox-run {
 #
 ##############################################################
 
-: ${BUILDBOX_PATH:="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"}
-: ${BUILDBOX_BIN_PATH:="$BUILDBOX_PATH/bin"}
-: ${BUILDBOX_BUILD_PATH:="$BUILDBOX_PATH/builds"}
+: ${BUILDKITE_PATH:="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"}
+: ${BUILDKITE_BIN_PATH:="$BUILDKITE_PATH/bin"}
+: ${BUILDKITE_BUILD_PATH:="$BUILDKITE_PATH/builds"}
 
-# Add the $BUILDBOX_BIN to the $PATH
-export PATH="$BUILDBOX_BIN_PATH:$PATH"
+# Add the $BUILDKITE_BIN to the $PATH
+export PATH="$BUILDKITE_BIN_PATH:$PATH"
 
-# Send the bootstrap version back to Buildbox
-buildbox-agent build-data set "buildkite:bootstrap:version" $BUILDBOX_BOOTSTRAP_VERSION
+# Send the bootstrap version back to Buildkite
+buildkite-agent build-data set "buildkite:bootstrap:version" $BUILDKITE_BOOTSTRAP_VERSION
 
 # Come up with the place that the repository will be checked out to
-SANITIZED_AGENT_NAME=$(echo $BUILDBOX_AGENT_NAME | tr -d '"')
-PROJECT_FOLDER_NAME="$SANITIZED_AGENT_NAME/$BUILDBOX_PROJECT_SLUG"
-BUILDBOX_BUILD_CHECKOUT_PATH="$BUILDBOX_BUILD_PATH/$PROJECT_FOLDER_NAME"
+SANITIZED_AGENT_NAME=$(echo $BUILDKITE_AGENT_NAME | tr -d '"')
+PROJECT_FOLDER_NAME="$SANITIZED_AGENT_NAME/$BUILDKITE_PROJECT_SLUG"
+BUILDKITE_BUILD_CHECKOUT_PATH="$BUILDKITE_BUILD_PATH/$PROJECT_FOLDER_NAME"
 
 ##############################################################
 #
@@ -67,17 +67,17 @@ BUILDBOX_BUILD_CHECKOUT_PATH="$BUILDBOX_BUILD_PATH/$PROJECT_FOLDER_NAME"
 #
 ##############################################################
 
-# Remove the checkout folder if BUILDBOX_CLEAN_CHECKOUT is present
-if [[ "$BUILDBOX_CLEAN_CHECKOUT" == "true" ]]; then
+# Remove the checkout folder if BUILDKITE_CLEAN_CHECKOUT is present
+if [[ "$BUILDKITE_CLEAN_CHECKOUT" == "true" ]]; then
   echo '--- Cleaning project checkout'
 
-  buildbox-run "rm -rf \"$BUILDBOX_BUILD_CHECKOUT_PATH\""
+  buildkite-run "rm -rf \"$BUILDKITE_BUILD_CHECKOUT_PATH\""
 fi
 
 echo '--- Preparing build folder'
 
-buildbox-run "mkdir -p \"$BUILDBOX_BUILD_CHECKOUT_PATH\""
-buildbox-run "cd \"$BUILDBOX_BUILD_CHECKOUT_PATH\""
+buildkite-run "mkdir -p \"$BUILDKITE_BUILD_CHECKOUT_PATH\""
+buildkite-run "cd \"$BUILDKITE_BUILD_CHECKOUT_PATH\""
 
 # Do we need to do a git checkout?
 if [[ ! -d ".git" ]]; then
@@ -85,28 +85,28 @@ if [[ ! -d ".git" ]]; then
   # fingerprint. To avoid this add the host's key to ~/.ssh/known_hosts ahead
   # of time:
   #   ssh-keyscan -H host.com >> ~/.ssh/known_hosts
-  buildbox-run "git clone \"$BUILDBOX_REPO\" . -qv"
+  buildkite-run "git clone \"$BUILDKITE_REPO\" . -qv"
 fi
 
-buildbox-run "git clean -fdq"
-buildbox-run "git fetch -q"
+buildkite-run "git clean -fdq"
+buildkite-run "git fetch -q"
 
 # Allow checkouts of forked pull requests on GitHub only. See:
 # https://help.github.com/articles/checking-out-pull-requests-locally/#modifying-an-inactive-pull-request-locally
-if [[ "$BUILDBOX_PULL_REQUEST" != "false" ]] && [[ "$BUILDBOX_PROJECT_PROVIDER" == *"github"* ]]; then
-  buildbox-run "git fetch origin \"+refs/pull/$BUILDBOX_PULL_REQUEST/head:\""
-elif [[ "$BUILDBOX_TAG" == "" ]]; then
+if [[ "$BUILDKITE_PULL_REQUEST" != "false" ]] && [[ "$BUILDKITE_PROJECT_PROVIDER" == *"github"* ]]; then
+  buildkite-run "git fetch origin \"+refs/pull/$BUILDKITE_PULL_REQUEST/head:\""
+elif [[ "$BUILDKITE_TAG" == "" ]]; then
   # Default empty branch names
-  : ${BUILDBOX_BRANCH:=master}
+  : ${BUILDKITE_BRANCH:=master}
 
-  buildbox-run "git reset --hard origin/$BUILDBOX_BRANCH"
+  buildkite-run "git reset --hard origin/$BUILDKITE_BRANCH"
 fi
 
-buildbox-run "git checkout -qf \"$BUILDBOX_COMMIT\""
+buildkite-run "git checkout -qf \"$BUILDKITE_COMMIT\""
 
-# Grab author and commit information and send it back to Buildbox
-buildbox-agent build-data set "buildkite:git:commit" "`git show "$BUILDBOX_COMMIT" -s --format=fuller --no-color`"
-buildbox-agent build-data set "buildkite:git:branch" "`git branch --contains "$BUILDBOX_COMMIT" --no-color`"
+# Grab author and commit information and send it back to Buildkite
+buildkite-agent build-data set "buildkite:git:commit" "`git show "$BUILDKITE_COMMIT" -s --format=fuller --no-color`"
+buildkite-agent build-data set "buildkite:git:branch" "`git branch --contains "$BUILDKITE_COMMIT" --no-color`"
 
 ##############################################################
 #
@@ -115,15 +115,15 @@ buildbox-agent build-data set "buildkite:git:branch" "`git branch --contains "$B
 #
 ##############################################################
 
-if [[ "$BUILDBOX_SCRIPT_PATH" == "" ]]; then
+if [[ "$BUILDKITE_SCRIPT_PATH" == "" ]]; then
   echo "ERROR: No script to run. Please go to \"Project Settings\" and configure your build step's \"Script to Run\""
   exit 1
 fi
 
 ## Docker
-if [[ "$BUILDBOX_DOCKER" != "" ]]; then
-  DOCKER_CONTAINER="buildbox_"$BUILDBOX_JOB_ID"_container"
-  DOCKER_IMAGE="buildbox_"$BUILDBOX_JOB_ID"_image"
+if [[ "$BUILDKITE_DOCKER" != "" ]]; then
+  DOCKER_CONTAINER="buildkite_"$BUILDKITE_JOB_ID"_container"
+  DOCKER_IMAGE="buildkite_"$BUILDKITE_JOB_ID"_image"
 
   function docker-cleanup {
     docker rm -f $DOCKER_CONTAINER
@@ -137,19 +137,19 @@ if [[ "$BUILDBOX_DOCKER" != "" ]]; then
   trap docker-cleanup EXIT
 
   # Build the Docker image, namespaced to the job
-  buildbox-run "docker build -t $DOCKER_IMAGE ."
+  buildkite-run "docker build -t $DOCKER_IMAGE ."
 
-  echo "+++ Running $BUILDBOX_SCRIPT_PATH (in Docker container $DOCKER_IMAGE)"
+  echo "+++ Running $BUILDKITE_SCRIPT_PATH (in Docker container $DOCKER_IMAGE)"
 
   # Run the build script command in a one-off container
-  buildbox-prompt-and-run "docker run --name $DOCKER_CONTAINER $DOCKER_IMAGE ./$BUILDBOX_SCRIPT_PATH"
+  buildkite-prompt-and-run "docker run --name $DOCKER_CONTAINER $DOCKER_IMAGE ./$BUILDKITE_SCRIPT_PATH"
 
 ## Fig
-elif [[ "$BUILDBOX_FIG_CONTAINER" != "" ]]; then
+elif [[ "$BUILDKITE_FIG_CONTAINER" != "" ]]; then
   # Fig strips dashes and underscores, so we'll remove them to match the docker container names
-  FIG_PROJ_NAME="buildbox"${BUILDBOX_JOB_ID//-}
+  FIG_PROJ_NAME="buildkite"${BUILDKITE_JOB_ID//-}
   # The name of the docker container fig creates when it creates the adhoc run
-  FIG_CONTAINER_NAME=$FIG_PROJ_NAME"_"$BUILDBOX_FIG_CONTAINER
+  FIG_CONTAINER_NAME=$FIG_PROJ_NAME"_"$BUILDKITE_FIG_CONTAINER
 
   function fig-cleanup {
     fig -p $FIG_PROJ_NAME kill
@@ -168,19 +168,19 @@ elif [[ "$BUILDBOX_FIG_CONTAINER" != "" ]]; then
   trap fig-cleanup EXIT
 
   # Build the Docker images using Fig, namespaced to the job
-  buildbox-run "fig -p $FIG_PROJ_NAME build"
+  buildkite-run "fig -p $FIG_PROJ_NAME build"
 
-  echo "+++ Running $BUILDBOX_SCRIPT_PATH (in Fig container '$BUILDBOX_FIG_CONTAINER')"
+  echo "+++ Running $BUILDKITE_SCRIPT_PATH (in Fig container '$BUILDKITE_FIG_CONTAINER')"
 
-  # Run the build script command in the service specified in BUILDBOX_FIG_CONTAINER
-  buildbox-prompt-and-run "fig -p $FIG_PROJ_NAME run $BUILDBOX_FIG_CONTAINER ./$BUILDBOX_SCRIPT_PATH"
+  # Run the build script command in the service specified in BUILDKITE_FIG_CONTAINER
+  buildkite-prompt-and-run "fig -p $FIG_PROJ_NAME run $BUILDKITE_FIG_CONTAINER ./$BUILDKITE_SCRIPT_PATH"
 
 ## Standard
 else
   echo "+++ Running build script"
-  echo -e "$BUILDBOX_PROMPT ./$BUILDBOX_SCRIPT_PATH"
+  echo -e "$BUILDKITE_PROMPT ./$BUILDKITE_SCRIPT_PATH"
 
-  ."/$BUILDBOX_SCRIPT_PATH"
+  ."/$BUILDKITE_SCRIPT_PATH"
 fi
 
 # Capture the exit status for the end
@@ -193,7 +193,7 @@ EXIT_STATUS=$?
 #
 ##############################################################
 
-if [[ "$BUILDBOX_ARTIFACT_PATHS" != "" ]]; then
+if [[ "$BUILDKITE_ARTIFACT_PATHS" != "" ]]; then
   # If you want to upload artifacts to your own server, uncomment the lines below
   # and replace the AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY with keys to your
   # own bucket.
@@ -201,14 +201,14 @@ if [[ "$BUILDBOX_ARTIFACT_PATHS" != "" ]]; then
   # export AWS_SECRET_ACCESS_KEY=yyy
   # export AWS_ACCESS_KEY_ID=xxx
   # export AWS_S3_ACL=private
-  # buildbox-run "buildbox-agent build-artifact upload \"$BUILDBOX_ARTIFACT_PATHS\" \"s3://name-of-your-s3-bucket/$BUILDBOX_JOB_ID\""
+  # buildkite-run "buildkite-agent build-artifact upload \"$BUILDKITE_ARTIFACT_PATHS\" \"s3://name-of-your-s3-bucket/$BUILDKITE_JOB_ID\""
 
   # Show the output of the artifact uploder when in debug mode
-  if [[ "$BUILDBOX_AGENT_DEBUG" == "true" ]]; then
+  if [[ "$BUILDKITE_AGENT_DEBUG" == "true" ]]; then
     echo '--- Uploading artifacts'
-    buildbox-run "buildbox-agent build-artifact upload \"$BUILDBOX_ARTIFACT_PATHS\""
+    buildkite-run "buildkite-agent build-artifact upload \"$BUILDKITE_ARTIFACT_PATHS\""
   else
-    buildbox-run "buildbox-agent build-artifact upload \"$BUILDBOX_ARTIFACT_PATHS\" > /dev/null 2>&1"
+    buildkite-run "buildkite-agent build-artifact upload \"$BUILDKITE_ARTIFACT_PATHS\" > /dev/null 2>&1"
   fi
 fi
 
