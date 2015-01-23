700dc6c53ba72320eacb25f2dbef1ad4953c6962
Keith Pitt <me@keithpitt.com>
2015-01-23 14:40:47 +0800

    Forgot add script that builds the binaries.

---
+++ scripts/utils/build-binary.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+if [[ ${#} -lt 3 ]]
+then
+  echo "Usage: ${0} [platform] [arch] [buildVersion]" >&2
+  exit 1
+fi
+
+GOOS=${1}
+GOARCH=${2}
+BUILD_VERSION=${3}
+NAME="buildkite-binary"
+
+BUILD_PATH="pkg/deb"
+BINARY_NAME="$NAME-$GOOS-$GOARCH"
+
+# Ensure the deb path exists
+mkdir -p $BUILD_PATH
+
+echo "Building $NAME with:"
+echo "GOOS=$GOOS"
+echo "GOARCH=$GOARCH"
+echo "BUILD_VERSION=$BUILD_VERSION"
+
+go build -ldflags "-X github.com/buildkite/agent/buildkite.buildVersion $BUILD_VERSION" -o $BUILD_PATH/$BINARY_NAME *.go
+
+echo -e "\nDone: \033[33m$BUILD_PATH/$BINARY_NAME\033[0m 💪"
