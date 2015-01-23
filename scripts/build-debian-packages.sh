1ef20cd393de3c7addf3d79eba4705b74de3b80c
Keith Pitt <me@keithpitt.com>
2015-01-23 13:53:15 +0800

    Include more debugging in the build-debian-packages script.

--- scripts/build-debian-packages.sh
+++ scripts/build-debian-packages.sh
@@ -7,13 +7,15 @@ if [[ "$CODENAME" == "" ]]; then
 fi
 
 function build-package {
-  echo "--- Building debian package $1/$2"
-
   # Attach the Buildkite build number to debian packages we're releasing to the
   # unstable chanel.
-  if [ "$CODENAME" == "unstable" ]; then
+  if [[ "$CODENAME" == "unstable" ]]; then
+    echo "--- Building debian package $1/$2 ($CODENAME/$BUILDKITE_BUILD_NUMBER)"
+
     ./scripts/utils/build-debian-package.sh $1 $2 $BUILDKITE_BUILD_NUMBER
   else
+    echo "--- Building debian package $1/$2 ($CODENAME)"
+
     ./scripts/utils/build-debian-package.sh $1 $2
   fi
 }
