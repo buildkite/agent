ab49bffb1d3ba139ed645862d176de6fd1874b28
Keith Pitt <me@keithpitt.com>
2015-01-23 19:10:32 +0800

    More fixes to the github release script.

--- scripts/github-release.sh
+++ scripts/github-release.sh
@@ -10,7 +10,7 @@ echo '--- Downloading binaries'
 
 rm -rf pkg
 mkdir -p pkg
-buildbox-agent build-artifact download "pkg/*" .
+# buildbox-agent build-artifact download "pkg/*" .
 
 function build() {
   echo "--- Building release for: $1"
@@ -31,12 +31,23 @@ echo '--- Getting agent version from build meta data'
 
 AGENT_VERSION=$(buildbox-agent build-data get "agent-version" | sed 's/buildkite-agent version //')
 
-echo "--- 🚀 $AGENT_VERSION"
+echo "Version is $AGENT_VERSION"
 
-if [[ "$AGENT_VERSION" == *"beta"* || "$AGENT_VERSION" == *"alpha"* ]]; then
-  export GITHUB_RELEASE_PRERELEASE="true"
-fi
+echo "--- 🚀 $AGENT_VERSION"
 
 export GITHUB_RELEASE_REPOSITORY="buildkite/agent"
 
-github-release "v$AGENT_VERSION" releases/*
+if [[ "$AGENT_VERSION" == *"beta"* || "$AGENT_VERSION" == *"alpha"* ]]; then
+  # Beta versions of the agent will have the build number at the end of them
+  # like this:
+  #
+  #    buildkite-agent version 1.0-beta.7.227
+  #
+  # We don't want the build numbers for GitHub releases, so this command will
+  # drop them.
+  AGENT_VERSION=$($AGENT_VERSION | sed -E 's/\.[0-9]+$//')
+
+  github-release "v$AGENT_VERSION" releases/* --prerelease
+else
+  github-release "v$AGENT_VERSION" releases/*
+fi
