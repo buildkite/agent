cb46e008daf466dd0d62931b215183f398fbada4
Keith Pitt <me@keithpitt.com>
2015-01-23 14:54:56 +0800

    Scripts to start relasing to GitHub

--- scripts/github-release.sh
+++ scripts/github-release.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "TODO"
-exit 1
-
-echo '--- building'
+echo '--- Building Github packages'
 ./scripts/build-release.sh "windows" "386"
 ./scripts/build-release.sh "windows" "amd64"
 ./scripts/build-release.sh "linux" "amd64"
@@ -13,19 +10,7 @@ echo '--- building'
 ./scripts/build-release.sh "darwin" "386"
 ./scripts/build-release.sh "darwin" "amd64"
 
-# setup the current repo as a package - super hax.
-mkdir -p gopath/src/github.com/buildbox
-ln -s `pwd` gopath/src/github.com/buildbox/agent
-export GOPATH="$GOPATH:`pwd`/gopath"
-
-echo '--- install dependencies'
-go get github.com/tools/godep
-godep restore
-
-echo '--- installing github-release'
-go get github.com/buildbox/github-release
-
-echo '--- download binaries'
+echo '--- Downloading binaries'
 rm -rf pkg
 mkdir -p pkg
 buildbox-agent artifact download "pkg/*" pkg
