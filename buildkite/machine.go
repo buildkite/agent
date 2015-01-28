c973f2d9d39901f98fc76746c9e777a07967d8b0
Keith Pitt <me@keithpitt.com>
2015-01-28 19:33:29 +0800

    Improved how we get the distro release information for Linux machines

--- buildkite/machine.go
+++ buildkite/machine.go
@@ -1,15 +1,24 @@
 package buildkite
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"os/exec"
+	"path/filepath"
 	"runtime"
 	"strings"
 )
 
 // Returns the machines hostname
 func MachineHostname() (string, error) {
-	return run("hostname")
+	hostname, err := run("hostname")
+
+	if err != nil {
+		Logger.Fatalf("Could not retrieve hostname: %s", hostname)
+	}
+
+	return hostname, err
 }
 
 // Returns a dump of the raw operating system information
@@ -17,7 +26,7 @@ func MachineOSDump() (string, error) {
 	if runtime.GOOS == "darwin" {
 		return run("sw_vers")
 	} else if runtime.GOOS == "linux" {
-		return run("cat", "/etc/*-release")
+		return cat("/etc/*-release"), nil
 	} else if runtime.GOOS == "windows" {
 		return run("ver")
 	}
@@ -30,12 +39,37 @@ func MachineIsWindows() bool {
 	return runtime.GOOS == "windows"
 }
 
+// Replicates how the command line tool `cat` works, but is more verbose about
+// what it does
+func cat(pathToRead string) string {
+	files, err := filepath.Glob(pathToRead)
+
+	if err != nil {
+		Logger.Debugf("Failed to get list of files for OS Dump: %s", pathToRead)
+
+		return ""
+	} else {
+		var buffer bytes.Buffer
+
+		for _, file := range files {
+			data, err := ioutil.ReadFile(file)
+
+			if err != nil {
+				Logger.Debugf("Could not read file for OS Dump: %s (%T: %v)", file, err, err)
+			} else {
+				buffer.WriteString(string(data))
+			}
+		}
+
+		return buffer.String()
+	}
+}
+
 func run(command string, arg ...string) (string, error) {
 	output, err := exec.Command(command, arg...).Output()
 
 	if err != nil {
-		Logger.Fatal(err)
-
+		Logger.Debugf("Could not run: %s %s (returned %s) (%T: %v)", command, arg, output, err, err)
 		return "", err
 	}
 
