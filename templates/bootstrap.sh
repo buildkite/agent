3aa814b0a51c73c65a0104d7417c13eae0f971c7
Keith Pitt <me@keithpitt.com>
2015-02-12 19:19:18 +0800

    Allow over-riding of the checkout and run-command parts of the bootstrap.sh script

--- templates/bootstrap.sh
+++ templates/bootstrap.sh
@@ -144,63 +144,68 @@ buildkite-header "Preparing build folder"
 buildkite-run "mkdir -p \"$BUILDKITE_BUILD_CHECKOUT_PATH\""
 buildkite-run "cd \"$BUILDKITE_BUILD_CHECKOUT_PATH\""
 
-# If enabled, automatically run an ssh-keyscan on the git ssh host, to prevent
-# a yes/no promp from appearing when cloning/fetching
-if [[ ! -z "${BUILDKITE_AUTO_SSH_FINGERPRINT_VERIFICATION:-}" ]] && [[ "$BUILDKITE_AUTO_SSH_FINGERPRINT_VERIFICATION" == "true" ]]; then
-  if [[ ! -z "${BUILDKITE_AUTO_SSH_FINGERPRINT_VERIFICATION:-}" ]]; then
-    : ${BUILDKITE_SSH_DIRECTORY:="$HOME/.ssh"}
-    : ${BUILDKITE_SSH_KNOWN_HOST_PATH:="$BUILDKITE_SSH_DIRECTORY/known_hosts"}
+# If the user has specificed their own checkout hook
+if [[ -e "$BUILDKITE_HOOKS_PATH/checkout" ]]; then
+  buildkite-hook "checkout"
+else
+  # If enabled, automatically run an ssh-keyscan on the git ssh host, to prevent
+  # a yes/no promp from appearing when cloning/fetching
+  if [[ ! -z "${BUILDKITE_AUTO_SSH_FINGERPRINT_VERIFICATION:-}" ]] && [[ "$BUILDKITE_AUTO_SSH_FINGERPRINT_VERIFICATION" == "true" ]]; then
+    if [[ ! -z "${BUILDKITE_AUTO_SSH_FINGERPRINT_VERIFICATION:-}" ]]; then
+      : ${BUILDKITE_SSH_DIRECTORY:="$HOME/.ssh"}
+      : ${BUILDKITE_SSH_KNOWN_HOST_PATH:="$BUILDKITE_SSH_DIRECTORY/known_hosts"}
 
-    # Ensure the known_hosts file exists
-    mkdir -p $BUILDKITE_SSH_DIRECTORY
-    touch $BUILDKITE_SSH_KNOWN_HOST_PATH
+      # Ensure the known_hosts file exists
+      mkdir -p $BUILDKITE_SSH_DIRECTORY
+      touch $BUILDKITE_SSH_KNOWN_HOST_PATH
 
-    # Only add the output from ssh-keyscan if it doesn't already exist in the
-    # known_hosts file
-    if ! ssh-keygen -H -F "$BUILDKITE_REPO_SSH_HOST" | grep --quiet "$BUILDKITE_REPO_SSH_HOST"; then
-      buildkite-run "ssh-keyscan \"$BUILDKITE_REPO_SSH_HOST\" >> \"$BUILDKITE_SSH_KNOWN_HOST_PATH\""
+      # Only add the output from ssh-keyscan if it doesn't already exist in the
+      # known_hosts file
+      if ! ssh-keygen -H -F "$BUILDKITE_REPO_SSH_HOST" | grep --quiet "$BUILDKITE_REPO_SSH_HOST"; then
+        buildkite-run "ssh-keyscan \"$BUILDKITE_REPO_SSH_HOST\" >> \"$BUILDKITE_SSH_KNOWN_HOST_PATH\""
+      fi
     fi
   fi
+
+  # Disable any interactive Git/SSH prompting
+  export GIT_TERMINAL_PROMPT=0
+
+  # Do we need to do a git checkout?
+  if [[ ! -d ".git" ]]; then
+    buildkite-run "git clone \"$BUILDKITE_REPO\" . -qv"
+  fi
+
+  buildkite-run "git clean -fdq"
+  buildkite-run "git submodule foreach --recursive git clean -fdq"
+
+  buildkite-run "git fetch -q"
+
+  # Allow checkouts of forked pull requests on GitHub only. See:
+  # https://help.github.com/articles/checking-out-pull-requests-locally/#modifying-an-inactive-pull-request-locally
+  if [[ "$BUILDKITE_PULL_REQUEST" != "false" ]] && [[ "$BUILDKITE_PROJECT_PROVIDER" == *"github"* ]]; then
+    buildkite-run "git fetch origin \"+refs/pull/$BUILDKITE_PULL_REQUEST/head:\""
+  elif [[ "$BUILDKITE_TAG" == "" ]]; then
+    # Default empty branch names
+    : ${BUILDKITE_BRANCH:=master}
+
+    buildkite-run "git reset --hard origin/$BUILDKITE_BRANCH"
+  fi
+
+  buildkite-run "git checkout -qf \"$BUILDKITE_COMMIT\""
+
+  # `submodule sync` will ensure the .git/config matches the .gitmodules file
+  buildkite-run "git submodule sync"
+  buildkite-run "git submodule update --init --recursive"
+  buildkite-run "git submodule foreach --recursive git reset --hard"
+
+  # Grab author and commit information and send it back to Buildkite
+  buildkite-header-debug "Saving Git information"
+  buildkite-run-debug "buildkite-agent build-data set \"buildkite:git:commit\" \"\`git show \"$BUILDKITE_COMMIT\" -s --format=fuller --no-color\`\""
+  buildkite-run-debug "buildkite-agent build-data set \"buildkite:git:branch\" \"\`git branch --contains \"$BUILDKITE_COMMIT\" --no-color\`\""
 fi
-
-# Disable any interactive Git/SSH prompting
-export GIT_TERMINAL_PROMPT=0
-
-# Do we need to do a git checkout?
-if [[ ! -d ".git" ]]; then
-  buildkite-run "git clone \"$BUILDKITE_REPO\" . -qv"
-fi
-
-buildkite-run "git clean -fdq"
-buildkite-run "git submodule foreach --recursive git clean -fdq"
-
-buildkite-run "git fetch -q"
-
-# Allow checkouts of forked pull requests on GitHub only. See:
-# https://help.github.com/articles/checking-out-pull-requests-locally/#modifying-an-inactive-pull-request-locally
-if [[ "$BUILDKITE_PULL_REQUEST" != "false" ]] && [[ "$BUILDKITE_PROJECT_PROVIDER" == *"github"* ]]; then
-  buildkite-run "git fetch origin \"+refs/pull/$BUILDKITE_PULL_REQUEST/head:\""
-elif [[ "$BUILDKITE_TAG" == "" ]]; then
-  # Default empty branch names
-  : ${BUILDKITE_BRANCH:=master}
-
-  buildkite-run "git reset --hard origin/$BUILDKITE_BRANCH"
-fi
-
-buildkite-run "git checkout -qf \"$BUILDKITE_COMMIT\""
-
-# `submodule sync` will ensure the .git/config matches the .gitmodules file
-buildkite-run "git submodule sync"
-buildkite-run "git submodule update --init --recursive"
-buildkite-run "git submodule foreach --recursive git reset --hard"
 
 # Run the `post-checkout` hook
 buildkite-hook "post-checkout"
-
-# Grab author and commit information and send it back to Buildkite
-buildkite-header-debug "Saving Git information"
-buildkite-run-debug "buildkite-agent build-data set \"buildkite:git:commit\" \"\`git show \"$BUILDKITE_COMMIT\" -s --format=fuller --no-color\`\""
-buildkite-run-debug "buildkite-agent build-data set \"buildkite:git:branch\" \"\`git branch --contains \"$BUILDKITE_COMMIT\" --no-color\`\""
 
 ##############################################################
 #
@@ -233,78 +238,83 @@ fi
 # Make sure the script we're going to run is executable
 chmod +x $BUILDKITE_SCRIPT_PATH
 
-# Run the `pre-command` hook
-buildkite-hook "pre-command"
+# Run the `pre-run-command` hook
+buildkite-hook "pre-run-command"
 
-## Docker
-if [[ ! -z "${BUILDKITE_DOCKER:-}" ]] && [[ "$BUILDKITE_DOCKER" != "" ]]; then
-  DOCKER_CONTAINER="buildkite_"$BUILDKITE_JOB_ID"_container"
-  DOCKER_IMAGE="buildkite_"$BUILDKITE_JOB_ID"_image"
-
-  function docker-cleanup {
-    buildkite-run "docker rm -f -v $DOCKER_CONTAINER"
-
-    # Enabling the following line will prevent your build server from filling up,
-    # but will slow down your builds because it'll be built from scratch each time.
-    #
-    # docker rmi -f -v $DOCKER_IMAGE
-  }
-
-  trap docker-cleanup EXIT
-
-  buildkite-header "Building Docker image $DOCKER_IMAGE"
-
-  # Build the Docker image, namespaced to the job
-  buildkite-run "docker build -t $DOCKER_IMAGE ."
-
-  buildkite-header-expand "Running $BUILDKITE_SCRIPT_PATH (in Docker container $DOCKER_IMAGE)"
-
-  # Run the build script command in a one-off container
-  buildkite-prompt-and-run "docker run --name $DOCKER_CONTAINER $DOCKER_IMAGE ./$BUILDKITE_SCRIPT_PATH"
-
-## Fig
-elif [[ ! -z "${BUILDKITE_FIG_CONTAINER:-}" ]] && [[ "$BUILDKITE_FIG_CONTAINER" != "" ]]; then
-  # Fig strips dashes and underscores, so we'll remove them to match the docker container names
-  FIG_PROJ_NAME="buildkite"${BUILDKITE_JOB_ID//-}
-  # The name of the docker container fig creates when it creates the adhoc run
-  FIG_CONTAINER_NAME=$FIG_PROJ_NAME"_"$BUILDKITE_FIG_CONTAINER
-
-  function fig-cleanup {
-    buildkite-run "fig -p $FIG_PROJ_NAME kill"
-    buildkite-run "fig -p $FIG_PROJ_NAME rm --force -v"
-
-    # The adhoc run container isn't cleaned up by fig, so we have to do it ourselves
-    buildkite-run "docker rm -f -v "$FIG_CONTAINER_NAME"_run_1"
-
-    # Enabling the following line will prevent your build server from filling up,
-    # but will slow down your builds because it'll be built from scratch each time.
-    #
-    # docker rmi -f -v $FIG_CONTAINER_NAME
-  }
-
-  trap fig-cleanup EXIT
-
-  buildkite-header "Building Fig Docker images"
-
-  # Build the Docker images using Fig, namespaced to the job
-  buildkite-run "fig -p $FIG_PROJ_NAME build"
-
-  buildkite-header-expand "Running $BUILDKITE_SCRIPT_PATH (in Fig container '$BUILDKITE_FIG_CONTAINER')"
-
-  # Run the build script command in the service specified in BUILDKITE_FIG_CONTAINER
-  buildkite-prompt-and-run "fig -p $FIG_PROJ_NAME run $BUILDKITE_FIG_CONTAINER ./$BUILDKITE_SCRIPT_PATH"
-
-## Standard
+# If the user has specificed their own run-command hook
+if [[ -e "$BUILDKITE_HOOKS_PATH/run-command" ]]; then
+  buildkite-hook "run-command"
 else
-  buildkite-header-expand "Running build script"
+  ## Docker
+  if [[ ! -z "${BUILDKITE_DOCKER:-}" ]] && [[ "$BUILDKITE_DOCKER" != "" ]]; then
+    DOCKER_CONTAINER="buildkite_"$BUILDKITE_JOB_ID"_container"
+    DOCKER_IMAGE="buildkite_"$BUILDKITE_JOB_ID"_image"
 
-  echo -e "$BUILDKITE_PROMPT ./$BUILDKITE_SCRIPT_PATH"
+    function docker-cleanup {
+      buildkite-run "docker rm -f -v $DOCKER_CONTAINER"
 
-  ."/$BUILDKITE_SCRIPT_PATH"
+      # Enabling the following line will prevent your build server from filling up,
+      # but will slow down your builds because it'll be built from scratch each time.
+      #
+      # docker rmi -f -v $DOCKER_IMAGE
+    }
+
+    trap docker-cleanup EXIT
+
+    buildkite-header "Building Docker image $DOCKER_IMAGE"
+
+    # Build the Docker image, namespaced to the job
+    buildkite-run "docker build -t $DOCKER_IMAGE ."
+
+    buildkite-header-expand "Running $BUILDKITE_SCRIPT_PATH (in Docker container $DOCKER_IMAGE)"
+
+    # Run the build script command in a one-off container
+    buildkite-prompt-and-run "docker run --name $DOCKER_CONTAINER $DOCKER_IMAGE ./$BUILDKITE_SCRIPT_PATH"
+
+  ## Fig
+  elif [[ ! -z "${BUILDKITE_FIG_CONTAINER:-}" ]] && [[ "$BUILDKITE_FIG_CONTAINER" != "" ]]; then
+    # Fig strips dashes and underscores, so we'll remove them to match the docker container names
+    FIG_PROJ_NAME="buildkite"${BUILDKITE_JOB_ID//-}
+    # The name of the docker container fig creates when it creates the adhoc run
+    FIG_CONTAINER_NAME=$FIG_PROJ_NAME"_"$BUILDKITE_FIG_CONTAINER
+
+    function fig-cleanup {
+      buildkite-run "fig -p $FIG_PROJ_NAME kill"
+      buildkite-run "fig -p $FIG_PROJ_NAME rm --force -v"
+
+      # The adhoc run container isn't cleaned up by fig, so we have to do it ourselves
+      buildkite-run "docker rm -f -v "$FIG_CONTAINER_NAME"_run_1"
+
+      # Enabling the following line will prevent your build server from filling up,
+      # but will slow down your builds because it'll be built from scratch each time.
+      #
+      # docker rmi -f -v $FIG_CONTAINER_NAME
+    }
+
+    trap fig-cleanup EXIT
+
+    buildkite-header "Building Fig Docker images"
+
+    # Build the Docker images using Fig, namespaced to the job
+    buildkite-run "fig -p $FIG_PROJ_NAME build"
+
+    buildkite-header-expand "Running $BUILDKITE_SCRIPT_PATH (in Fig container '$BUILDKITE_FIG_CONTAINER')"
+
+    # Run the build script command in the service specified in BUILDKITE_FIG_CONTAINER
+    buildkite-prompt-and-run "fig -p $FIG_PROJ_NAME run $BUILDKITE_FIG_CONTAINER ./$BUILDKITE_SCRIPT_PATH"
+
+  ## Standard
+  else
+    buildkite-header-expand "Running build script"
+
+    echo -e "$BUILDKITE_PROMPT ./$BUILDKITE_SCRIPT_PATH"
+
+    ."/$BUILDKITE_SCRIPT_PATH"
+  fi
+
+  # Capture the exit status for the end
+  export BUILDKITE_COMMAND_EXIT_STATUS=$?
 fi
-
-# Capture the exit status for the end
-export BUILDKITE_COMMAND_EXIT_STATUS=$?
 
 # Run the `post-command` hook
 buildkite-hook "post-command"
