e6d61e760bdb1a4c2ab07526b5f4a5457d5b840d
Tim Lucas <t@toolmantim.com>
2015-02-18 15:40:01 +1100

    Fix the Github auto-updating of the homebrew recipe

--- scripts/utils/update-homebrew-formula.sh
+++ scripts/utils/update-homebrew-formula.sh
@@ -3,6 +3,13 @@
 set -euo pipefail
 set -x
 
+# Allows you to pipe JSON in and fetch keys using Ruby hash syntax
+#
+# Example: echo '{"key":"value"}' | parse_json '["key"]'
+function parse_json {
+  ruby -rjson -e "print JSON.parse(\$<.read)$1"
+}
+
 BINARY_NAME=buildkite-agent-darwin-386.tar.gz
 RELEASE_FILE="./releases/$BINARY_NAME"
 
@@ -15,11 +22,15 @@ DOWNLOAD_URL="https://github.com/buildkite/agent/releases/download/v$AGENT_VERSI
 FORMULA_FILE=./releases/buildkite-agent.rb
 UPDATED_FORMULA_FILE=./releases/buildkite-agent-updated.rb
 
-echo "Grab the formula from master"
+echo "Fetching master formula from Github Contents API"
 
-curl https://raw.githubusercontent.com/buildkite/homebrew-buildkite/master/buildkite-agent.rb -o $FORMULA_FILE
+CONTENTS_API_RESPONSE=$(curl "https://api.github.com/repos/buildkite/homebrew-buildkite/contents/buildkite-agent.rb?access_token=$GITHUB_RELEASE_ACCESS_TOKEN")
 
-echo "Update the homebrew formula"
+echo "Decoding into $FORMULA_FILE"
+
+echo $CONTENTS_API_RESPONSE | parse_json '["content"]' | base64 -D > $FORMULA_FILE
+
+echo "Writing updated formula to $UPDATED_FORMULA_FILE"
 
 RELEASE_SHA=($(shasum $RELEASE_FILE))
 
@@ -27,14 +38,16 @@ cat $FORMULA_FILE |
   ./scripts/utils/update-homebrew-formula.rb $BREW_RELEASE_TYPE $AGENT_VERSION $DOWNLOAD_URL $RELEASE_SHA \
   > $UPDATED_FORMULA_FILE
 
-echo "Update Github"
+echo "Updating master formula via Github Contents API"
 
 UPDATED_FORMULA_BASE64=$(base64 $UPDATED_FORMULA_FILE)
-MASTER_FORMULA_SHA=($(shasum $FORMULA_FILE))
+MASTER_FORMULA_SHA=$(echo $CONTENTS_API_RESPONSE | parse_json '["sha"]')
 
-curl -X PUT https://api.github.com/repos/buildkite/homebrew-buildkite/contents/buildkite-agent.rb \
-     -d "message=buildkite-agent $AGENT_VERSION" \
-     -d "sha=$MASTER_FORMULA_SHA" \
-     -d "content=$UPDATED_FORMULA_BASE64" \
-     -d "branch=master" \
-     -d "access_token=$GITHUB_RELEASE_ACCESS_TOKEN"
+curl -X PUT "https://api.github.com/repos/buildkite/homebrew-buildkite/contents/buildkite-agent.rb?access_token=$GITHUB_RELEASE_ACCESS_TOKEN" \
+     -d "{
+          \"message\": \"buildkite-agent $AGENT_VERSION\",
+          \"sha\": \"$MASTER_FORMULA_SHA\",
+          \"content\": \"$UPDATED_FORMULA_BASE64\",
+          \"branch\": \"master\"
+        }" \
+     -i
