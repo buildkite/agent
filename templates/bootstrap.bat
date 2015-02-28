2c89c22d74963687f7f019b5d1c27120ac4dc9d5
Tim Lucas <t@toolmantim.com>
2015-02-28 12:50:42 +1100

    Fix Windows buildbox-agent references

--- templates/bootstrap.bat
+++ templates/bootstrap.bat
@@ -7,36 +7,36 @@ echo --- Creating Build Environment
 
 REM Returns the location of this file
 
-SET BUILDBOX_DIR=%~dp0
+SET BUILDKITE_DIR=%~dp0
 
-REM Add the BUILDBOX_DIR to the PATH
+REM Add the BUILDKITE_DIR to the PATH
 
-SET PATH=%PATH%;%BUILDBOX_DIR%
+SET PATH=%PATH%;%BUILDKITE_DIR%
 
 REM Create the build directory
 
-SET SANITIED_PROJECT_SLUG=%BUILDBOX_PROJECT_SLUG:/=\%
-SET BUILDBOX_BUILD_DIR=%BUILDBOX_DIR%%BUILDBOX_AGENT_NAME%\%SANITIED_PROJECT_SLUG%
+SET SANITIED_PROJECT_SLUG=%BUILDKITE_PROJECT_SLUG:/=\%
+SET BUILDKITE_BUILD_DIR=%BUILDKITE_DIR%%BUILDKITE_AGENT_NAME%\%SANITIED_PROJECT_SLUG%
 
-IF NOT EXIST %BUILDBOX_BUILD_DIR% (
+IF NOT EXIST %BUILDKITE_BUILD_DIR% (
   REM Create the build directory
 
-  ECHO ^> MKDIR %BUILDBOX_BUILD_DIR%
-  MKDIR %BUILDBOX_BUILD_DIR%
+  ECHO ^> MKDIR %BUILDKITE_BUILD_DIR%
+  MKDIR %BUILDKITE_BUILD_DIR%
   IF %ERRORLEVEL% NEQ 0 EXIT %ERRORLEVEL%
 )
 
 REM Move to the build directory
 
-ECHO ^> CD %BUILDBOX_BUILD_DIR%
-CD %BUILDBOX_BUILD_DIR%
+ECHO ^> CD %BUILDKITE_BUILD_DIR%
+CD %BUILDKITE_BUILD_DIR%
 IF %ERRORLEVEL% NEQ 0 EXIT %ERRORLEVEL%
 
 REM Do we need to do a git checkout?
 
 IF NOT EXIST ".git" (
-  ECHO ^> git clone %BUILDBOX_REPO%
-  CALL git clone "%BUILDBOX_REPO%" . -v
+  ECHO ^> git clone %BUILDKITE_REPO%
+  CALL git clone "%BUILDKITE_REPO%" . -v
   IF %ERRORLEVEL% NEQ 0 EXIT %ERRORLEVEL%
 )
 
@@ -54,28 +54,28 @@ IF %ERRORLEVEL% NEQ 0 EXIT %ERRORLEVEL%
 
 REM Only reset to the branch if we're not on a tag
 
-IF "%BUILDBOX_TAG%" == "" (
-  ECHO ^> git reset --hard origin/%BUILDBOX_BRANCH%
-  CALL git reset --hard origin/%BUILDBOX_BRANCH%
+IF "%BUILDKITE_TAG%" == "" (
+  ECHO ^> git reset --hard origin/%BUILDKITE_BRANCH%
+  CALL git reset --hard origin/%BUILDKITE_BRANCH%
   IF %ERRORLEVEL% NEQ 0 EXIT %ERRORLEVEL%
 )
 
-ECHO ^> git checkout -qf "%BUILDBOX_COMMIT%"
-CALL git checkout -qf "%BUILDBOX_COMMIT%"
+ECHO ^> git checkout -qf "%BUILDKITE_COMMIT%"
+CALL git checkout -qf "%BUILDKITE_COMMIT%"
 IF %ERRORLEVEL% NEQ 0 EXIT %ERRORLEVEL%
 
 ECHO --- Running Build Script
 
-IF "%BUILDBOX_SCRIPT_PATH%" == "" (
+IF "%BUILDKITE_SCRIPT_PATH%" == "" (
   echo ERROR: No script path has been set for this project. Please go to \"Project Settings\" and add the path to your build script
   exit 1
 ) ELSE (
-  ECHO ^> CALL %BUILDBOX_SCRIPT_PATH%
-  CALL %BUILDBOX_SCRIPT_PATH%
+  ECHO ^> CALL %BUILDKITE_SCRIPT_PATH%
+  CALL %BUILDKITE_SCRIPT_PATH%
   SET EXIT_STATUS=%ERRORLEVEL%
 )
 
-IF NOT "%BUILDBOX_ARTIFACT_PATHS%" == "" (
+IF NOT "%BUILDKITE_ARTIFACT_PATHS%" == "" (
   REM If you want to upload artifacts to your own server, uncomment the lines below
   REM and replace the AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY with keys to your
   REM own bucket.
@@ -83,16 +83,16 @@ IF NOT "%BUILDBOX_ARTIFACT_PATHS%" == "" (
   REM SET AWS_SECRET_ACCESS_KEY=yyy
   REM SET AWS_ACCESS_KEY_ID=xxx
   REM SET AWS_S3_ACL=private
-  REM call buildbox-agent artifact upload "%BUILDBOX_ARTIFACT_PATHS%" "s3://name-of-your-s3-bucket/%BUILDBOX_JOB_ID%"
+  REM call buildkite-agent artifact upload "%BUILDKITE_ARTIFACT_PATHS%" "s3://name-of-your-s3-bucket/%BUILDKITE_JOB_ID%"
 
   REM Show the output of the artifact uploder when in debug mode
-  IF "%BUILDBOX_AGENT_DEBUG%" == "true" (
+  IF "%BUILDKITE_AGENT_DEBUG%" == "true" (
     ECHO --- Uploading Artifacts
-    ECHO ^> %BUILDBOX_DIR%\buildbox-agent artifact upload "%BUILDBOX_ARTIFACT_PATHS%"
-    call %BUILDBOX_DIR%\buildbox-agent artifact upload "%BUILDBOX_ARTIFACT_PATHS%"
+    ECHO ^> %BUILDKITE_DIR%\buildkite-agent artifact upload "%BUILDKITE_ARTIFACT_PATHS%"
+    call %BUILDKITE_DIR%\buildkite-agent artifact upload "%BUILDKITE_ARTIFACT_PATHS%"
     IF %ERRORLEVEL% NEQ 0 EXIT %ERRORLEVEL%
   ) ELSE (
-    call %BUILDBOX_DIR%\buildbox-agent artifact upload "%BUILDBOX_ARTIFACT_PATHS%" > nul 2>&1
+    call %BUILDKITE_DIR%\buildkite-agent artifact upload "%BUILDKITE_ARTIFACT_PATHS%" > nul 2>&1
     IF %ERRORLEVEL% NEQ 0 EXIT %ERRORLEVEL%
   )
 )
