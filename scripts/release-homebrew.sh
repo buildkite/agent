8bc54d465ab02fa40ad4e105763b25d29f95a2ae
Tim Lucas <t@toolmantim.com>
2015-03-18 16:04:27 +1100

    Add some debug info to release step

--- scripts/release-homebrew.sh
+++ scripts/release-homebrew.sh
@@ -23,24 +23,22 @@ function parse_json {
 }
 
 BINARY_NAME=buildkite-agent-darwin-386.tar.gz
-RELEASES_DIR=releases
-RELEASE_ARTIFACT_PATH="$RELEASES_DIR/$BINARY_NAME"
 
 DOWNLOAD_URL="https://github.com/buildkite/agent/releases/download/v$GITHUB_RELEASE_VERSION/$BINARY_NAME"
 FORMULA_FILE=./pkg/buildkite-agent.rb
 UPDATED_FORMULA_FILE=./pkg/buildkite-agent-updated.rb
 
-echo "--- :package: Downloading artifacts from Github Release step"
+echo "--- :package: Fetching artifact SHA from Github release step"
 
-echo "Release download URL: $DOWNLOAD_URL"
-
-echo "Fetching release artifact"
-buildkite-agent artifact download $RELEASE_ARTIFACT_PATH $RELEASES_DIR
-RELEASE_SHA=($(shasum $RELEASE_ARTIFACT_PATH))
+RELEASE_SHA=($(buildkite-agent artifact shasum "releases/$BINARY_NAME"))
 
 echo "Release SHA1: $RELEASE_SHA"
 
-echo "--- :github: Fetching current homebrew formula from Github Contents API"
+INVALID_SHA=$(buildkite-agent artifact shasum invalid_artifact.gz)
+
+echo "Invalid SHA: $INVALID_SHA"
+
+echo "--- :octocat: Fetching current homebrew formula from Github Contents API"
 
 CONTENTS_API_RESPONSE=$(curl "https://api.github.com/repos/buildkite/homebrew-buildkite/contents/buildkite-agent.rb?access_token=$GITHUB_RELEASE_ACCESS_TOKEN")
 
@@ -49,6 +47,11 @@ echo "Base64 decoding Github response into $FORMULA_FILE"
 echo $CONTENTS_API_RESPONSE | parse_json '["content"]' | base64 -d > $FORMULA_FILE
 
 echo "--- :ruby: Updating formula file"
+
+echo "Homebrew release type: $BREW_RELEASE_TYPE"
+echo "Homebrew release version: $GITHUB_RELEASE_VERSION"
+echo "Homebrew release download URL: $DOWNLOAD_URL"
+echo "Homebrew release download SHA: $RELEASE_SHA"
 
 cat $FORMULA_FILE |
   ./scripts/utils/update-homebrew-formula.rb $BREW_RELEASE_TYPE $GITHUB_RELEASE_VERSION $DOWNLOAD_URL $RELEASE_SHA \
@@ -62,7 +65,6 @@ MASTER_FORMULA_SHA=$(echo $CONTENTS_API_RESPONSE | parse_json '["sha"]')
 echo "Old formula SHA: $MASTER_FORMULA_SHA"
 
 echo "Posting JSON to Github Contents API"
-
 curl -X PUT "https://api.github.com/repos/buildkite/homebrew-buildkite/contents/buildkite-agent.rb?access_token=$GITHUB_RELEASE_ACCESS_TOKEN" \
      -i \
      --fail \
