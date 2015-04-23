6c56fa56afccc3a5a20015b974571a6801f09144
Keith Pitt <me@keithpitt.com>
2015-04-23 08:31:53 +0200

    Bumping to 1.0-beta.26

--- buildkite/version.go
+++ buildkite/version.go
@@ -9,7 +9,7 @@ import "strings"
 //
 // On CI, the binaries are always build with the buildVersion variable set.
 
-var baseVersion string = "1.0-beta.25"
+var baseVersion string = "1.0-beta.26"
 var buildVersion string = ""
 
 func Version() string {
