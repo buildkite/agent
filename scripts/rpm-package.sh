aa15704508d34bcfda6271a9ccc927e929399a04
Keith Pitt <me@keithpitt.com>
2015-04-23 11:25:00 +0200

    Added some scripts to build RPM packages

---
+++ scripts/rpm-package.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+
+echo $PATH
+whoami
+env
+
+if [[ "$CODENAME" == "" ]]; then
+  echo "Error: Missing \$CODENAME (stable or unstable)"
+  exit 1
+fi
+
+function build() {
+  echo "--- Building rpm package $1/$2"
+
+  BINARY_FILENAME="pkg/buildkite-agent-$1-$2"
+
+  # Download the built binary artifact
+  buildkite-agent artifact download $BINARY_FILENAME . --job ""
+
+  # Make sure it's got execute permissions so we can extract the version out of it
+  chmod +x $BINARY_FILENAME
+
+  # Build the rpm package using the architectre and binary, they are saved to rpm/
+  ./scripts/utils/build-rpm-package.sh $2 $BINARY_FILENAME rpm
+}
+
+function publish() {
+  echo "+++ Shipping $1"
+
+  ./scripts/utils/publish-rpm-package.sh $1 $CODENAME
+}
+
+# Export the function so we can use it in xargs
+export -f publish
+
+echo '--- Installing dependencies'
+bundle
+
+# Make sure we have a clean rpm folder
+rm -rf rpm
+
+# Build the packages into rpm/
+build "linux" "amd64"
+build "linux" "386"
+
+# Loop over all the .rpm files and publish them
+# ls rpm/*.rpm | xargs -I {} bash -c "publish {}"
