e04af75e444273a64607196f41c650d1e0fc20e1
Keith Pitt <me@keithpitt.com>
2015-05-18 23:52:23 +0200

    Install the right system service depending on what's installed on the current system.

--- packaging/linux/scripts/after-install.sh
+++ packaging/linux/scripts/after-install.sh
@@ -1,1 +1,46 @@
-true
+#!/bin/sh
+
+set -e
+
+cat <<"TXT"
+ _           _ _     _ _    _ _                                _
+| |         (_) |   | | |  (_) |                              | |
+| |__  _   _ _| | __| | | ___| |_ ___    __ _  __ _  ___ _ __ | |_
+| '_ \| | | | | |/ _` | |/ / | __/ _ \  / _` |/ _` |/ _ \ '_ \| __|
+| |_) | |_| | | | (_| |   <| | ||  __/ | (_| | (_| |  __/ | | | |_
+|_.__/ \__,_|_|_|\__,_|_|\_\_|\__\___|  \__,_|\__, |\___|_| |_|\__|
+                                               __/ |
+                                              |___/
+
+TXT
+
+if command -v systemctl > /dev/null; then
+  # Install systemd service
+  cp /usr/share/buildkite-agent/systemd/buildkite-agent.service /lib/systemd/system/buildkite-agent.service
+  START_COMMAND="sudo systemctl enable buildkite-agent && sudo systemctl start buildkite-agent"
+elif [ -d /etc/init ]; then
+  # Install upstart service
+  cp -r /usr/share/buildkite-agent/upstart/buildkite-agent.conf /etc/init/buildkite-agent.conf
+  START_COMMAND="sudo service buildkite-agent start"
+elif [ -d /etc/init.d ]; then
+  # Install old-school init.d script
+  cp -r /usr/share/buildkite-agent/lsb/buildkite-agent.conf /etc/init.d/buildkite-agent
+  START_COMMAND="sudo /etc/init.d/buildkite-agent start"
+else
+  # If all the others fails, warn them and just let them run it the old
+  # fasioned way.
+  echo "============================== WARNING ==================================="
+  echo ""
+  echo "The Buildkite Agent could not find a suitable system service to install."
+  echo "Please open an issue at https://github.com/buildkite/agent and let us know"
+  echo ""
+  echo "=========================================================================="
+  echo ""
+
+  START_COMMAND="sudo /usr/bin/buildkite-agent start"
+fi
+
+echo "You now need to add your agent token to \"/etc/buildkite-agent/buildkite-agent.cfg\""
+echo "and then you can start your agent by running \"$START_COMMAND\""
+
+exit 0
