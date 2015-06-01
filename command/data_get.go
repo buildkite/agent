de26ce5093e35cfb3b7a04cb1996c414bcadbd47
Keith Pitt <me@keithpitt.com>
2015-06-01 21:20:27 +0200

    Meta data get doesn't require the value arg.

--- command/data_get.go
+++ command/data_get.go
@@ -13,7 +13,7 @@ func DataGetCommandAction(context *cli.Context) {
 	}.Setup()
 
 	c.Require("endpoint", "agent-access-token", "job")
-	c.RequireArgs("key", "value")
+	c.RequireArgs("key")
 
 	var metaData = buildkite.MetaData{
 		API: buildkite.API{
