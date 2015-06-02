829150942401977a103ea4fde964b5be88219444
Keith Pitt <me@keithpitt.com>
2015-06-02 21:57:47 +0200

    Updated the artifact download command structure.

--- command/artifact_download.go
+++ command/artifact_download.go
@@ -7,6 +7,32 @@ import (
 	"github.com/codegangsta/cli"
 )
 
+var DownloadHelpDescription = `Usage:
+
+   buildkite-agent artifact download [arguments...]
+
+Description:
+
+   Downloads artifacts from Buildkite to the local machine.
+
+   Note: You need to ensure that your search query is surrounded by quotes if
+   using a wild card as the built-in shell path globbing will provide files,
+   which will break the download.
+
+Example:
+
+   $ buildkite-agent artifact download "pkg/*.tar.gz" . --build xxx
+
+   This will search across all the artifacts for the build with files that match that part.
+   The first argument is the search query, and the second argument is the download destination.
+
+   If you're trying to download a specific file, and there are multiple artifacts from different
+   jobs, you can target the particular job you want to download the artifact from:
+
+   $ buildkite-agent artifact download "pkg/*.tar.gz" . --step "tests" --build xxx
+
+   You can also use the step's jobs id (provided by the environment variable $BUILDKITE_JOB_ID)`
+
 type ArtifactDownloadConfig struct {
 	Query            string `cli:"arg:0" label:"artifact search query" validate:"required"`
 	Destination      string `cli:"arg:1" label:"artifact download path" validate:"required"`
@@ -19,32 +45,77 @@ type ArtifactDownloadConfig struct {
 	Debug            bool   `cli:"debug"`
 }
 
-func ArtifactDownloadCommandAction(c *cli.Context) {
-	// The configuration will be loaded into this struct
-	cfg := ArtifactDownloadConfig{}
-
-	// Load the configuration
-	if err := config.Load(c, &cfg); err != nil {
-		logger.Fatal("%s", err)
-	}
-
-	// Setup the any global configuration options
-	SetupGlobalConfiguration(cfg)
-
-	// Setup the downloader
-	downloader := buildkite.ArtifactDownloader{
-		API: buildkite.API{
-			Endpoint: cfg.Endpoint,
-			Token:    cfg.AgentAccessToken,
+var ArtifactDownloadCommand = cli.Command{
+	Name:        "download",
+	Usage:       "Downloads artifacts from Buildkite to the local machine",
+	Description: DownloadHelpDescription,
+	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:  "job",
+			Value: "",
+			Usage: "DEPRECATED",
 		},
-		Query:       cfg.Query,
-		Destination: cfg.Destination,
-		BuildID:     cfg.Build,
-		Step:        cfg.Step,
-	}
+		cli.StringFlag{
+			Name:  "step",
+			Value: "",
+			Usage: "Scope the search to a paticular step by using either it's name of job ID",
+		},
+		cli.StringFlag{
+			Name:   "build",
+			Value:  "",
+			EnvVar: "BUILDKITE_BUILD_ID",
+			Usage:  "The build that the artifacts were uploaded to",
+		},
+		cli.StringFlag{
+			Name:   "agent-access-token",
+			Value:  "",
+			Usage:  "The access token used to identify the agent",
+			EnvVar: "BUILDKITE_AGENT_ACCESS_TOKEN",
+		},
+		cli.StringFlag{
+			Name:   "endpoint",
+			Value:  DefaultEndpoint,
+			Usage:  "The Agent API endpoint",
+			EnvVar: "BUILDKITE_AGENT_ENDPOINT",
+		},
+		cli.BoolFlag{
+			Name:   "debug",
+			Usage:  "Enable debug mode",
+			EnvVar: "BUILDKITE_AGENT_DEBUG",
+		},
+		cli.BoolFlag{
+			Name:   "no-color",
+			Usage:  "Don't show colors in logging",
+			EnvVar: "BUILDKITE_AGENT_NO_COLOR",
+		},
+	},
+	Action: func(c *cli.Context) {
+		// The configuration will be loaded into this struct
+		cfg := ArtifactDownloadConfig{}
 
-	// Download the artifacts
-	if err := downloader.Download(); err != nil {
-		logger.Fatal("Failed to download artifacts: %s", err)
-	}
+		// Load the configuration
+		if err := config.Load(c, &cfg); err != nil {
+			logger.Fatal("%s", err)
+		}
+
+		// Setup the any global configuration options
+		SetupGlobalConfiguration(cfg)
+
+		// Setup the downloader
+		downloader := buildkite.ArtifactDownloader{
+			API: buildkite.API{
+				Endpoint: cfg.Endpoint,
+				Token:    cfg.AgentAccessToken,
+			},
+			Query:       cfg.Query,
+			Destination: cfg.Destination,
+			BuildID:     cfg.Build,
+			Step:        cfg.Step,
+		}
+
+		// Download the artifacts
+		if err := downloader.Download(); err != nil {
+			logger.Fatal("Failed to download artifacts: %s", err)
+		}
+	},
 }
--- commands.go
+++ commands.go
@@ -51,32 +51,6 @@ Example:
 
    You can also use the step's job id (provided by the environment variable $BUILDKITE_JOB_ID)`
 
-var DownloadHelpDescription = `Usage:
-
-   buildkite-agent artifact download [arguments...]
-
-Description:
-
-   Downloads artifacts from Buildkite to the local machine.
-
-   Note: You need to ensure that your search query is surrounded by quotes if
-   using a wild card as the built-in shell path globbing will provide files,
-   which will break the download.
-
-Example:
-
-   $ buildkite-agent artifact download "pkg/*.tar.gz" . --build xxx
-
-   This will search across all the artifacts for the build with files that match that part.
-   The first argument is the search query, and the second argument is the download destination.
-
-   If you're trying to download a specific file, and there are multiple artifacts from different
-   jobs, you can target the particular job you want to download the artifact from:
-
-   $ buildkite-agent artifact download "pkg/*.tar.gz" . --step "tests" --build xxx
-
-   You can also use the step's jobs id (provided by the environment variable $BUILDKITE_JOB_ID)`
-
 var SetHelpDescription = `Usage:
 
    buildkite-agent meta-data set <key> <value> [arguments...]
@@ -200,53 +174,8 @@ func init() {
 			Name:  "artifact",
 			Usage: "Upload/download artifacts from Buildkite jobs",
 			Subcommands: []cli.Command{
-				{
-					Name:        "download",
-					Usage:       "Downloads artifacts from Buildkite to the local machine",
-					Description: DownloadHelpDescription,
-					Flags: []cli.Flag{
-						cli.StringFlag{
-							Name:  "job",
-							Value: "",
-							Usage: "DEPRECATED",
-						},
-						cli.StringFlag{
-							Name:  "step",
-							Value: "",
-							Usage: "Scope the search to a paticular step by using either it's name of job ID",
-						},
-						cli.StringFlag{
-							Name:   "build",
-							Value:  "",
-							EnvVar: "BUILDKITE_BUILD_ID",
-							Usage:  "The build that the artifacts were uploaded to",
-						},
-						cli.StringFlag{
-							Name:   "agent-access-token",
-							Value:  "",
-							Usage:  "The access token used to identify the agent",
-							EnvVar: "BUILDKITE_AGENT_ACCESS_TOKEN",
-						},
-						cli.StringFlag{
-							Name:   "endpoint",
-							Value:  DefaultEndpoint,
-							Usage:  "The Agent API endpoint",
-							EnvVar: "BUILDKITE_AGENT_ENDPOINT",
-						},
-						cli.BoolFlag{
-							Name:   "debug",
-							Usage:  "Enable debug mode",
-							EnvVar: "BUILDKITE_AGENT_DEBUG",
-						},
-						cli.BoolFlag{
-							Name:   "no-color",
-							Usage:  "Don't show colors in logging",
-							EnvVar: "BUILDKITE_AGENT_NO_COLOR",
-						},
-					},
-					Action: command.ArtifactDownloadCommandAction,
-				},
 				command.ArtifactUploadCommand,
+				command.ArtifactDownloadCommand,
 				{
 					Name:        "shasum",
 					Usage:       "Prints the SHA-1 checksum for the artifact provided to STDOUT",
