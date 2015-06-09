3b1b10d7a1d4df66483fece15d7ec8b0d3771ab5
Keith Pitt <me@keithpitt.com>
2015-06-09 20:05:25 +0200

    Added a `meta-data exists` command.

--- api/meta_data.go
+++ api/meta_data.go
@@ -16,6 +16,12 @@ type MetaData struct {
 	Value string `json:"value,omitempty"`
 }
 
+// MetaDataExists represents a Buildkite Agent API MetaData Exists check
+// response
+type MetaDataExists struct {
+	Exists bool `json:"exists"`
+}
+
 // Sets the meta data value
 func (ps *MetaDataService) Set(jobId string, metaData *MetaData) (*MetaData, *Response, error) {
 	u := fmt.Sprintf("v2/jobs/%s/data/set", jobId)
@@ -51,4 +57,23 @@ func (ps *MetaDataService) Get(jobId string, key string) (*MetaData, *Response, 
 	}
 
 	return metaData, resp, err
+}
+
+// Returns true if the meta data key has been set, false if it hasn't.
+func (ps *MetaDataService) Exists(jobId string, key string) (*MetaDataExists, *Response, error) {
+	u := fmt.Sprintf("v2/jobs/%s/data/exists", jobId)
+	m := &MetaData{Key: key}
+
+	req, err := ps.client.NewRequest("POST", u, m)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	e := new(MetaDataExists)
+	resp, err := ps.client.Do(req, e)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return e, resp, err
 }
---
+++ clicommand/meta_data_exists.go
@@ -0,0 +1,78 @@
+package clicommand
+
+import (
+	"github.com/buildkite/agent/agent"
+	"github.com/buildkite/agent/cliconfig"
+	"github.com/buildkite/agent/logger"
+	"github.com/codegangsta/cli"
+	"os"
+)
+
+var MetaDataExistsHelpDescription = `Usage:
+
+   buildkite-agent meta-data exists <key> [arguments...]
+
+Description:
+
+   The command exits with a status of 0 if the key has been set, 1 if it
+   hasn't.
+
+Example:
+
+   $ buildkite-agent meta-data exists "foo"`
+
+type MetaDataExistsConfig struct {
+	Key              string `cli:"arg:0" label:"meta-data key" validate:"required"`
+	Job              string `cli:"job" validate:"required"`
+	AgentAccessToken string `cli:"agent-access-token" validate:"required"`
+	Endpoint         string `cli:"endpoint" validate:"required"`
+	NoColor          bool   `cli:"no-color"`
+	Debug            bool   `cli:"debug"`
+}
+
+var MetaDataExistsCommand = cli.Command{
+	Name:        "exists",
+	Usage:       "Check to see if the meta data key exists for a build",
+	Description: MetaDataExistsHelpDescription,
+	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:   "job",
+			Value:  "",
+			Usage:  "Which job should the meta-data be checked for",
+			EnvVar: "BUILDKITE_JOB_ID",
+		},
+		AgentAccessTokenFlag,
+		EndpointFlag,
+		DebugFlag,
+		NoColorFlag,
+	},
+	Action: func(c *cli.Context) {
+		// The configuration will be loaded into this struct
+		cfg := MetaDataExistsConfig{}
+
+		// Load the configuration
+		if err := cliconfig.Load(c, &cfg); err != nil {
+			logger.Fatal("%s", err)
+		}
+
+		// Setup the any global configuration options
+		HandleGlobalFlags(cfg)
+
+		// Create the API client
+		client := agent.APIClient{
+			Endpoint: cfg.Endpoint,
+			Token:    cfg.AgentAccessToken,
+		}.Create()
+
+		// Find the meta data value
+		exists, _, err := client.MetaData.Exists(cfg.Job, cfg.Key)
+		if err != nil {
+			logger.Fatal("Failed to see if meta-data exists: %s", err)
+		}
+
+		// If the meta data didn't exist, exit with an error.
+		if !exists.Exists {
+			os.Exit(1)
+		}
+	},
+}
