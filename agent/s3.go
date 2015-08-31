35489d0c93c8a1d71ee203453a67d8ffaf2821ad
Keith Pitt <me@keithpitt.com>
2015-08-30 19:37:12 -0700

    Created a common way of creating an S3 client

--- agent/s3.go
+++ agent/s3.go
@@ -2,16 +2,23 @@ package agent
 
 import (
 	"errors"
+	"fmt"
 	"os"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/credentials/ec2rolecreds"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/buildkite/agent/logger"
 )
 
-type BuildkiteAWSCredentialsProvider struct{}
+type BuildkiteAWSCredentialsProvider struct {
+	retrieved bool
+}
 
-func (m *BuildkiteAWSCredentialsProvider) Retrieve() (creds credentials.Value, err error) {
+func (e *BuildkiteAWSCredentialsProvider) Retrieve() (creds credentials.Value, err error) {
+	e.retrieved = false
+
 	creds.AccessKeyID = os.Getenv("BUILDKITE_S3_ACCESS_KEY_ID")
 	if creds.AccessKeyID == "" {
 		creds.AccessKeyID = os.Getenv("BUILDKITE_S3_ACCESS_KEY")
@@ -29,12 +36,13 @@ func (m *BuildkiteAWSCredentialsProvider) Retrieve() (creds credentials.Value, e
 		err = errors.New("BUILDKITE_S3_SECRET_ACCESS_KEY or BUILDKITE_S3_SECRET_KEY not found in environment")
 	}
 
+	e.retrieved = true
+
 	return
 }
 
-// The custom BUILDKITE_ env vars never expire
-func (m *BuildkiteAWSCredentialsProvider) IsExpired() bool {
-	return false
+func (e *BuildkiteAWSCredentialsProvider) IsExpired() bool {
+	return !e.retrieved
 }
 
 func awsS3Credentials() *credentials.Credentials {
@@ -44,6 +52,34 @@ func awsS3Credentials() *credentials.Credentials {
 			&credentials.EnvProvider{},
 			&ec2rolecreds.EC2RoleProvider{},
 		})
+}
+
+func newS3Client(bucket string) (*s3.S3, error) {
+	// Generate the AWS config used by the S3 client
+	region := awsS3RegionFromEnv()
+	config := &aws.Config{Credentials: awsS3Credentials(), Region: aws.String(region)}
+
+	logger.Debug("Authorizing S3 credentials and finding bucket `%s` in region `%s`...", bucket, region)
+
+	// Create the S3 client
+	s3client := s3.New(config)
+
+	// Test the authentication by trying to list the first 0 objects in the
+	// bucket.
+	params := &s3.ListObjectsInput{
+		Bucket:  aws.String(bucket),
+		MaxKeys: aws.Int64(0),
+	}
+	_, err := s3client.ListObjects(params)
+	if err != nil {
+		if err == credentials.ErrNoValidProvidersFoundInChain {
+			return nil, fmt.Errorf("Could not find a valid authentication strategy to connect to S3. Try setting BUILDKITE_S3_ACCESS_KEY and BUILDKITE_S3_SECRET_KEY")
+		}
+
+		return nil, fmt.Errorf("Failed to authenticate to bucket `%s` in region `%s` (%s)", bucket, region, err.Error())
+	}
+
+	return s3client, nil
 }
 
 func awsS3RegionFromEnv() string {
