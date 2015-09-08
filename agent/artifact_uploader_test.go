57e673c708e4723eef2abd240cb666ce797267c4
Keith Pitt <me@keithpitt.com>
2015-09-08 10:29:58 -0700

    Fixed artifact uploader test.

--- agent/artifact_uploader_test.go
+++ agent/artifact_uploader_test.go
@@ -3,11 +3,23 @@ package agent
 import (
 	"fmt"
 	"os"
+	"path"
 	"path/filepath"
 	"testing"
 
+	"github.com/buildkite/agent/api"
 	"github.com/stretchr/testify/assert"
 )
+
+func findArtifact(artifacts []*api.Artifact, search string) *api.Artifact {
+	for _, a := range artifacts {
+		if path.Base(a.Path) == search {
+			return a
+		}
+	}
+
+	return nil
+}
 
 func TestCollect(t *testing.T) {
 	wd, _ := os.Getwd()
@@ -22,30 +34,40 @@ func TestCollect(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, len(artifacts), 4)
 
-	assert.Equal(t, artifacts[0].Path, "test/fixtures/artifacts/Mr Freeze.jpg")
-	assert.Equal(t, artifacts[0].AbsolutePath, filepath.Join(root, "test/fixtures/artifacts/Mr Freeze.jpg"))
-	assert.Equal(t, artifacts[0].GlobPath, "test/fixtures/artifacts/**/*.jpg")
-	assert.Equal(t, int(artifacts[0].FileSize), 362371)
-	assert.Equal(t, artifacts[0].Sha1Sum, "f5bc7bc9f5f9c3e543dde0eb44876c6f9acbfb6b")
+	var a *api.Artifact
 
-	assert.Equal(t, artifacts[1].Path, "test/fixtures/artifacts/folder/Commando.jpg")
-	assert.Equal(t, artifacts[1].AbsolutePath, filepath.Join(root, "test/fixtures/artifacts/folder/Commando.jpg"))
-	assert.Equal(t, artifacts[1].GlobPath, "test/fixtures/artifacts/**/*.jpg")
-	assert.Equal(t, int(artifacts[1].FileSize), 113000)
-	assert.Equal(t, artifacts[1].Sha1Sum, "811d7cb0317582e22ebfeb929d601cdabea4b3c0")
+	a = findArtifact(artifacts, "Mr Freeze.jpg")
+	assert.NotNil(t, a)
+	assert.Equal(t, a.Path, "test/fixtures/artifacts/Mr Freeze.jpg")
+	assert.Equal(t, a.AbsolutePath, filepath.Join(root, "test/fixtures/artifacts/Mr Freeze.jpg"))
+	assert.Equal(t, a.GlobPath, "test/fixtures/artifacts/**/*.jpg")
+	assert.Equal(t, int(a.FileSize), 362371)
+	assert.Equal(t, a.Sha1Sum, "f5bc7bc9f5f9c3e543dde0eb44876c6f9acbfb6b")
 
-	assert.Equal(t, artifacts[2].Path, "test/fixtures/artifacts/this is a folder with a space/The Terminator.jpg")
-	assert.Equal(t, artifacts[2].AbsolutePath, filepath.Join(root, "test/fixtures/artifacts/this is a folder with a space/The Terminator.jpg"))
-	assert.Equal(t, artifacts[2].GlobPath, "test/fixtures/artifacts/**/*.jpg")
-	assert.Equal(t, int(artifacts[2].FileSize), 47301)
-	assert.Equal(t, artifacts[2].Sha1Sum, "ed76566ede9cb6edc975fcadca429665aad8785a")
+	a = findArtifact(artifacts, "Commando.jpg")
+	assert.NotNil(t, a)
+	assert.Equal(t, a.Path, "test/fixtures/artifacts/folder/Commando.jpg")
+	assert.Equal(t, a.AbsolutePath, filepath.Join(root, "test/fixtures/artifacts/folder/Commando.jpg"))
+	assert.Equal(t, a.GlobPath, "test/fixtures/artifacts/**/*.jpg")
+	assert.Equal(t, int(a.FileSize), 113000)
+	assert.Equal(t, a.Sha1Sum, "811d7cb0317582e22ebfeb929d601cdabea4b3c0")
+
+	a = findArtifact(artifacts, "The Terminator.jpg")
+	assert.NotNil(t, a)
+	assert.Equal(t, a.Path, "test/fixtures/artifacts/this is a folder with a space/The Terminator.jpg")
+	assert.Equal(t, a.AbsolutePath, filepath.Join(root, "test/fixtures/artifacts/this is a folder with a space/The Terminator.jpg"))
+	assert.Equal(t, a.GlobPath, "test/fixtures/artifacts/**/*.jpg")
+	assert.Equal(t, int(a.FileSize), 47301)
+	assert.Equal(t, a.Sha1Sum, "ed76566ede9cb6edc975fcadca429665aad8785a")
 
 	// Need to trim the first charcater because it's path doesn't contain
 	// the root, which in this case is /
+	a = findArtifact(artifacts, "Smile.gif")
+	assert.NotNil(t, a)
 	gifPath := filepath.Join(root, "test/fixtures/artifacts/gifs/Smile.gif")
-	assert.Equal(t, artifacts[3].Path, gifPath[1:len(gifPath)])
-	assert.Equal(t, artifacts[3].AbsolutePath, filepath.Join(root, "test/fixtures/artifacts/gifs/Smile.gif"))
-	assert.Equal(t, artifacts[3].GlobPath, filepath.Join(root, "test/fixtures/artifacts/**/*.gif"))
-	assert.Equal(t, int(artifacts[3].FileSize), 2038453)
-	assert.Equal(t, artifacts[3].Sha1Sum, "bd4caf2e01e59777744ac1d52deafa01c2cb9bfd")
+	assert.Equal(t, a.Path, gifPath[1:len(gifPath)])
+	assert.Equal(t, a.AbsolutePath, filepath.Join(root, "test/fixtures/artifacts/gifs/Smile.gif"))
+	assert.Equal(t, a.GlobPath, filepath.Join(root, "test/fixtures/artifacts/**/*.gif"))
+	assert.Equal(t, int(a.FileSize), 2038453)
+	assert.Equal(t, a.Sha1Sum, "bd4caf2e01e59777744ac1d52deafa01c2cb9bfd")
 }
