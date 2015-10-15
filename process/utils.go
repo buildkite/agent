117e90711132d4ab7a04ae8bdfe72afffbec9b60
Keith Pitt <me@keithpitt.com>
2015-10-15 18:42:55 +1030

    We don't need to `PrepareCommandProcess` anymore

--- process/utils.go
+++ process/utils.go
@@ -5,17 +5,10 @@ package process
 import (
 	"os"
 	"os/exec"
-	"syscall"
 
 	"github.com/kr/pty"
 )
 
 func StartPTY(c *exec.Cmd) (*os.File, error) {
 	return pty.Start(c)
-}
-
-func PrepareCommandProcess(p *Process) {
-	// Children of the forked process will inherit its process group
-	// This is to make sure that all grandchildren dies when this Process instance is killed
-	p.command.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
 }
