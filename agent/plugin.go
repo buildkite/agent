acf4f7d16ef6d5e0afec952b098790d396156b6d
Keith Pitt <me@keithpitt.com>
2015-10-18 15:39:40 +1030

    Added plugin data structures.

---
+++ agent/plugin.go
@@ -0,0 +1,193 @@
+package agent
+
+import (
+	"encoding/json"
+	"fmt"
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type Plugin struct {
+	// Where the plugin can be found (can either be a file system path, or
+	// a git repository)
+	Location string
+
+	// The version of the plugin that should be running
+	Version string
+
+	// Configuration for the plugin
+	Configuration map[string]interface{}
+}
+
+func CreatePlugin(location string, config map[string]interface{}) (*Plugin, error) {
+	plugin := &Plugin{Configuration: config}
+
+	// Extract the version from the location
+	parts := strings.Split(location, "#")
+	if len(parts) > 2 {
+		return nil, fmt.Errorf("Too many #'s in \"%s\"", location)
+	} else if len(parts) == 2 {
+		plugin.Location = parts[0]
+		plugin.Version = parts[1]
+	} else {
+		plugin.Location = location
+	}
+
+	return plugin, nil
+}
+
+// Given a JSON structure, convert it to an array of plugins
+func CreatePluginsFromJSON(j string) ([]*Plugin, error) {
+	// Parse the JSON
+	var f interface{}
+	err := json.Unmarshal([]byte(j), &f)
+	if err != nil {
+		return nil, err
+	}
+
+	// Try and convert the structure to an array
+	m, ok := f.([]interface{})
+	if !ok {
+		return nil, fmt.Errorf("JSON structure was not an array")
+	}
+
+	// Convert the JSON elements to plugins
+	plugins := []*Plugin{}
+	for _, v := range m {
+		switch vv := v.(type) {
+		case string:
+			// Add the plugin with no config to the array
+			plugin, err := CreatePlugin(string(vv), map[string]interface{}{})
+			if err != nil {
+				return nil, err
+			}
+			plugins = append(plugins, plugin)
+		case map[string]interface{}:
+			for location, config := range vv {
+				// Ensure the config is a hash
+				config, ok := config.(map[string]interface{})
+				if !ok {
+					return nil, fmt.Errorf("Configuration for \"%s\" is not a hash", location)
+				}
+
+				// Add the plugin with config to the array
+				plugin, err := CreatePlugin(string(location), config)
+				if err != nil {
+					return nil, err
+				}
+				plugins = append(plugins, plugin)
+			}
+		default:
+			return nil, fmt.Errorf("Unknown type in plugin definition (%s)", vv)
+		}
+	}
+
+	return plugins, nil
+}
+
+// Returns the name of the plugin
+func (p *Plugin) Name() string {
+	if p.Location != "" {
+		// Grab the last part of the location
+		parts := strings.Split(p.Location, "/")
+		name := parts[len(parts)-1]
+
+		// Clean up the name
+		name = strings.ToLower(name)
+		name = regexp.MustCompile(`\s+`).ReplaceAllString(name, " ")
+		name = regexp.MustCompile(`[^a-zA-Z0-9]`).ReplaceAllString(name, "-")
+
+		return name
+	} else {
+		return ""
+	}
+}
+
+// Returns and ID for the plugin that can be used as a folder name
+func (p *Plugin) Identifier() (string, error) {
+	nonIdCharacterRegex := regexp.MustCompile(`[^a-zA-Z0-9]`)
+	removeDoubleUnderscore := regexp.MustCompile(`-+`)
+
+	id := p.Location + "#" + p.Version
+	id = nonIdCharacterRegex.ReplaceAllString(id, "-")
+	id = removeDoubleUnderscore.ReplaceAllString(id, "-")
+	id = strings.TrimSuffix(id, "-")
+
+	return id, nil
+}
+
+// Returns the repository host where the code is stored
+func (p *Plugin) Repository() (string, error) {
+	if p.Location == "" {
+		return "", fmt.Errorf("Missing plugin location")
+	}
+
+	parts := strings.Split(p.Location, "/")
+	if len(parts) < 2 {
+		return "", fmt.Errorf("Incomplete plugin path \"%s\"", p.Location)
+	}
+
+	if parts[0] == "github.com" || parts[0] == "bitbucket.org" || parts[0] == "gitlab.com" {
+		if len(parts) < 3 {
+			return "", fmt.Errorf("Incomplete %s path \"%s\"", parts[0], p.Location)
+		}
+
+		return strings.Join(parts[:3], "/"), nil
+	} else {
+		repo := []string{}
+
+		for _, p := range parts {
+			repo = append(repo, p)
+
+			if strings.HasSuffix(p, ".git") {
+				break
+			}
+		}
+
+		return strings.Join(repo, "/"), nil
+	}
+
+	return "", nil
+}
+
+// Returns the subdirectory path that the plugin is in
+func (p *Plugin) RepositorySubdirectory() (string, error) {
+	repository, err := p.Repository()
+	if err != nil {
+		return "", err
+	}
+
+	dir := strings.TrimPrefix(p.Location, repository)
+
+	return strings.TrimPrefix(dir, "/"), nil
+}
+
+// Converts the plugin configuration values to environment variables
+func (p *Plugin) ConfigurationToEnv() []string {
+	env := []string{}
+
+	toDashRegex := regexp.MustCompile(`-|\s+`)
+	removeWhitespaceRegex := regexp.MustCompile(`\s+`)
+	removeDoubleUnderscore := regexp.MustCompile(`_+`)
+
+	for k, v := range p.Configuration {
+		k = removeWhitespaceRegex.ReplaceAllString(k, " ")
+		name := strings.ToUpper(toDashRegex.ReplaceAllString(fmt.Sprintf("BUILDKITE_PLUGIN_%s_%s", p.Name(), k), "_"))
+		name = removeDoubleUnderscore.ReplaceAllString(name, "_")
+
+		switch vv := v.(type) {
+		case string:
+			env = append(env, fmt.Sprintf("%s=%s", name, vv))
+		case int:
+			env = append(env, fmt.Sprintf("%s=%d", name, vv))
+		default:
+			// unknown type
+		}
+	}
+
+	// Sort them into a consistent order
+	sort.Strings(env)
+
+	return env
+}
---
+++ agent/plugin_test.go
@@ -0,0 +1,173 @@
+package agent
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreatePluginsFromJSON(t *testing.T) {
+	var plugins []*Plugin
+	var err error
+
+	plugins, err = CreatePluginsFromJSON(`[{"github.com/buildkite/plugins/docker-compose#a34fa34":{"container":"app"}}, "github.com/buildkite/plugins/ping#master"]`)
+	assert.Equal(t, len(plugins), 2)
+	assert.Nil(t, err)
+
+	assert.Equal(t, plugins[0].Location, "github.com/buildkite/plugins/docker-compose")
+	assert.Equal(t, plugins[0].Version, "a34fa34")
+	assert.Equal(t, plugins[0].Configuration, map[string]interface{}{"container": "app"})
+
+	assert.Equal(t, plugins[1].Location, "github.com/buildkite/plugins/ping")
+	assert.Equal(t, plugins[1].Version, "master")
+	assert.Equal(t, plugins[1].Configuration, map[string]interface{}{})
+
+	plugins, err = CreatePluginsFromJSON(`blah`)
+	assert.Equal(t, len(plugins), 0)
+	assert.NotNil(t, err)
+	assert.Equal(t, err.Error(), "invalid character 'b' looking for beginning of value")
+
+	plugins, err = CreatePluginsFromJSON(`{"foo": "bar"}`)
+	assert.Equal(t, len(plugins), 0)
+	assert.NotNil(t, err)
+	assert.Equal(t, err.Error(), "JSON structure was not an array")
+
+	plugins, err = CreatePluginsFromJSON(`["github.com/buildkite/plugins/ping#master#lololo"]`)
+	assert.Equal(t, len(plugins), 0)
+	assert.NotNil(t, err)
+	assert.Equal(t, err.Error(), "Too many #'s in \"github.com/buildkite/plugins/ping#master#lololo\"")
+}
+
+func TestPluginName(t *testing.T) {
+	var plugin *Plugin
+
+	plugin = &Plugin{Location: "github.com/buildkite/plugins/docker-compose"}
+	assert.Equal(t, plugin.Name(), "docker-compose")
+
+	plugin = &Plugin{Location: "github.com/buildkite/my-plugin"}
+	assert.Equal(t, plugin.Name(), "my-plugin")
+
+	plugin = &Plugin{Location: "~/Development/plugins/test"}
+	assert.Equal(t, plugin.Name(), "test")
+
+	plugin = &Plugin{Location: "~/Development/plugins/UPPER     CASE_party"}
+	assert.Equal(t, plugin.Name(), "upper-case-party")
+
+	plugin = &Plugin{Location: "vendor/src/vendored with a space"}
+	assert.Equal(t, plugin.Name(), "vendored-with-a-space")
+
+	plugin = &Plugin{Location: ""}
+	assert.Equal(t, plugin.Name(), "")
+}
+
+func TestIdentifier(t *testing.T) {
+	var plugin *Plugin
+	var id string
+	var err error
+
+	plugin = &Plugin{Location: "github.com/buildkite/plugins/docker-compose/beta#master"}
+	id, err = plugin.Identifier()
+	assert.Equal(t, id, "github-com-buildkite-plugins-docker-compose-beta-master")
+	assert.Nil(t, err)
+
+	plugin = &Plugin{Location: "github.com/buildkite/plugins/docker-compose/beta"}
+	id, err = plugin.Identifier()
+	assert.Equal(t, id, "github-com-buildkite-plugins-docker-compose-beta")
+	assert.Nil(t, err)
+
+	plugin = &Plugin{Location: "192.168.0.1/foo.git#12341234"}
+	id, err = plugin.Identifier()
+	assert.Equal(t, id, "192-168-0-1-foo-git-12341234")
+	assert.Nil(t, err)
+}
+
+func TestRepositoryAndSubdirectory(t *testing.T) {
+	var plugin *Plugin
+	var repo string
+	var sub string
+	var err error
+
+	plugin = &Plugin{Location: "github.com/buildkite/plugins/docker-compose/beta"}
+	repo, err = plugin.Repository()
+	assert.Equal(t, repo, "github.com/buildkite/plugins")
+	assert.Nil(t, err)
+	sub, err = plugin.RepositorySubdirectory()
+	assert.Equal(t, sub, "docker-compose/beta")
+	assert.Nil(t, err)
+
+	plugin = &Plugin{Location: "github.com/buildkite/test-plugin"}
+	repo, err = plugin.Repository()
+	assert.Equal(t, repo, "github.com/buildkite/test-plugin")
+	assert.Nil(t, err)
+	sub, err = plugin.RepositorySubdirectory()
+	assert.Equal(t, sub, "")
+	assert.Nil(t, err)
+
+	plugin = &Plugin{Location: "github.com/buildkite"}
+	repo, err = plugin.Repository()
+	assert.Equal(t, repo, "")
+	assert.NotNil(t, err)
+	assert.Equal(t, err.Error(), `Incomplete github.com path "github.com/buildkite"`)
+	sub, err = plugin.RepositorySubdirectory()
+	assert.Equal(t, sub, "")
+	assert.NotNil(t, err)
+	assert.Equal(t, err.Error(), `Incomplete github.com path "github.com/buildkite"`)
+
+	plugin = &Plugin{Location: "bitbucket.org/buildkite"}
+	repo, err = plugin.Repository()
+	assert.Equal(t, repo, "")
+	assert.NotNil(t, err)
+	assert.Equal(t, err.Error(), `Incomplete bitbucket.org path "bitbucket.org/buildkite"`)
+	sub, err = plugin.RepositorySubdirectory()
+	assert.Equal(t, sub, "")
+	assert.NotNil(t, err)
+	assert.Equal(t, err.Error(), `Incomplete bitbucket.org path "bitbucket.org/buildkite"`)
+
+	plugin = &Plugin{Location: "bitbucket.org/user/project/sub/directory"}
+	repo, err = plugin.Repository()
+	assert.Equal(t, repo, "bitbucket.org/user/project")
+	assert.Nil(t, err)
+	sub, err = plugin.RepositorySubdirectory()
+	assert.Equal(t, sub, "sub/directory")
+	assert.Nil(t, err)
+
+	plugin = &Plugin{Location: "114.135.234.212/foo.git"}
+	repo, err = plugin.Repository()
+	assert.Equal(t, repo, "114.135.234.212/foo.git")
+	assert.Nil(t, err)
+	sub, err = plugin.RepositorySubdirectory()
+	assert.Equal(t, sub, "")
+	assert.Nil(t, err)
+
+	plugin = &Plugin{Location: "github.com/buildkite/plugins/docker-compose/beta"}
+	repo, err = plugin.Repository()
+	assert.Equal(t, repo, "github.com/buildkite/plugins")
+	assert.Nil(t, err)
+	sub, err = plugin.RepositorySubdirectory()
+	assert.Equal(t, sub, "docker-compose/beta")
+	assert.Nil(t, err)
+
+	plugin = &Plugin{Location: "/Users/keithpitt/Development/plugins.git/test-plugin"}
+	repo, err = plugin.Repository()
+	assert.Equal(t, repo, "/Users/keithpitt/Development/plugins.git")
+	assert.Nil(t, err)
+	sub, err = plugin.RepositorySubdirectory()
+	assert.Equal(t, sub, "test-plugin")
+	assert.Nil(t, err)
+
+	plugin = &Plugin{Location: ""}
+	repo, err = plugin.Repository()
+	assert.Equal(t, repo, "")
+	assert.NotNil(t, err)
+	assert.Equal(t, err.Error(), "Missing plugin location")
+}
+
+func TestPluginConfigurationToEnv(t *testing.T) {
+	plugin := &Plugin{Location: "github.com/buildkite/plugins/docker-compose"}
+
+	plugin.Configuration = map[string]interface{}{"container": "app", "some-other-setting": "else right here"}
+	assert.Equal(t, plugin.ConfigurationToEnv(), []string{"BUILDKITE_PLUGIN_DOCKER_COMPOSE_CONTAINER=app", "BUILDKITE_PLUGIN_DOCKER_COMPOSE_SOME_OTHER_SETTING=else right here"})
+
+	plugin.Configuration = map[string]interface{}{"and _ with a    - number": 12}
+	assert.Equal(t, plugin.ConfigurationToEnv(), []string{"BUILDKITE_PLUGIN_DOCKER_COMPOSE_AND_WITH_A_NUMBER=12"})
+}
