f87692d41cab4ccaa4f4f960379d543f39895b2d
Keith Pitt <me@keithpitt.com>
2015-10-20 19:37:42 +1030

    Added some tests to make sure buckets with "s" in them work

--- agent/s3_uploader_test.go
+++ agent/s3_uploader_test.go
@@ -8,12 +8,16 @@ import (
 
 func TestBucketPath(t *testing.T) {
 	s3Uploader := S3Uploader{Destination: "s3://my-bucket-name/foo/bar"}
-
 	assert.Equal(t, s3Uploader.BucketPath(), "foo/bar")
+
+	s3Uploader.Destination = "s3://starts-with-an-s/and-this-is-its/folder"
+	assert.Equal(t, s3Uploader.BucketPath(), "and-this-is-its/folder")
 }
 
 func TestBucketName(t *testing.T) {
 	s3Uploader := S3Uploader{Destination: "s3://my-bucket-name/foo/bar"}
-
 	assert.Equal(t, s3Uploader.BucketName(), "my-bucket-name")
+
+	s3Uploader.Destination = "s3://starts-with-an-s"
+	assert.Equal(t, s3Uploader.BucketName(), "starts-with-an-s")
 }
