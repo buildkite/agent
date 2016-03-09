b9f3dca9fcb071a22c09e071018228a50d04eb9e
Samuel Cochran <sj26@sj26.com>
2016-03-10 08:27:18 +1100

    Bump version 2.1.6.1

--- agent/version.go
+++ agent/version.go
@@ -6,7 +6,7 @@ package agent
 //
 // On CI, the binaries are always build with the buildVersion variable set.
 
-var baseVersion string = "2.1.6"
+var baseVersion string = "2.1.6.1"
 var buildVersion string = ""
 
 func Version() string {
