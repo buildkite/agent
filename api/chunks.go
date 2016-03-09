cd08b97072576c9e17225d7ef89e6564aae35272
Samuel Cochran <sj26@sj26.com>
2016-03-09 14:31:09 +1100

    Transparently compress chunk create request body

--- api/chunks.go
+++ api/chunks.go
@@ -25,33 +25,23 @@ type Chunk struct {
 // but a multi-part HTTP form upload
 func (cs *ChunksService) Upload(jobId string, chunk *Chunk) (*Response, error) {
 	body := &bytes.Buffer{}
-	writer := multipart.NewWriter(body)
+	gzipper := gzip.NewWriter(body)
+	writer := multipart.NewWriter(gzipper)
 
 	// Write the sequence, offset and size values to the form
 	writer.WriteField("sequence", fmt.Sprintf("%d", chunk.Sequence))
 	writer.WriteField("offset", fmt.Sprintf("%d", chunk.Offset))
 	writer.WriteField("size", fmt.Sprintf("%d", chunk.Size))
 
-	// Gzip the chunk data
-	var b bytes.Buffer
-	gz := gzip.NewWriter(&b)
-	if _, err := gz.Write([]byte(chunk.Data)); err != nil {
-		return nil, err
-	}
-	if err := gz.Flush(); err != nil {
-		return nil, err
-	}
-	if err := gz.Close(); err != nil {
-		return nil, err
-	}
-
 	// Write the chunk to the form
-	part, _ := writer.CreateFormFile("chunk", "chunk.gz")
-	part.Write(b.Bytes())
+	part, _ := writer.CreateFormFile("chunk", "chunk")
+	part.Write([]byte(chunk.Data))
 
-	// Close the writer because we don't need to add any more values to it
-	err := writer.Close()
-	if err != nil {
+	// Close the writer and gzipper to finalise the buffer
+	if err := writer.Close(); err != nil {
+		return nil, err
+	}
+	if err := gzipper.Close(); err != nil {
 		return nil, err
 	}
 
@@ -62,6 +52,7 @@ func (cs *ChunksService) Upload(jobId string, chunk *Chunk) (*Response, error) {
 	}
 
 	req.Header.Add("Content-Type", writer.FormDataContentType())
+	req.Header.Add("Content-Encoding", "gzip")
 
 	return cs.client.Do(req, nil)
 }
