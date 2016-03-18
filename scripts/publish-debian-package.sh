7a0ca6b5bb1c63906eecdcee74d964956560c375
Tim Lucas <t@toolmantim.com>
2016-03-18 15:46:04 +1100

    Build debs once, publish in stages

---
+++ scripts/publish-debian-package.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+if [[ "$CODENAME" == "" ]]; then
+  echo "Error: Missing \$CODENAME (stable or unstable)"
+  exit 1
+fi
+
+echo '--- Downloading built debian packages'
+buildkite-agent artifact download "deb/*.deb" "."
+
+echo '--- Installing dependencies'
+bundle
+
+# Loop over all the .deb files and publish them
+for file in deb/*.deb; do
+  echo "+++ Shipping $file"
+  ./scripts/utils/publish-debian-package.sh $file $CODENAME
+done
