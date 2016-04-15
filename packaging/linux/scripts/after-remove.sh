8632eb56f757b05439b3ff20d2c80fc55b8109b8
John Newbigin <john.newbigin@rea-group.com>
2016-04-15 21:51:08 +1000

    Check all 3 locations which could have been touched by the install script

--- packaging/linux/scripts/after-remove.sh
+++ packaging/linux/scripts/after-remove.sh
@@ -1,8 +1,10 @@
 # Remove the system service we installed
 if command -v systemctl > /dev/null; then
   rm -f /lib/systemd/system/buildkite-agent.service
-elif [ -d /etc/init ]; then
+fi
+if [ -f /etc/init/buildkite-agent.conf ]; then
   rm -f /etc/init/buildkite-agent.conf
-elif [ -d /etc/init.d ]; then
+fi
+if [ -f /etc/init.d/buildkite-agent ]; then
   rm -f /etc/init.d/buildkite-agent
 fi
