d495895b6c6f6c65fc91ac2501c3a2c6e755d0db
Tim Lucas <t@toolmantim.com>
2016-05-13 19:49:40 +1000

    Publish the SHA-1

--- scripts/publish-to-s3.sh
+++ scripts/publish-to-s3.sh
@@ -24,12 +24,17 @@ for binary in buildkite-agent-*; do
 
   echo "Publishing $binary to $binary_s3_url"
   aws s3 cp --acl "public-read" "$binary" "$binary_s3_url"
+
+  echo "Fetching SHA1"
+  buildkite-agent artifact shasum "pkg/$binary" > "$binary.sha1"
+
+  echo "Publishing $binary.sha1 to $binary_s3_url.sha1"
+  aws s3 cp --acl "public-read" --content-type "text/plain" "$binary.sha1" "$binary_s3_url.sha1"
 done
 
-echo "--- :s3: Updating latest"
+echo "--- :s3: Copying /$version to /latest"
 
 latest_version=$(aws s3 ls --page-size 1000 "$s3_base_url/" | grep PRE | awk '{print $2}' | awk -F '/' '{print $1}' | ruby -pe 'readlines.map {|s| Gem::Version.new(s)}.max')
-
 latest_version_s3_url="$s3_base_url/$latest_version/"
 latest_s3_url="$s3_base_url/latest/"
 
@@ -37,4 +42,4 @@ echo "Copying $latest_version_s3_url to $latest_s3_url"
 
 aws s3 cp --acl public-read-write --recursive "$latest_version_s3_url" "$latest_s3_url"
 
-echo "--- :tada: All done!"
+echo "--- :llama::sparkles::llama: All done!"
