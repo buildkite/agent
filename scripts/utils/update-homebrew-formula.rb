eecd4ba5f4a7046cdc9d54fb0a75de79411b311b
Tim Lucas <t@toolmantim.com>
2016-07-01 12:11:34 +1000

    Fix the homebrew sha recipe updating

--- scripts/utils/update-homebrew-formula.rb
+++ scripts/utils/update-homebrew-formula.rb
@@ -8,12 +8,14 @@
 #     version "..."
 #     url     "..."
 #     sha1    "..."
+#     sha256  "..."
 #   end
 #
 #   devel do
 #     version "..."
 #     url     "..."
 #     sha1    "..."
+#     sha256  "..."
 #   end
 
 release, version, url, sha1 = ARGV
@@ -24,6 +26,7 @@ print $stdin.read.sub(%r{
       version \s+ ").*?("  .*?
       url     \s+ ").*?("  .*?
       sha1    \s+ ").*?("  .*?
+      sha256  \s+ ").*?("  .*?
     end
   )
-}xm, "\\1#{version}\\2#{url}\\3#{sha1}\\4")
+}xm, "\\1#{version}\\2#{url}\\3#{sha1}\\4#{sha256}\\5")
