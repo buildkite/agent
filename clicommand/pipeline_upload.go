059f1a3d394b99ae1a1d6f07236bfc7debfd202a
Keith Pitt <me@keithpitt.com>
2016-10-18 11:57:44 +1100

    Added `--format` option to the `pipeline upload` command

--- clicommand/pipeline_upload.go
+++ clicommand/pipeline_upload.go
@@ -24,8 +24,8 @@ var PipelineUploadHelpDescription = `Usage:
 Description:
 
    Allows you to change the pipeline of a running build by uploading either a
-   JSON or Yaml configuration file. If no configuration file is provided,
-   we look for the file in the following locations:
+   YAML (recommended) or JSON configuration file. If no configuration file is
+   provided, the command looks for the file in the following locations:
 
    - buildkite.yml
    - buildkite.yaml
@@ -34,17 +34,18 @@ Description:
    - .buildkite/pipeline.yaml
    - .buildkite/pipeline.json
 
-   You can also pipe build pipelines to the command, allowing you to create scripts
-   that generate dynamic pipelines.
+   You can also pipe build pipelines to the command allowing you to create
+   scripts that generate dynamic pipelines.
 
 Example:
 
    $ buildkite-agent pipeline upload
    $ buildkite-agent pipeline upload my-custom-pipeline.yml
-   $ ./script/dynamic_step_generator | buildkite-agent pipeline upload`
+   $ ./script/dynamic_step_generator | buildkite-agent pipeline upload --format json`
 
 type PipelineUploadConfig struct {
 	FilePath         string `cli:"arg:0" label:"upload paths"`
+	Format           string `cli:"format"`
 	Replace          bool   `cli:"replace"`
 	Job              string `cli:"job" validate:"required"`
 	AgentAccessToken string `cli:"agent-access-token" validate:"required"`
@@ -59,6 +60,12 @@ var PipelineUploadCommand = cli.Command{
 	Usage:       "Uploads a description of a build pipeline adds it to the currently running build after the current job.",
 	Description: PipelineUploadHelpDescription,
 	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:   "format",
+			Value:  "",
+			Usage:  "The file format of the pipeline (yaml, json). This argument is required when reading a build pipeline via STDIN.",
+			EnvVar: "BUILDKITE_PIPELINE_FORMAT",
+		},
 		cli.BoolFlag{
 			Name:   "replace",
 			Usage:  "Replace the rest of the existing pipeline with the steps uploaded. Jobs that are already running are not removed.",
@@ -94,6 +101,7 @@ var PipelineUploadCommand = cli.Command{
 		var input []byte
 		var err error
 		var filename string
+		var format string
 
 		if cfg.FilePath != "" {
 			logger.Info("Reading pipeline config from \"%s\"", cfg.FilePath)
@@ -106,6 +114,16 @@ var PipelineUploadCommand = cli.Command{
 		} else if stdin.IsPipe() {
 			logger.Info("Reading pipeline config from STDIN")
 
+			// Make sure a format was passed
+			if cfg.Format == "" {
+				logger.Fatal("A format defined with `--format (yaml or json)` is required when reading a config via STDIN, for example (./script/dynamic_step_generator | buildkite-agent pipeline upload --format json). See `buildkite-agent pipeline upload --help` for more information.")
+			} else if cfg.Format != "yaml" || cfg.Format != "json" {
+				logger.Fatal("Unknown pipeline format `%s` - only `yaml` and `json` are supported. See `buildkite-agent pipeline upload --help` for more information.", cfg.Format)
+			}
+
+			format = cfg.Format
+
+			// Now we can read from STDIN
 			input, err = ioutil.ReadAll(os.Stdin)
 			if err != nil {
 				logger.Fatal("Failed to read from STDIN: %s", err)
