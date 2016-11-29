8d4b250d7be2d3da6df39e3e3c435b1102634758
Samuel Cochran <sj26@sj26.com>
2016-11-29 16:52:42 +1100

    Fix typo

--- clicommand/artifact_download.go
+++ clicommand/artifact_download.go
@@ -53,7 +53,7 @@ var ArtifactDownloadCommand = cli.Command{
 		cli.StringFlag{
 			Name:  "step",
 			Value: "",
-			Usage: "Scope the search to a paticular step by using either it's name of job ID",
+			Usage: "Scope the search to a paticular step by using either it's name or job ID",
 		},
 		cli.StringFlag{
 			Name:   "build",
