4689bdcea4614625baaeb1b029fda025f9644267
Keith Pitt <me@keithpitt.com>
2017-03-07 16:26:04 +1100

    Revert "Add option to timestamp lines"

--- agent/job_runner.go
+++ agent/job_runner.go
@@ -61,13 +61,11 @@ func (r JobRunner) Create() (runner *JobRunner, err error) {
 
 	// The process that will run the bootstrap script
 	runner.process = process.Process{
-		Script:             r.AgentConfiguration.BootstrapScript,
-		Env:                r.createEnvironment(),
-		PTY:                r.AgentConfiguration.RunInPty,
-		Timestamp:          r.AgentConfiguration.TimestampLines,
-		StartCallback:      r.onProcessStartCallback,
-		LineCallback:       runner.headerTimesStreamer.Scan,
-		LineCallbackFilter: runner.headerTimesStreamer.LineIsHeader,
+		Script:        r.AgentConfiguration.BootstrapScript,
+		Env:           r.createEnvironment(),
+		PTY:           r.AgentConfiguration.RunInPty,
+		StartCallback: r.onProcessStartCallback,
+		LineCallback:  runner.headerTimesStreamer.Scan,
 	}.Create()
 
 	return
--- process/process.go
+++ process/process.go
@@ -23,7 +23,6 @@ import (
 type Process struct {
 	Pid        int
 	PTY        bool
-	Timestamp  bool
 	Script     string
 	Env        []string
 	ExitStatus string
@@ -36,9 +35,8 @@ type Process struct {
 	StartCallback func()
 
 	// For every line in the process output, this callback will be called
-	// with the contents of the line if its filter returns true.
-	LineCallback       func(string)
-	LineCallbackFilter func(string) bool
+	// with the contents of the line
+	LineCallback func(string)
 
 	// Running is stored as an int32 so we can use atomic operations to
 	// set/get it (it's accessed by multiple goroutines)
@@ -69,12 +67,7 @@ func (p *Process) Start() error {
 
 	lineReaderPipe, lineWriterPipe := io.Pipe()
 
-	var multiWriter io.Writer
-	if p.Timestamp {
-		multiWriter = io.MultiWriter(lineWriterPipe)
-	} else {
-		multiWriter = io.MultiWriter(&p.buffer, lineWriterPipe)
-	}
+	multiWriter := io.MultiWriter(&p.buffer, lineWriterPipe)
 
 	logger.Info("Starting to run script: %s", p.command.Path)
 
@@ -178,37 +171,11 @@ func (p *Process) Start() error {
 				}
 			}
 
-			// If we're timestamping this main thread will take
-			// the hit of running the regex so we can build up
-			// the timestamped buffer without breaking headers,
-			// otherwise we let the goroutines take the perf hit.
-
-			checkedForCallback := false
-			lineHasCallback := false
-			lineString := string(line)
-
-			// Create the prefixed buffer
-			if p.Timestamp {
-				lineHasCallback = p.LineCallbackFilter(lineString)
-				checkedForCallback = true
-				if lineHasCallback {
-					// Don't timestamp special lines (e.g. header)
-					p.buffer.WriteString(fmt.Sprintf("%s\n", line))
-				} else {
-					currentTime := time.Now().UTC().Format(time.RFC3339)
-					p.buffer.WriteString(fmt.Sprintf("[%s] %s\n", currentTime, line))
-				}
-			}
-
-			if lineHasCallback || !checkedForCallback {
-				lineCallbackWaitGroup.Add(1)
-				go func(line string) {
-					defer lineCallbackWaitGroup.Done()
-					if (checkedForCallback && lineHasCallback) || p.LineCallbackFilter(lineString) {
-						p.LineCallback(line)
-					}
-				}(lineString)
-			}
+			lineCallbackWaitGroup.Add(1)
+			go func(line string) {
+				defer lineCallbackWaitGroup.Done()
+				p.LineCallback(line)
+			}(string(line))
 		}
 
 		// We need to make sure all the line callbacks have finish before
