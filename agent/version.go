cbfab909598deda7dc0b40747f4cd9e2f0c6cdec
Keith Pitt <me@keithpitt.com>
2017-03-20 12:02:22 +1100

    Bumped to 2.4.1

--- agent/version.go
+++ agent/version.go
@@ -6,7 +6,7 @@ package agent
 //
 // On CI, the binaries are always build with the buildVersion variable set.
 
-var baseVersion string = "2.4"
+var baseVersion string = "2.4.1"
 var buildVersion string = ""
 
 func Version() string {
