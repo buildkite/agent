c080cd497ad251bae5d3d5a7bb3c47d46ff7eb70
Keith Pitt <me@keithpitt.com>
2017-03-27 15:39:40 +1100

    Bumped to 3.0-beta.19

--- agent/version.go
+++ agent/version.go
@@ -6,7 +6,7 @@ package agent
 //
 // On CI, the binaries are always build with the buildVersion variable set.
 
-var baseVersion string = "3.0-beta.18"
+var baseVersion string = "3.0-beta.19"
 var buildVersion string = ""
 
 func Version() string {
