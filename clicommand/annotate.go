5604a0adac88455a8115c3fc81b0f3ace42d8504
Keith Pitt <me@keithpitt.com>
2017-05-09 17:44:19 +1000

    Started working on an `buildkite-agent annotate` command

---
+++ clicommand/annotate.go
@@ -0,0 +1,181 @@
+package clicommand
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/buildkite/agent/agent"
+	"github.com/buildkite/agent/api"
+	"github.com/buildkite/agent/cliconfig"
+	"github.com/buildkite/agent/logger"
+	"github.com/buildkite/agent/retry"
+	"github.com/buildkite/agent/stdin"
+	"github.com/urfave/cli"
+)
+
+var AnnotateHelpDescription = `Usage:
+
+   buildkite-agent annotate <file> [arguments...]
+
+Description:
+
+   Something something...
+
+Example:
+
+   $ buildkite-agent annotate body.md
+   $ ./script/dynamic_annotation_generator | buildkite-agent annotate`
+
+type AnnotateConfig struct {
+	FilePath         string `cli:"arg:0" label:"upload paths" validate:"required"`
+	Style            string `cli:"style" validate:"required"`
+	Context          string `cli:"context" validate:"required"`
+	AgentAccessToken string `cli:"agent-access-token" validate:"required"`
+	Endpoint         string `cli:"endpoint" validate:"required"`
+	NoColor          bool   `cli:"no-color"`
+	Debug            bool   `cli:"debug"`
+	DebugHTTP        bool   `cli:"debug-http"`
+}
+
+var AnnotateCommand = cli.Command{
+	Name:        "annotate",
+	Usage:       "Annotate the build page within the Buildkite UI with text from within a Buildkite job",
+	Description: AnnotateHelpDescription,
+	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:   "context",
+			Value:  "default",
+			Usage:  "todo",
+			EnvVar: "BUILDKITE_ANNOTATION_CONTEXT",
+		},
+		cli.StringFlag{
+			Name:   "style",
+			Value:  "default",
+			Usage:  "todo",
+			EnvVar: "BUILDKITE_ANNOTATION_STYLE",
+		},
+		AgentAccessTokenFlag,
+		EndpointFlag,
+		NoColorFlag,
+		DebugFlag,
+		DebugHTTPFlag,
+	},
+	Action: func(c *cli.Context) {
+		// The configuration will be loaded into this struct
+		cfg := AnnotateConfig{}
+
+		// Load the configuration
+		loader := cliconfig.Loader{CLI: c, Config: &cfg}
+		if err := loader.Load(); err != nil {
+			logger.Fatal("%s", err)
+		}
+
+		// Setup the any global configuration options
+		HandleGlobalFlags(cfg)
+
+		// Find the pipeline file either from STDIN or the first
+		// argument
+		var input []byte
+		var err error
+		var filename string
+
+		if cfg.FilePath != "" {
+			logger.Info("Reading pipeline config from \"%s\"", cfg.FilePath)
+
+			filename = filepath.Base(cfg.FilePath)
+			input, err = ioutil.ReadFile(cfg.FilePath)
+			if err != nil {
+				logger.Fatal("Failed to read file: %s", err)
+			}
+		} else if stdin.IsPipe() {
+			logger.Info("Reading pipeline config from STDIN")
+
+			// Actually read the file from STDIN
+			input, err = ioutil.ReadAll(os.Stdin)
+			if err != nil {
+				logger.Fatal("Failed to read from STDIN: %s", err)
+			}
+		} else {
+			logger.Info("Searching for pipeline config...")
+
+			paths := []string{
+				"buildkite.yml",
+				"buildkite.yaml",
+				"buildkite.json",
+				filepath.FromSlash(".buildkite/pipeline.yml"),
+				filepath.FromSlash(".buildkite/pipeline.yaml"),
+				filepath.FromSlash(".buildkite/pipeline.json"),
+			}
+
+			// Collect all the files that exist
+			exists := []string{}
+			for _, path := range paths {
+				if _, err := os.Stat(path); err == nil {
+					exists = append(exists, path)
+				}
+			}
+
+			// If more than 1 of the config files exist, throw an
+			// error. There can only be one!!
+			if len(exists) > 1 {
+				logger.Fatal("Found multiple configuration files: %s. Please only have 1 configuration file present.", strings.Join(exists, ", "))
+			} else if len(exists) == 0 {
+				logger.Fatal("Could not find a default pipeline configuration file. See `buildkite-agent pipeline upload --help` for more information.")
+			}
+
+			found := exists[0]
+
+			logger.Info("Found config file \"%s\"", found)
+
+			// Read the default file
+			filename = path.Base(found)
+			input, err = ioutil.ReadFile(found)
+			if err != nil {
+				logger.Fatal("Failed to read file \"%s\" (%s)", found, err)
+			}
+		}
+
+		// Make sure the file actually has something in it
+		if len(input) == 0 {
+			logger.Fatal("Config file is empty")
+		}
+
+		var parsed interface{}
+
+		// Parse the pipeline
+		parsed, err = agent.PipelineParser{Filename: filename, Pipeline: input}.Parse()
+		if err != nil {
+			logger.Fatal("Pipeline parsing of \"%s\" failed (%s)", filename, err)
+		}
+
+		// Create the API client
+		client := agent.APIClient{
+			Endpoint: cfg.Endpoint,
+			Token:    cfg.AgentAccessToken,
+		}.Create()
+
+		// Generate a UUID that will identifiy this pipeline change. We
+		// do this outside of the retry loop because we want this UUID
+		// to be the same for each attempt at updating the pipeline.
+		uuid := api.NewUUID()
+
+		// Retry the pipeline upload a few times before giving up
+		err = retry.Do(func(s *retry.Stats) error {
+			_, err = client.Pipelines.Upload(cfg.Job, &api.Pipeline{UUID: uuid, Pipeline: parsed, Replace: cfg.Replace})
+			if err != nil {
+				logger.Warn("%s (%s)", err, s)
+			}
+
+			return err
+		}, &retry.Config{Maximum: 5, Interval: 1 * time.Second})
+		if err != nil {
+			logger.Fatal("Failed to upload and process pipeline: %s", err)
+		}
+
+		logger.Info("Successfully uploaded and parsed pipeline config")
+	},
+}
