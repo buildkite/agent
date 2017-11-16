252e4a7cbed002656413c0dc9aa6a4ec4b35f7e9
Lachlan Donald <lachlan@buildkite.com>
2017-11-16 17:03:22 +1100

    Add DRY_RUN technology

--- scripts/publish-to-s3.sh
+++ scripts/publish-to-s3.sh
@@ -10,6 +10,14 @@ if [[ "$CODENAME" == "experimental" ]]; then
   version="$version.$build"
 fi
 
+dry_run() {
+  if [[ "${DRY_RUN:-false}" == "true" ]] ; then
+    echo "[dry-run] $*"
+  else
+    "$@"
+  fi
+}
+
 echo "--- :package: Downloading built binaries from build ${artifacts_build}"
 
 rm -rf pkg/*
@@ -24,23 +32,23 @@ for binary in *; do
   binary_s3_url="$s3_base_url/$version/$binary"
 
   echo "Publishing $binary to $binary_s3_url"
-  echo aws s3 --region "us-east-1" cp --acl "public-read" "$binary" "$binary_s3_url"
+  dry_run aws s3 --region "us-east-1" cp --acl "public-read" "$binary" "$binary_s3_url"
 
   echo "Calculating SHA256"
   sha256sum "$binary" | awk '{print $1}' > "$binary.sha256"
 
   echo "Publishing $binary.sha256 to $binary_s3_url.sha256"
-  echo aws s3 cp --region "us-east-1" --acl "public-read" --content-type "text/plain" "$binary.sha256" "$binary_s3_url.sha256"
+  dry_run aws s3 cp --region "us-east-1" --acl "public-read" --content-type "text/plain" "$binary.sha256" "$binary_s3_url.sha256"
 done
 
-# echo "--- :s3: Copying /$version to /latest"
+echo "--- :s3: Copying /$version to /latest"
 
-# latest_version=$(aws s3 ls --region "us-east-1" "$s3_base_url/" | grep PRE | awk '{print $2}' | awk -F '/' '{print $1}' | ruby ../scripts/utils/latest_version.rb)
-# latest_version_s3_url="$s3_base_url/$latest_version/"
-# latest_s3_url="$s3_base_url/latest/"
+latest_version=$(aws s3 ls --region "us-east-1" "$s3_base_url/" | grep PRE | awk '{print $2}' | awk -F '/' '{print $1}' | ruby ../scripts/utils/latest_version.rb)
+latest_version_s3_url="$s3_base_url/$latest_version/"
+latest_s3_url="$s3_base_url/latest/"
 
-# echo "Copying $latest_version_s3_url to $latest_s3_url"
+echo "Copying $latest_version_s3_url to $latest_s3_url"
 
-# aws s3 cp --region "us-east-1" --acl "public-read" --recursive "$latest_version_s3_url" "$latest_s3_url"
+dry_run aws s3 cp --region "us-east-1" --acl "public-read" --recursive "$latest_version_s3_url" "$latest_s3_url"
 
-# echo "--- :llama::sparkles::llama: All done!"
+echo "--- :llama::sparkles::llama: All done!"
