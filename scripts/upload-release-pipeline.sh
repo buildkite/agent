95f5575f0bb539dc45a4678a63e69a4a3cce974e
Lachlan Donald <lachlan@buildkite.com>
2017-11-16 11:55:29 +1100

    Comment out parts of upload 🤔

--- scripts/upload-release-pipeline.sh
+++ scripts/upload-release-pipeline.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 export AGENT_VERSION=$(buildkite-agent meta-data get "agent-version")
 
 buildkite-agent pipeline upload .buildkite/pipeline.release-experimental.yml
-buildkite-agent pipeline upload .buildkite/pipeline.release-unstable.yml
+# buildkite-agent pipeline upload .buildkite/pipeline.release-unstable.yml
 
-if [[ ! $AGENT_VERSION =~ beta|rc ]] ; then
-  buildkite-agent pipeline upload .buildkite/pipeline.release-stable.yml
-fi
+# if [[ ! $AGENT_VERSION =~ beta|rc ]] ; then
+#   buildkite-agent pipeline upload .buildkite/pipeline.release-stable.yml
+# fi
