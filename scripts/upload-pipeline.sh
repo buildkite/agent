b2e5ba223b0c1f4f677a3fac241e51cd6e8e82a4
Lachlan Donald <lachlan@buildkite.com>
2017-11-16 14:54:30 +1100

    Correct steps in experimental pipeline

--- scripts/upload-pipeline.sh
+++ scripts/upload-pipeline.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 echo '+++ Extracting agent version source code'
 
-export AGENT_VERSION=$(awk -F\" '/var baseVersion string = "/ {print $2}' agent/version.go)
+export AGENT_VERSION; AGENT_VERSION=$(awk -F\" '/var baseVersion string = "/ {print $2}' agent/version.go)
 export BUILD_VERSION=$BUILDKITE_BUILD_NUMBER
 export FULL_AGENT_VERSION="buildkite-agent version ${AGENT_VERSION}, build ${BUILD_VERSION}"
 
@@ -15,7 +15,7 @@ buildkite-agent meta-data set "agent-version" "$AGENT_VERSION"
 buildkite-agent meta-data set "agent-version-full" "$FULL_AGENT_VERSION"
 buildkite-agent meta-data set "agent-version-build" "$BUILD_VERSION"
 
-PIPELINE_FILE=${1:-.buildkite/pipeline.yml}
+PIPELINE_FILE="${1:-.buildkite/pipeline.yml}"
 
 echo "+++ Uploading pipeline file ${PIPELINE_FILE}"
 
