871e93439d06361173c880591d88572ea3e1c98c
Lachlan Donald <lachlan@buildkite.com>
2017-11-16 11:00:54 +1100

    Split up pipelines into different files

--- scripts/upload-release-pipeline.sh
+++ scripts/upload-release-pipeline.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-AGENT_VERSION=$(buildkite-agent meta-data get "agent-version") \
-  buildkite-agent pipeline upload .buildkite/pipeline.release.yml
+export AGENT_VERSION=$(buildkite-agent meta-data get "agent-version")
+
+buildkite-agent pipeline upload .buildkite/pipeline.release-experimental.yml
+buildkite-agent pipeline upload .buildkite/pipeline.release-unstable.yml
+
+if [[ ! AGENT_VERSION =~ beta|rc ]] ; then
+  buildkite-agent pipeline upload .buildkite/pipeline.release-stable.yml
+fi
