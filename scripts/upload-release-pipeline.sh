c38a1ab495604dfbb11f2d95219bfcdc9ccf4bd4
Lachlan Donald <lachlan@buildkite.com>
2017-11-16 11:36:35 +1100

    Add some sleeps? 💤

--- scripts/upload-release-pipeline.sh
+++ scripts/upload-release-pipeline.sh
@@ -4,7 +4,10 @@ set -euo pipefail
 export AGENT_VERSION=$(buildkite-agent meta-data get "agent-version")
 
 buildkite-agent pipeline upload .buildkite/pipeline.release-experimental.yml
+sleep 1
+
 buildkite-agent pipeline upload .buildkite/pipeline.release-unstable.yml
+sleep 1
 
 if [[ ! AGENT_VERSION =~ beta|rc ]] ; then
   buildkite-agent pipeline upload .buildkite/pipeline.release-stable.yml
