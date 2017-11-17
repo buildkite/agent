15d519e1b09c84a94ab42fe8a23c26f3c102be1a
Lachlan Donald <lachlan@buildkite.com>
2017-11-17 17:25:17 +1100

    Split into different pipelines for release

---
+++ scripts/generate-release-steps.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+set -euo pipefail
+
+agent_version=$(buildkite-agent meta-data get "agent-version")
+build_version=$(buildkite-agent meta-data get "agent-version-build")
+full_agent_version=$(buildkite-agent meta-data get "agent-version-full")
+
+trigger_step() {
+  local name="$1"
+  local trigger_pipeline="$2"
+
+  cat <<YAML
+  - name: ":rocket: ${name}"
+    trigger: "${trigger_pipeline}"
+    async: false
+    branches: "master show-version-in-block-steps"
+    build:
+      message: "Release for ${agent_version}, build ${build_version}"
+      commit: "${BUILDKITE_COMMIT}"
+      branch: "${BUILDKITE_BRANCH}"
+      meta_data:
+        agent-artifacts-build: "${BUILDKITE_BUILD_ID}"
+        agent-version: "${agent_version}"
+        agent-version-build: "${build_version}"
+        agent-version-full:  "${full_agent_version}"
+      env:
+        DRY_RUN: "${DRY_RUN:-false}"
+YAML
+}
+
+block_step() {
+  local label="$1"
+  cat <<YAML
+  - wait
+  - block: "${label}"
+YAML
+}
+
+echo "steps:"
+
+git fetch --tags
+
+## only allow a release if a release tag for it doesn't exist
+if git rev-parse -q --verify "refs/tags/v${agent_version}" >/dev/null; then
+  exit 0
+fi
+
+trigger_step \
+  "edge ${agent_version}.${build_version}" \
+  "agent-release-experimental"
+
+block_step "beta?"
+
+trigger_step \
+  "beta ${agent_version}" \
+  "agent-release-unstable"
+
+if [[ ! $agent_version =~ (beta|rc) ]] ; then
+  block_step "stable?"
+
+  trigger_step \
+    "stable ${agent_version}" \
+    "agent-release-stable"
+fi
--- scripts/publish-to-s3.sh
+++ scripts/publish-to-s3.sh
@@ -4,15 +4,24 @@ set -eo pipefail
 
 version=$(buildkite-agent meta-data get "agent-version")
 build=$(buildkite-agent meta-data get "agent-version-build")
+artifacts_build=$(buildkite-agent meta-data get "agent-artifacts-build")
 
 if [[ "$CODENAME" == "experimental" ]]; then
   version="$version.$build"
 fi
 
-echo "--- :package: Downloading built binaries"
+dry_run() {
+  if [[ "${DRY_RUN:-}" == "false" ]] ; then
+    echo "$@"
+  else
+    echo "[dry-run] $*"
+  fi
+}
+
+echo "--- :package: Downloading built binaries from build ${artifacts_build}"
 
 rm -rf pkg/*
-buildkite-agent artifact download "pkg/buildkite-agent-*" .
+buildkite-agent artifact download --build "$artifacts_build" "pkg/buildkite-agent-*" .
 cd pkg
 
 echo "--- :s3: Publishing $version to download.buildkite.com"
@@ -22,14 +31,14 @@ s3_base_url="s3://download.buildkite.com/agent/$CODENAME"
 for binary in *; do
   binary_s3_url="$s3_base_url/$version/$binary"
 
-  echo "Publishing $binary to $binary_s3_url"
-  aws s3 --region "us-east-1" cp --acl "public-read" "$binary" "$binary_s3_url"
+  echo "--- :s3: Publishing $binary to $binary_s3_url"
+  dry_run aws s3 --region "us-east-1" cp --acl "public-read" "$binary" "$binary_s3_url"
 
   echo "Calculating SHA256"
   sha256sum "$binary" | awk '{print $1}' > "$binary.sha256"
 
   echo "Publishing $binary.sha256 to $binary_s3_url.sha256"
-  aws s3 cp --region "us-east-1" --acl "public-read" --content-type "text/plain" "$binary.sha256" "$binary_s3_url.sha256"
+  dry_run aws s3 cp --region "us-east-1" --acl "public-read" --content-type "text/plain" "$binary.sha256" "$binary_s3_url.sha256"
 done
 
 echo "--- :s3: Copying /$version to /latest"
@@ -40,6 +49,6 @@ latest_s3_url="$s3_base_url/latest/"
 
 echo "Copying $latest_version_s3_url to $latest_s3_url"
 
-aws s3 cp --region "us-east-1" --acl "public-read" --recursive "$latest_version_s3_url" "$latest_s3_url"
+dry_run aws s3 cp --region "us-east-1" --acl "public-read" --recursive "$latest_version_s3_url" "$latest_s3_url"
 
 echo "--- :llama::sparkles::llama: All done!"
