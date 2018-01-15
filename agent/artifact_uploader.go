6ca890757f83ac14fd10a7b908e129945e21da21
Lachlan Donald <lachlan@buildkite.com>
2018-01-15 13:28:48 +1100

    Tweak error message

--- agent/artifact_uploader.go
+++ agent/artifact_uploader.go
@@ -80,10 +80,11 @@ func (a *ArtifactUploader) Collect() (artifacts []*api.Artifact, err error) {
 
 		logger.Debug("Searching for %s", globPath)
 
-		// Resolve the globs (with * and ** in them)
+		// Resolve the globs (with * and ** in them), if it's a non-globbed path and doesn't exists
+		// then we will get the ErrNotExist that is handled below
 		files, err := zglob.Glob(globPath)
 		if err == os.ErrNotExist {
-			logger.Info("No files matched glob: %s", globPath)
+			logger.Info("File not found: %s", globPath)
 			continue
 		} else if err != nil {
 			return nil, err
