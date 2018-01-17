8613a38b3233ac1fa54202117a833467a00bbf6a
Lachlan Donald <lachlan@buildkite.com>
2018-01-17 16:12:42 +1100

    Create a minimal nupkg/chocolatey package

---
+++ packaging/windows/chocolatey/buildkite-agent/tools/chocolateyinstall.ps1
@@ -0,0 +1,4 @@
+﻿Install-ChocolateyZipPackage -PackageName 'buildkite-agent' `
+ -Url 'https://github.com/buildkite/agent/releases/download/v3.0-beta.38/buildkite-agent-windows-386-3.0-beta.38.zip' `
+ -UnzipLocation "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)" `
+ -Url64 'https://github.com/buildkite/agent/releases/download/v3.0-beta.38/buildkite-agent-windows-amd64-3.0-beta.38.zip'
