f07171cbeb3a0ae11ddf46c4d74117bd852d3ea3
Lachlan Donald <lachlan@buildkite.com>
2018-01-22 10:42:22 +1100

    Pass in files to process for junit report converter

--- scripts/junit.sh
+++ scripts/junit.sh
@@ -6,7 +6,7 @@ echo "--- :junit: Download the junits"
 buildkite-agent artifact download tmp/junit-*.xml tmp
 
 echo "--- :junit::golang: Processing the junits"
-docker run --rm -v "$(pwd):/app" -w /app gugahoi/junit-report-converter > tmp/annotation.md
+docker run --rm -v "${PWD}:${PWD}" -w "$PWD" gugahoi/junit-report-converter tmp/junit-*.xml > tmp/annotation.md
 
 echo "--- :buildkite: Creating annotation"
 buildkite-agent annotate --context junit --style error < tmp/annotation.md
