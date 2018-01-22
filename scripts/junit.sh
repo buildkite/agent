5e3f39f41c70c31bc4b96bc0ba693ec2bb9fa369
Lachlan Donald <lachlan@buildkite.com>
2018-01-22 12:09:13 +1100

    Only annotate when there is junit output

--- scripts/junit.sh
+++ scripts/junit.sh
@@ -8,5 +8,7 @@ buildkite-agent artifact download tmp/junit-*.xml tmp
 echo "--- :junit::golang: Processing the junits"
 docker run --rm -v "${PWD}:${PWD}" -w "$PWD" gugahoi/junit-report-converter tmp/junit-*.xml > tmp/annotation.md
 
-echo "--- :buildkite: Creating annotation"
-buildkite-agent annotate --context junit --style error < tmp/annotation.md
+if [[ -s tmp/annotation.md ]] ; then
+  echo "--- :buildkite: Creating annotation"
+  buildkite-agent annotate --context junit --style error < tmp/annotation.md
+fi
