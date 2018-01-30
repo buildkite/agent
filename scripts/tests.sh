f2ccd0adf5669ac2cac933106d1576db3aeb55a9
Lachlan Donald <lachlan@buildkite.com>
2018-01-30 17:20:52 +1100

    Check vendor status in tests

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -euo pipefail
-mkdir tmp/
+mkdir -p tmp/
 
 echo "~~~ Installing test dependencies"
 go get github.com/kyoh86/richgo
 go get github.com/jstemmer/go-junit-report
+go get github.com/kardianos/govendor
+
+echo "~~~ Checking vendor status"
+govendor status
 
 echo '+++ Running tests'
 go test -race ./... 2>&1 | tee tmp/output.txt | richgo testfilter
