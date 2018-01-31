e4fae51ef4ed5b2aa46fee1cad868db529720a11
Lachlan Donald <lachlan@buildkite.com>
2018-01-31 22:25:52 +1100

    Use tests.sh for nix and windows

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 set -euo pipefail
-mkdir tmp/
-
-echo "~~~ Installing test dependencies"
-go get github.com/kyoh86/richgo
-go get github.com/jstemmer/go-junit-report
 
 echo '+++ Running tests'
-go test -race ./... 2>&1 | tee tmp/output.txt | richgo testfilter
-
-echo "~~~ Creating junit output"
-go-junit-report < tmp/output.txt > "tmp/junit-${BUILDKITE_JOB_ID}.xml"
+go test -race ./... 2>&1 | sed -e 's/^---/***/'
