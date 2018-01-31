4b8605447851ca565f61187cc5352090f439b1a7
Lachlan Donald <lachlan@buildkite.com>
2018-01-31 21:53:13 +1100

    Remove junit steps, keep it simple

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-set -euo pipefail
-mkdir -p tmp/
-
-echo "~~~ Installing test dependencies"
-go get github.com/kyoh86/richgo
-go get github.com/jstemmer/go-junit-report
-go get github.com/kardianos/govendor
-
-echo "~~~ Checking vendor status"
-govendor status
-
-echo '+++ Running tests'
-go test -race ./... 2>&1 | tee tmp/output.txt | richgo testfilter
-
-echo "~~~ Creating junit output"
-go-junit-report < tmp/output.txt > "tmp/junit-${BUILDKITE_JOB_ID}.xml"
+set -e
+go test -race -v ./...
