36e33944d9dbae5728b844e96b53b5870be2b797
Keith Pitt <me@keithpitt.com>
2018-03-14 11:40:10 +0800

    All paths will now be considered "absolute" when normalizing them for bootstrap use

--- utils/path.go
+++ utils/path.go
@@ -3,24 +3,34 @@ package utils
 import (
 	"os"
 	"os/user"
+	"path/filepath"
 	"strings"
 )
 
-// Replaces ~/ with the users home directory, ./ with the woring directory, and
-// replaces any parses any environment variables.
-func NormalizeFilePath(path string) string {
+// Normalizes a path and returns an clean absolute version. It correctly
+// expands environment variables inside paths, converts "~/" into the users
+// home directory, and replaces "./" with the current working directory.
+func NormalizeFilePath(path string) string, error {
 	expandedPath := os.ExpandEnv(path)
 
 	if len(expandedPath) > 2 {
 		if expandedPath[:2] == "~/" {
-			usr, _ := user.Current()
-			dir := usr.HomeDir
-			return strings.Replace(expandedPath, "~", dir, 1)
+			if usr, err := user.Current(); err != nil {
+				return "", err
+			}
+			homeDir := usr.HomeDir
+			return strings.Replace(expandedPath, "~", homeDir, 1), nil
 		} else if expandedPath[:2] == "./" {
-			dir, _ := os.Getwd()
-			return strings.Replace(expandedPath, ".", dir, 1)
+			if workingDir, err := os.Getwd(); err != nil {
+				return "", err
+			}
+			return strings.Replace(expandedPath, ".", workingDir, 1), nil
 		}
 	}
 
-	return expandedPath
+	if absolutePath, err := filepath.Abs(expandedPath); err != nil {
+		return "", err
+	}
+
+	return absolutePath
 }
