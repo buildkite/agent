46fa48a5c455e2d75747ad25cb348b4c80dd5ade
Keith Pitt <me@keithpitt.com>
2018-03-14 13:01:55 +0800

    Fixed NormalizeFilePath and it's usage since it returns an error now

--- utils/path.go
+++ utils/path.go
@@ -10,27 +10,31 @@ import (
 // Normalizes a path and returns an clean absolute version. It correctly
 // expands environment variables inside paths, converts "~/" into the users
 // home directory, and replaces "./" with the current working directory.
-func NormalizeFilePath(path string) string, error {
+func NormalizeFilePath(path string) (string, error) {
 	expandedPath := os.ExpandEnv(path)
 
 	if len(expandedPath) > 2 {
 		if expandedPath[:2] == "~/" {
-			if usr, err := user.Current(); err != nil {
+			usr, err := user.Current()
+			if err != nil {
 				return "", err
 			}
-			homeDir := usr.HomeDir
-			return strings.Replace(expandedPath, "~", homeDir, 1), nil
+
+			return strings.Replace(expandedPath, "~", usr.HomeDir, 1), nil
 		} else if expandedPath[:2] == "./" {
-			if workingDir, err := os.Getwd(); err != nil {
+			workingDir, err := os.Getwd()
+			if err != nil {
 				return "", err
 			}
+
 			return strings.Replace(expandedPath, ".", workingDir, 1), nil
 		}
 	}
 
-	if absolutePath, err := filepath.Abs(expandedPath); err != nil {
+	absolutePath, err := filepath.Abs(expandedPath)
+	if err != nil {
 		return "", err
 	}
 
-	return absolutePath
+	return absolutePath, nil
 }
