fbfb0b4f42333dc1fdb15ca3d7acec649259f98e
Lachlan Donald <lachlan@buildkite.com>
2018-03-14 15:06:25 +0800

    Add BUILDKITE_CONFIG_PATH to the shim warning

--- packaging/linux/root/usr/share/buildkite-agent/bootstrap.sh
+++ packaging/linux/root/usr/share/buildkite-agent/bootstrap.sh
@@ -7,7 +7,7 @@
 echo "+++ :warning: Your buildkite-agent.cfg file contains a deprecated bootstrap.sh"
 echo "As part of the upgrade from Agent v2 to v3, a bootstrap-script compatibility shim was added to your buildkite-agent.cfg."
 echo ""
-echo "To silence this warning, you can comment our the bootstrap-script line in your buildkite-agent.cfg file."
+echo "To silence this warning, you can comment out the bootstrap-script line in ${BUILDKITE_CONFIG_PATH:-buildkite-agent.cfg}."
 echo ""
 echo "For more information, see our Buildkite Agent 3.0 upgrade guide:"
 echo "https://buildkite.com/docs/agent/upgrading-to-v3"
