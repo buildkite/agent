e5b4c62bf49b077256a07f6f6a328549fa12eca8
Lachlan Donald <lachlan@buildkite.com>
2018-03-14 09:55:15 +0800

    Reference the upgrade guide in the error

--- packaging/linux/root/usr/share/buildkite-agent/bootstrap.sh
+++ packaging/linux/root/usr/share/buildkite-agent/bootstrap.sh
@@ -4,5 +4,12 @@
 # v2 configurations will still have bootstrap-path=/usr/share/buildkite-agent/bootstrap.sh
 # which will cause all sorts of wierd behaviour
 
-echo "+++ :warning: Your agent configuration contains an outdated bootstrap-path reference. It can be safely removed."
+echo "+++ :warning: Your buildkite-agent.cfg file contains a deprecated bootstrap.sh"
+echo "As part of the upgrade from Agent v2 to v3, a bootstrap-script compatibility shim was added to your buildkite-agent.cfg."
+echo ""
+echo "To silence this warning, you can comment our the bootstrap-script line in your buildkite-agent.cfg file."
+echo ""
+echo "For more information, see our Buildkite Agent 3.0 upgrade guide:"
+echo "https://buildkite.com/docs/agent/upgrading-to-v3"
+
 exec buildkite-agent bootstrap "$@"
