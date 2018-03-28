4989e59589591d0914804bf82752df65a12f1f91
Lachlan Donald <lachlan@buildkite.com>
2018-03-28 17:16:09 +1100

    Move metadata set into extract-metadata step

--- scripts/extract-agent-version-metadata.sh
+++ scripts/extract-agent-version-metadata.sh
@@ -4,11 +4,14 @@ set -euo pipefail
 agent_version=$(awk -F\" '/var baseVersion string = "/ {print $2}' agent/version.go)
 build_version=${BUILDKITE_BUILD_NUMBER:-1}
 full_agent_version="buildkite-agent version ${agent_version}, build ${build_version}"
+docker_alpine_image_tag="buildkiteci/agent:alpine-build-${BUILDKITE_BUILD_NUMBER}"
 
 echo "Full agent version: $full_agent_version"
 echo "Agent version: $agent_version"
 echo "Build version: $build_version"
+echo "Docker Alpine Image Tag: $docker_alpine_image_tag"
 
 buildkite-agent meta-data set "agent-version" "$agent_version"
 buildkite-agent meta-data set "agent-version-full" "$full_agent_version"
 buildkite-agent meta-data set "agent-version-build" "$build_version"
+buildkite-agent meta-data set "agent-docker-image-alpine" "$docker_alpine_image_tag"
