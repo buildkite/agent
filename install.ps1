13a484d374dff82e03d096f461349c915896830e
Lachlan Donald <lachlan@buildkite.com>
2018-03-28 18:22:31 +1100

    Install windows binary to c:\buildkite-agent\bin

--- install.ps1
+++ install.ps1
@@ -1,11 +1,13 @@
-Param(
-    [string]$installDir = "C:\buildkite",
-    [string]$arch = "amd64",
-    [switch]$beta = ($env:buildkiteAgentBeta -eq 'true'),
-    [switch]$path = $true,
-    [string]$token = $env:buildkiteAgentToken,
-    [string]$tags = $env:builkiteAgentTags
-)   
+$installDir = "C:\buildkite-agent"
+$arch = "amd64"
+$beta = $env:buildkiteAgentBeta
+$token = $env:buildkiteAgentToken
+$tags = $env:builkiteAgentTags
+$path = $false
+
+if ([string]::IsNullOrEmpty($token)) {
+    throw "No token specified, set `$env:buildkiteAgentToken"
+}
 
 $ErrorActionPreference = "Stop"
 
@@ -23,14 +25,12 @@ Write-Host "
 ## https://superuser.com/questions/749243/detect-if-powershell-is-running-as-administrator
 
 $elevated = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
-if($elevated -eq $false)
-{
+if($elevated -eq $false) {
     throw "In order to install services, please run this script elevated."
 }
 
 $releaseInfoUrl = "https://buildkite.com/agent/releases/latest?platform=windows&arch=$arch"
-if($beta -eq $true) 
-{
+if($beta) {
     $releaseInfoUrl = $releaseInfoUrl + "&prerelease=true"
 }
 
@@ -39,8 +39,7 @@ Write-Host "Finding latest release"
 $resp = Invoke-WebRequest -Uri "$releaseInfoUrl" -UseBasicParsing -Method GET
 
 $releaseInfo = @{}
-foreach ($line in $resp.Content.Split("`n")) 
-{
+foreach ($line in $resp.Content.Split("`n")) {
     $info = $line -split "="
     $releaseInfo.add($info[0],$info[1])
 }
@@ -55,9 +54,15 @@ Write-Host 'Expanding buildkite-agent.zip'
 Expand-Archive -Force -Path buildkite-agent.zip -DestinationPath $installDir
 Remove-Item buildkite-agent.zip -Force
 
+$binDir = Join-Path $installDir "bin"
+if (![System.IO.Directory]::Exists($binDir)) {[void][System.IO.Directory]::CreateDirectory($binDir)}
+
+Write-Host 'Expanding buildkite-agent.exe into bin'
+Join-Path $installDir "buildkite-agent.exe" | Move-item -Destination $binDir -Force
+
 if ($path -eq $true) {
     Write-Host 'Updating PATH'
-    $env:PATH = "${installDir};" + $env:PATH
+    $env:PATH = "${binDir};" + $env:PATH
     [Environment]::SetEnvironmentVariable('PATH', $env:PATH, [EnvironmentVariableTarget]::Machine)
 }   
 
@@ -65,16 +70,20 @@ if ($path -eq $true) {
 buildkite-agent --version
 
 Write-Host "Updating configuration in ${installDir}\buildkite-agent.cfg"
-$buildkiteAgentCfgTemplate = type "${installDir}\buildkite-agent.cfg"
+$buildkiteAgentCfgTemplate = Get-Content "${installDir}\buildkite-agent.cfg"
 $buildkiteAgentCfgTemplate = $buildkiteAgentCfgTemplate -replace 'token="xxx"', ('token="{0}"' -f $token.Trim())
-$buildkiteAgentCfgTemplate = $buildkiteAgentCfgTemplate -replace '# tags="key1=val2,key2=val2"', ('tags="{0}"' -f $tags)
+
+if (![string]::IsNullOrEmpty($tags)) {
+    $buildkiteAgentCfgTemplate = $buildkiteAgentCfgTemplate -replace '# tags="key1=val2,key2=val2"', ('tags="{0}"' -f $tags)
+}
+
 [System.IO.File]::WriteAllLines("${installDir}\buildkite-agent.cfg", $buildkiteAgentCfgTemplate);
 
 Write-Host "Successfully installed to $installDir
 
 You can now start the agent!
 
-  ${installDir}\buildkite-agent.exe start
+  ${binDir}\buildkite-agent.exe start
 
 For docs, help and support:
 
