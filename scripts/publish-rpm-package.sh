c3e6a9b7084c9016b9ff581f56c9fe27c9ebedb2
Samuel Cochran <sj26@sj26.com>
2018-04-03 14:28:21 +1000

    Again, I reckon simple is better in these

--- scripts/publish-rpm-package.sh
+++ scripts/publish-rpm-package.sh
@@ -18,19 +18,6 @@ fi
 
 YUM_PATH=/yum.buildkite.com
 
-function publish() {
-  echo "--- Creating yum repositories for $CODENAME/$1"
-
-  ARCH_PATH="$YUM_PATH/buildkite-agent/$CODENAME/$1"
-  mkdir -p $ARCH_PATH
-  find "rpm/" -type f -name "*$1*" | xargs cp -t "$ARCH_PATH"
-  # createrepo_c is much faster and more resilient than createrepo
-  createrepo_c --no-database --unique-md-filenames --retain-old-md-by-age=7d --update "$ARCH_PATH" || \
-    createrepo_c --no-database --unique-md-filenames --retain-old-md-by-age=7d "$ARCH_PATH"
-  #createrepo --no-database --unique-md-filenames --update "$ARCH_PATH" || \
-  #  createrepo --no-database --unique-md-filenames "$ARCH_PATH"
-}
-
 echo '--- Downloading built yum packages packages'
 rm -rf rpm
 mkdir -p rpm
@@ -44,9 +31,19 @@ echo "--- Syncing s3://$RPM_S3_BUCKET to `hostname`"
 mkdir -p $YUM_PATH
 dry_run aws --region us-east-1 s3 sync --delete "s3://$RPM_S3_BUCKET" "$YUM_PATH"
 
-# Move the files to the right places
-dry_run publish "x86_64"
-dry_run publish "i386"
+# Add the rpms and update meta-data
+for ARCH in "x86_64" "i386"; do
+  echo "--- Updating yum repository for ${CODENAME}/${ARCH}"
+
+  ARCH_PATH="${YUM_PATH}/buildkite-agent/${CODENAME}/${ARCH}"
+  mkdir -p "$ARCH_PATH"
+  find "rpm/" -type f -name "*${ARCH}*" | xargs cp -t "$ARCH_PATH"
+  # createrepo_c is much faster and more resilient than createrepo
+  dry_run createrepo_c --no-database --unique-md-filenames --retain-old-md-by-age=7d --update "$ARCH_PATH" || \
+    dry_run createrepo_c --no-database --unique-md-filenames --retain-old-md-by-age=7d "$ARCH_PATH"
+  #createrepo --no-database --unique-md-filenames --update "$ARCH_PATH" || \
+  #  createrepo --no-database --unique-md-filenames "$ARCH_PATH"
+done
 
 # Sync back our changes to S3
 echo "--- Syncing local $YUM_PATH changes back to s3://$RPM_S3_BUCKET"
