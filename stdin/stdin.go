0e033460772c2f7c0e880bf743af1f144561ac9d
Lachlan Donald <lachlan@buildkite.com>
2018-05-02 09:10:57 +1000

    Properly handle stdin on linux

--- stdin/stdin.go
+++ stdin/stdin.go
@@ -4,15 +4,20 @@ import (
 	"os"
 )
 
+// This is a tricky problem and we have gone through several iterations before
+// settling on something that works well for recent golang across windows,
+// linux and macos.
+
 func IsReadable() bool {
 	fi, err := os.Stdin.Stat()
 	if err != nil {
 		return false
 	}
-	// Check we have something to read on stdin
-	// See https://flaviocopes.com/go-shell-pipes/
-	if fi.Mode()&os.ModeCharDevice != 0 || fi.Size() <= 0 {
-		return false
+
+	// Named pipes on unix/linux indicate a readable stdin, but might not have size yet
+	if fi.Mode()&os.ModeNamedPipe != 0 {
+		return true
 	}
-	return true
+
+	return fi.Size() > 0
 }
