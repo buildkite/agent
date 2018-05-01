1c093734dcda194bff899be545e2127931329d5c
Lachlan Donald <lachlan@buildkite.com>
2018-05-01 14:06:08 +1000

    Move back to a singular publish-docker-images.sh script

--- .buildkite/steps/publish-docker-images.sh
+++ .buildkite/steps/publish-docker-images.sh
@@ -9,12 +9,6 @@ dry_run() {
   fi
 }
 
-docker_image="buildkite/agent"
-stable_version="3"
-
-version=$(buildkite-agent meta-data get "agent-version")
-build=$(buildkite-agent meta-data get "agent-version-build")
-
 # Convert 2.3.2 into [ 2.3.2 2.3 2 ] or 3.0-beta.42 in [ 3.0-beta.42 3.0 3 ]
 parse_version() {
   local v="$1"
@@ -44,45 +38,47 @@ release_image() {
   dry_run docker push "${docker_image}:$tag"
 }
 
-variant="$1"
-suffix=""
+docker_image="buildkite/agent"
+stable_version="3"
 
-if [[ ! "$variant" =~ ^(alpine|ubuntu)$ ]] ; then
-  echo "Unknown docker variant $variant"
-  exit 1
-fi
+version=$(buildkite-agent meta-data get "agent-version")
+build=$(buildkite-agent meta-data get "agent-version-build")
 
-if [[ "$variant" != "alpine" ]] ; then
-  suffix="-$variant"
-fi
+for variant in "alpine" "ubuntu" ; do
+  suffix=""
 
-echo "--- Getting docker image tag for $variant from build meta data"
-image_tag=$(buildkite-agent meta-data get "agent-docker-image-$variant")
-echo "Docker Image Tag for $variant: $image_tag"
+  if [[ "$variant" != "alpine" ]] ; then
+    suffix="-$variant"
+  fi
 
-echo "--- :docker: Pulling prebuilt image"
-dry_run docker pull "$image_tag"
+  echo "--- Getting docker image tag for $variant from build meta data"
+  image_tag=$(buildkite-agent meta-data get "agent-docker-image-$variant")
+  echo "Docker Image Tag for $variant: $image_tag"
 
-# variants of edge/experimental
-if [[ "$CODENAME" == "experimental" ]] ; then
-  release_image "edge-build-${build}${suffix}"
-  release_image "edge${suffix}"
-fi
+  echo "--- :docker: Pulling prebuilt image"
+  dry_run docker pull "$image_tag"
 
-# variants of stable - e.g 2.3.2
-if [[ "$CODENAME" == "stable" ]] ; then
-  for tag in latest stable $(parse_version "$version") ; do
-    release_image "$tag${suffix}"
-  done
-fi
+  # variants of edge/experimental
+  if [[ "$CODENAME" == "experimental" ]] ; then
+    release_image "edge-build-${build}${suffix}"
+    release_image "edge${suffix}"
+  fi
 
-# variants of beta/unstable - e.g 3.0-beta.16
-if [[ "$CODENAME" == "unstable" ]] ; then
-  for tag in beta $(parse_version "$version") ; do
-    if is_stable_version "$tag" ; then
-      echo "--- :docker: Skipping tagging stable ${docker_image}:${tag}"
-      continue
-    fi
-    release_image "$tag${suffix}"
-  done
-fi
+  # variants of stable - e.g 2.3.2
+  if [[ "$CODENAME" == "stable" ]] ; then
+    for tag in latest stable $(parse_version "$version") ; do
+      release_image "$tag${suffix}"
+    done
+  fi
+
+  # variants of beta/unstable - e.g 3.0-beta.16
+  if [[ "$CODENAME" == "unstable" ]] ; then
+    for tag in beta $(parse_version "$version") ; do
+      if is_stable_version "$tag" ; then
+        echo "--- :docker: Skipping tagging stable ${docker_image}:${tag}"
+        continue
+      fi
+      release_image "$tag${suffix}"
+    done
+  fi
+done
