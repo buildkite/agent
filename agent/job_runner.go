c9d7dd6bea0ef5c8282bfa33a7396d72f1b1c78a
Lachlan Donald <lachlan@buildkite.com>
2018-07-17 12:04:48 +1000

    Read BUILDKITE_PLUGIN_VALIDATION from env

--- agent/job_runner.go
+++ agent/job_runner.go
@@ -308,11 +308,23 @@ func (r *JobRunner) createEnvironment() ([]string, error) {
 	env["BUILDKITE_GIT_SUBMODULES"] = fmt.Sprintf("%t", r.AgentConfiguration.GitSubmodules)
 	env["BUILDKITE_COMMAND_EVAL"] = fmt.Sprintf("%t", r.AgentConfiguration.CommandEval)
 	env["BUILDKITE_PLUGINS_ENABLED"] = fmt.Sprintf("%t", r.AgentConfiguration.PluginsEnabled)
-	env["BUILDKITE_PLUGIN_VALIDATION"] = fmt.Sprintf("%t", r.AgentConfiguration.PluginValidation)
 	env["BUILDKITE_LOCAL_HOOKS_ENABLED"] = fmt.Sprintf("%t", r.AgentConfiguration.LocalHooksEnabled)
 	env["BUILDKITE_GIT_CLONE_FLAGS"] = r.AgentConfiguration.GitCloneFlags
 	env["BUILDKITE_GIT_CLEAN_FLAGS"] = r.AgentConfiguration.GitCleanFlags
 	env["BUILDKITE_SHELL"] = r.AgentConfiguration.Shell
+
+	enablePluginValidation := r.AgentConfiguration.PluginValidation
+
+	// Allow BUILDKITE_PLUGIN_VALIDATION to be enabled from env for easier
+	// per-pipeline testing
+	if pluginValidation, ok := env["BUILDKITE_PLUGIN_VALIDATION"]; ok {
+		switch pluginValidation {
+		case "true", "1", "on":
+			enablePluginValidation = true
+		}
+	}
+
+	env["BUILDKITE_PLUGIN_VALIDATION"] = fmt.Sprintf("%t", enablePluginValidation)
 
 	// Convert the env map into a slice (which is what the script gear
 	// needs)
