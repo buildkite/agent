0de1be27aae983f9b556120c4ec9b44b65cc52a4
Lachlan Donald <lachlan@buildkite.com>
2019-01-02 16:06:08 +1100

    Add prefix to logs and tweak colors

--- logger/log.go
+++ logger/log.go
@@ -13,13 +13,14 @@ import (
 )
 
 const (
-	nocolor = "0"
-	red     = "31"
-	green   = "1;32"
-	yellow  = "33"
-	blue    = "34"
-	gray    = "1;30"
-	cyan    = "1;36"
+	nocolor   = "0"
+	red       = "31"
+	green     = "38;5;48"
+	yellow    = "33"
+	blue      = "34"
+	gray      = "38;5;251"
+	lightgray = "38;5;243"
+	cyan      = "1;36"
 )
 
 const (
@@ -55,18 +56,21 @@ func ColorsEnabled() bool {
 	if runtime.GOOS == "windows" {
 		// Boo, no colors on Windows.
 		return false
-	} else {
-		// Colors can only be shown if STDOUT is a terminal
-		if terminal.IsTerminal(int(os.Stdout.Fd())) {
-			return colors
-		} else {
-			return false
-		}
 	}
+
+	// Colors can only be shown if STDOUT is a terminal
+	if terminal.IsTerminal(int(os.Stdout.Fd())) {
+		return colors
+	}
+
+	return false
 }
 
-func (l *LevelLogger) SetLevel(v Level) {
-	l.Level = v
+// WithPrefix returns a copy of the logger with the provided prefix
+func (l *Logger) WithPrefix(prefix string) *Logger {
+	clone := *l
+	clone.Prefix = prefix
+	return &clone
 }
 
 func (l *Logger) Debug(format string, v ...interface{}) {
@@ -108,27 +112,35 @@ func (l *Logger) log(level Level, format string, v ...interface{}) {
 	line := ""
 
 	if l.Colors {
-		prefixColor := green
+		levelColor := green
 		messageColor := nocolor
 
 		switch level {
 		case DEBUG:
-			prefixColor = gray
+			levelColor = gray
 			messageColor = gray
 		case NOTICE:
-			prefixColor = cyan
+			levelColor = cyan
 		case WARN:
-			prefixColor = yellow
+			levelColor = yellow
 		case ERROR:
-			prefixColor = red
+			levelColor = red
 		case FATAL:
-			prefixColor = red
+			levelColor = red
 			messageColor = red
 		}
 
-		line = fmt.Sprintf("\x1b[%sm%s %-6s\x1b[0m \x1b[%sm%s\x1b[0m\n", prefixColor, now, level, messageColor, message)
+		if l.Prefix != "" {
+			line = fmt.Sprintf("\x1b[%sm%s %-6s\x1b[0m \x1b[%sm%s\x1b[0m \x1b[%sm%s\x1b[0m\n", levelColor, now, level, lightgray, l.Prefix, messageColor, message)
+		} else {
+			line = fmt.Sprintf("\x1b[%sm%s %-6s\x1b[0m \x1b[%sm%s\x1b[0m\n", levelColor, now, level, messageColor, message)
+		}
 	} else {
-		line = fmt.Sprintf("%s %-6s %s\n", now, level, message)
+		if l.Prefix != "" {
+			line = fmt.Sprintf("%s %-6s %s %s\n", now, level, l.Prefix, message)
+		} else {
+			line = fmt.Sprintf("%s %-6s %s\n", now, level, message)
+		}
 	}
 
 	// Make sure we're only outputing a line one at a time
