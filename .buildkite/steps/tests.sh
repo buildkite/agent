6d048772aafb1bd48ee545511d308b13a586c066
Lachlan Donald <lachlan@buildkite.com>
2019-03-06 15:39:58 +1100

    Fix typo

--- .buildkite/steps/tests.sh
+++ .buildkite/steps/tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-GO111MODULE=off get go get gotest.tools/gotestsum
+GO111MODULE=off go get gotest.tools/gotestsum
 
 echo '+++ Running tests'
 gotestsum --junitfile "junit-${OSTYPE}.xml" ./...
