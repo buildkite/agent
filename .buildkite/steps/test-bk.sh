b861adcda5627023e86a8be27e62a7c557dad11b
Lachlan Donald <lachlan@buildkite.com>
2019-03-14 21:33:36 +1100

    Try running bk test inside docker env

--- .buildkite/steps/test-bk.sh
+++ .buildkite/steps/test-bk.sh
@@ -12,22 +12,5 @@ buildkite-agent artifact download pkg/buildkite-agent-linux-amd64 .
 mv pkg/buildkite-agent-linux-amd64 pkg/buildkite-agent
 chmod +x pkg/buildkite-agent
 
-tmpdir="$(mktemp -d)"
-cleanup() {
-  rm -rf "$tmpdir"
-}
-trap cleanup EXIT
-
-BUILDKITE_AGENT_CONFIG="$tmpdir/buildkite-agent.cfg"
-BUILDKITE_PLUGINS_PATH="$tmpdir/plugins"
-BUILDKITE_HOOKS_PATH="$tmpdir/hooks"
-
-touch "$BUILDKITE_AGENT_CONFIG"
-mkdir -p "$BUILDKITE_PLUGINS_PATH" "$BUILDKITE_HOOKS_PATH"
-
-env -i \
-  BUILDKITE_AGENT_CONFIG="$BUILDKITE_AGENT_CONFIG" \
-  BUILDKITE_PLUGINS_PATH="$BUILDKITE_PLUGINS_PATH" \
-  BUILDKITE_HOOKS_PATH="$BUILDKITE_HOOKS_PATH" \
-  PATH="$PWD/pkg:$PATH" \
-  ./bk run --debug .buildkite/pipeline.bk-test.yml
+export PATH="$PWD/pkg:$PATH"
+./bk run --debug .buildkite/pipeline.bk-test.yml
