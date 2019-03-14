fd7af53084a850d6b5951aadf183d1b3d2816b82
Lachlan Donald <lachlan@buildkite.com>
2019-03-14 19:38:17 +1100

    Check the agent works with bk cli

---
+++ .buildkite/steps/test-bk.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "--- :package: Downloading bk binary"
+curl -Lfs -o bk https://github.com/buildkite/cli/releases/download/v0.4.1/bk-linux-amd64-0.4.1
+
+echo "--- :package: Downloading built binary"
+rm -rf pkg/*
+buildkite-agent artifact download pkg/buildkite-agent-linux-amd64 .
+mv pkg/buildkite-agent-linux-amd64 pkg/buildkite-agent
+chmod +x pkg/buildkite-agent
+
+export PATH="$PWD/pkg"
+./bk run .buildkite/pipeline.bk-test.yml
