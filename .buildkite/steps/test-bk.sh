eb4581962c0546b35f4585f728b14c49375b8eba
Lachlan Donald <lachlan@buildkite.com>
2019-03-14 21:07:27 +1100

    Run bk cli with clean environment

--- .buildkite/steps/test-bk.sh
+++ .buildkite/steps/test-bk.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
+
 echo "--- :package: Downloading bk binary"
 curl -Lfs -o bk https://github.com/buildkite/cli/releases/download/v0.4.1/bk-linux-amd64-0.4.1
 chmod +x ./bk
@@ -11,5 +12,22 @@ buildkite-agent artifact download pkg/buildkite-agent-linux-amd64 .
 mv pkg/buildkite-agent-linux-amd64 pkg/buildkite-agent
 chmod +x pkg/buildkite-agent
 
-export PATH="$PWD/pkg:$PATH"
-./bk run --debug .buildkite/pipeline.bk-test.yml
+tmpdir="$(mktemp -d)"
+cleanup() {
+  rm -rf "$tmpdir"
+}
+trap cleanup EXIT
+
+BUILDKITE_AGENT_CONFIG="$tmpdir/buildkite-agent.cfg"
+BUILDKITE_PLUGINS_PATH="$tmpdir/plugins"
+BUILDKITE_HOOKS_PATH="$tmpdir/hooks"
+
+touch "$BUILDKITE_AGENT_CONFIG"
+mkdir -p "$BUILDKITE_PLUGINS_PATH" "$BUILDKITE_HOOKS_PATH"
+
+env -i \
+  BUILDKITE_AGENT_CONFIG="$BUILDKITE_AGENT_CONFIG" \
+  BUILDKITE_PLUGINS_PATH="$BUILDKITE_PLUGINS_PATH" \
+  BUILDKITE_HOOKS_PATH="$BUILDKITE_HOOKS_PATH" \
+  PATH="$PWD/pkg:$PATH" \
+  ./bk run --debug .buildkite/pipeline.bk-test.yml
