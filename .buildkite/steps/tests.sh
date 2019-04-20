8eec396edc45ecc878fa1c8e2f70606d8aa01108
Lachlan Donald <lachlan@buildkite.com>
2019-04-20 21:22:34 +1000

    Only run git-mirrors-experiment test on master

--- .buildkite/steps/tests.sh
+++ .buildkite/steps/tests.sh
@@ -6,5 +6,7 @@ GO111MODULE=off go get gotest.tools/gotestsum
 echo '+++ Running tests'
 gotestsum --junitfile "junit-${OSTYPE}.xml" -- -count=1 -failfast ./...
 
-echo '+++ Running integration tests for git-mirrors experiment'
-TEST_EXPERIMENT=git-mirrors gotestsum --junitfile "junit-${OSTYPE}-git-mirrors.xml" -- -count=1 -failfast ./bootstrap/integration
+if [[ "$BUILDKITE_BRANCH" == "master" ]] ; then
+  echo '+++ Running integration tests for git-mirrors experiment'
+  TEST_EXPERIMENT=git-mirrors gotestsum --junitfile "junit-${OSTYPE}-git-mirrors.xml" -- -count=1 -failfast ./bootstrap/integration
+fi
