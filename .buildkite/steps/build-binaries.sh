ea69f7594fb95e805ad012ccc7288d5a61641f72
Lachlan Donald <lachlan@buildkite.com>
2019-04-20 21:09:34 +1000

    Build all binaries in parallel in a single step

---
+++ .buildkite/steps/build-binaries.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -euo pipefail
+
+GO111MODULE=off go get github.com/mitchellh/gox
+
+# Disable CGO completely
+export CGO_ENABLED=0
+
+# mkdir -p $BUILD_PATH
+# go build -v -ldflags "-X github.com/buildkite/agent/agent.buildVersion=$BUILD_VERSION" -o $BUILD_PATH/$BINARY_FILENAME *.go
+
+archs=(
+  windows/386
+  windows/amd64
+  linux/amd64
+  linux/386
+  linux/arm
+  linux/armhf
+  linux/arm64
+  linux/ppc64le
+  darwin/386
+  darwin/amd64
+  freebsd/amd64
+  freebsd/386
+  openbsd/amd64
+  openbsd/386
+  dragonfly/amd64
+)
+
+rm -rf pkg
+gox -output "pkg/buildkite-agent-{{.OS}}-{{.Arch}}" -osarch "${archs[*]}" .
+
+
--- .buildkite/steps/build-binary.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-echo "--- :${1}: Building ${1}/${2}"
-
-rm -rf pkg
-
-./scripts/build-binary.sh "${1}" "${2}" "${BUILDKITE_BUILD_NUMBER}"
