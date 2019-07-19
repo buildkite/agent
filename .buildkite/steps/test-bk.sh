e1daa15a4f9cd4e2c97d407341f87b316215b013
Lachlan Donald <lachlan@buildkite.com>
2019-07-19 10:33:44 +1000

    Bump cli in tests and drop soft_fail

--- .buildkite/steps/test-bk.sh
+++ .buildkite/steps/test-bk.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 
 echo "--- :package: Downloading bk binary"
-curl -Lfs -o bk https://github.com/buildkite/cli/releases/download/v0.4.1/bk-linux-amd64-0.4.1
+curl -Lfs -o bk https://github.com/buildkite/cli/releases/download/v1.0.0/bk-linux-amd64-1.0.0
 chmod +x ./bk
 
 echo "--- :package: Downloading built binary"
