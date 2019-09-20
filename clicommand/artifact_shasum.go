e1c62642c6e6a825525665afe8529731fa807a49
Tim Lucas <tim@buildkite.com>
2019-09-20 11:30:07 +1000

    Fix typo

--- clicommand/artifact_shasum.go
+++ clicommand/artifact_shasum.go
@@ -61,7 +61,7 @@ var ArtifactShasumCommand = cli.Command{
 		cli.StringFlag{
 			Name:  "step",
 			Value: "",
-			Usage: "Scope the search to a paticular step by using either it's name of job ID",
+			Usage: "Scope the search to a particular step by using either it's name of job ID",
 		},
 		cli.StringFlag{
 			Name:   "build",
