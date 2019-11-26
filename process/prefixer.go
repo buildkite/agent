1809888e93ce1df35c8924630e7b7f8421ccd5c2
Lachlan Donald <lachlan@buildkite.com>
2019-11-26 16:29:31 +1100

    Detect ansi clear lines and add ansi timestamps

--- process/prefixer.go
+++ process/prefixer.go
@@ -3,6 +3,8 @@ package process
 import (
 	"bytes"
 	"io"
+	"log"
+	"unicode/utf8"
 )
 
 type Prefixer struct {
@@ -32,19 +34,40 @@ func (p *Prefixer) Write(data []byte) (n int, err error) {
 	offset := 0
 	out := make([]byte, 0, len(data))
 
-	// loop through newlines
-	for offset < len(data) {
-		next := bytes.IndexRune(data[offset:], '\n')
+	// loop through line breaks and add prefix
+	for l := len(data); offset < l; {
+		// find either a newline or an escape char
+		next := bytes.IndexAny(data[offset:], "\n\x1b")
 		if next == -1 {
 			break
 		}
-		out = append(out, data[offset:offset+next+1]...)
-		out = append(out, []byte(p.f())...)
-		offset = offset + next + 1
+
+		// decode the first rune in the string of the match
+		r, _ := utf8.DecodeRune(data[offset+next:])
+		switch r {
+		case '\n':
+			out = append(out, data[offset:offset+next+1]...)
+			out = append(out, []byte(p.f())...)
+			offset = offset + next + 1
+		case '\x1b':
+			// match a clear line escape
+			if bytes.HasPrefix(data[offset+next+1:], []byte("[K")) {
+				out = append(out, data[offset:offset+next+3]...)
+				out = append(out, []byte(p.f())...)
+				offset = offset + next + 3
+			} else {
+				out = append(out, data[offset:offset+next+1]...)
+				offset = offset + next + 1
+			}
+		default:
+			out = append(out, data[offset:offset+next+1]...)
+			offset = offset + next + 1
+		}
 	}
 
 	// add any left overs
 	if offset < len(data) {
+		log.Printf("Found leftovers %q", data[offset:])
 		out = append(out, data[offset:]...)
 	}
 
