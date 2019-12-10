843f43855648625c13382b6b643fbb73f30bead4
Keith Pitt <me@keithpitt.com>
2019-12-10 16:32:29 +0800

    Added a jobs API method to acquire a job given an id

--- api/jobs.go
+++ api/jobs.go
@@ -49,6 +49,24 @@ func (c *Client) GetJobState(id string) (*JobState, *Response, error) {
 	return s, resp, err
 }
 
+// Acquires a job using it's ID
+func (c *Client) AcquireJob(id string) (*Job, *Response, error) {
+	u := fmt.Sprintf("jobs/%s/acquire", id)
+
+	req, err := c.newRequest("PUT", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	j := new(Job)
+	resp, err := c.doRequest(req, j)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return j, resp, err
+}
+
 // AcceptJob accepts the passed in job. Returns the job with it's finalized set of
 // environment variables (when a job is accepted, the agents environment is
 // applied to the job)
