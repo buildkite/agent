6159e00274fa85507ef472f3dac01ee9a10bfa90
Paul Annesley <paul@annesley.cc>
2020-07-14 19:54:20 +1000

    bootstrap: prefer early return to indent

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -247,35 +247,37 @@ func (b *Bootstrap) applyEnvironmentChanges(environ *env.Environment, dir string
 	}
 
 	// Do we even have any environment variables to change?
-	if environ != nil && environ.Length() > 0 {
-		// First, let see any of the environment variables are supposed
-		// to change the bootstrap configuration at run time.
-		bootstrapConfigEnvChanges := b.Config.ReadFromEnvironment(environ)
-
-		// Print out the env vars that changed. As we go through each
-		// one, we'll determine if it was a special "bootstrap"
-		// environment variable that has changed the bootstrap
-		// configuration at runtime.
-		//
-		// If it's "special", we'll show the value it was changed to -
-		// otherwise we'll hide it. Since we don't know if an
-		// environment variable contains sensitive information (i.e.
-		// THIRD_PARTY_API_KEY) we'll just not show any values for
-		// anything not controlled by us.
-		for k, v := range environ.ToMap() {
-			_, ok := bootstrapConfigEnvChanges[k]
-			if ok {
-				b.shell.Commentf("%s is now %q", k, v)
-			} else {
-				b.shell.Commentf("%s changed", k)
-			}
-		}
-
-		// Now that we've finished telling the user what's changed,
-		// let's mutate the current shell environment to include all
-		// the new values.
-		b.shell.Env = b.shell.Env.Merge(environ)
+	if environ == nil || environ.Length() == 0 {
+		return
 	}
+
+	// First, let see any of the environment variables are supposed
+	// to change the bootstrap configuration at run time.
+	bootstrapConfigEnvChanges := b.Config.ReadFromEnvironment(environ)
+
+	// Print out the env vars that changed. As we go through each
+	// one, we'll determine if it was a special "bootstrap"
+	// environment variable that has changed the bootstrap
+	// configuration at runtime.
+	//
+	// If it's "special", we'll show the value it was changed to -
+	// otherwise we'll hide it. Since we don't know if an
+	// environment variable contains sensitive information (i.e.
+	// THIRD_PARTY_API_KEY) we'll just not show any values for
+	// anything not controlled by us.
+	for k, v := range environ.ToMap() {
+		_, ok := bootstrapConfigEnvChanges[k]
+		if ok {
+			b.shell.Commentf("%s is now %q", k, v)
+		} else {
+			b.shell.Commentf("%s changed", k)
+		}
+	}
+
+	// Now that we've finished telling the user what's changed,
+	// let's mutate the current shell environment to include all
+	// the new values.
+	b.shell.Env = b.shell.Env.Merge(environ)
 }
 
 // Returns the absolute path to the best matching hook file in a path, or os.ErrNotExist if none is found
