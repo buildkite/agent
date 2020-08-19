266e44e6fb90b6a97f16a4115dadc4433d39791b
Paul Annesley <paul@annesley.cc>
2020-08-19 17:10:46 +1000

    experiments: local var name refactor
    This is mostly to make the implementation of Enabled() clearer.
    
    Idiomatic Go uses `ok` to indicate whether a key _existed_ in a map,
    e.g. `value, ok := experiments["foo"]`.
    Whereas we were using it to refer to the boolean _value_ of the map in
    the context of a range, i.e. `for key, ok := range experiments`.
    
    And `var enabled []string` as the accumulator list of keys which are
    enabled is problematic because it isn't plural, and the same `enabled`
    name could also refer to a boolean value in the `map[string]bool`. So
    this patch renames the list to `keys`, and also uses `key` instead of
    `experiment` everywhere to refer to experiment keys.
    
    With the name `enabled` no longer being used for a list of enabled keys,
    this patch uses `enabled` instead of `ok`, resulting in a clearer
    `if enabled { ... }`.

--- experiments/experiments.go
+++ experiments/experiments.go
@@ -3,27 +3,27 @@ package experiments
 var experiments = make(map[string]bool)
 
 // Enable a particular experiment in the agent
-func Enable(experiment string) {
-	experiments[experiment] = true
+func Enable(key string) {
+	experiments[key] = true
 }
 
 // Disable a particular experiment in the agent
-func Disable(experiment string) {
-	delete(experiments, experiment)
+func Disable(key string) {
+	delete(experiments, key)
 }
 
 // IsEnabled returns whether the named experiment is enabled
-func IsEnabled(experiment string) bool {
-	return experiments[experiment] // map[T]bool returns false for missing keys
+func IsEnabled(key string) bool {
+	return experiments[key] // map[T]bool returns false for missing keys
 }
 
 // Enabled returns the keys of all the enabled experiments
 func Enabled() []string {
-	var enabled []string
-	for exp, ok := range experiments {
-		if ok {
-			enabled = append(enabled, exp)
+	var keys []string
+	for key, enabled := range experiments {
+		if enabled {
+			keys = append(keys, key)
 		}
 	}
-	return enabled
+	return keys
 }
