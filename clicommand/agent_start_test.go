c04d65d6618ae4a9089b0a85e8bc5f5ca59b3653
Paul Annesley <paul@annesley.cc>
2020-10-13 16:12:36 +1100

    shutdown hook: pipe output lines to main logger

--- clicommand/agent_start_test.go
+++ clicommand/agent_start_test.go
@@ -22,7 +22,12 @@ func setupHooksPath() (string, func(), error) {
 // Since the hook doesn't really return anything, we can't really test stuff but at
 // least make sure the code doesn't explode.
 func TestShutdownHook(t *testing.T) {
-
+	cfg := func(hooksPath string) AgentStartConfig {
+		return AgentStartConfig{
+			HooksPath: hooksPath,
+			NoColor:   true,
+		}
+	}
 	t.Run("with shutdown hook", func(t *testing.T) {
 		hooksPath, closer, err := setupHooksPath()
 		if err != nil {
@@ -34,16 +39,19 @@ func TestShutdownHook(t *testing.T) {
 		if runtime.GOOS == "windows" {
 			filename = "shutdown.bat"
 		}
-		err = ioutil.WriteFile(filepath.Join(hooksPath, filename), []byte("echo hello"), 0755)
+		filepath := filepath.Join(hooksPath, filename)
+		err = ioutil.WriteFile(filepath, []byte("echo hello"), 0755)
 		if err != nil {
 			assert.FailNow(t, "failed to write shutdown hook: %v", err)
 		}
 
 		log := logger.NewBuffer()
-		shutdownHook(log, hooksPath)
-		assert.Equal(t, []string{}, log.Messages)
+		shutdownHook(log, cfg(hooksPath))
+		assert.Equal(t, []string{
+			"[info] $ " + filepath, // prompt
+			"[info] hello",         // output
+		}, log.Messages)
 	})
-
 	t.Run("with no shutdown hook", func(t *testing.T) {
 		hooksPath, closer, err := setupHooksPath()
 		if err != nil {
@@ -52,13 +60,12 @@ func TestShutdownHook(t *testing.T) {
 		defer closer()
 
 		log := logger.NewBuffer()
-		shutdownHook(log, hooksPath)
+		shutdownHook(log, cfg(hooksPath))
 		assert.Equal(t, []string{}, log.Messages)
 	})
-
 	t.Run("with bad hooks path", func(t *testing.T) {
 		log := logger.NewBuffer()
-		shutdownHook(log, "zxczxczxc")
+		shutdownHook(log, cfg("zxczxczxc"))
 		assert.Equal(t, []string{}, log.Messages)
 	})
 }
