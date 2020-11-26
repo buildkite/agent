05402a6100147dc7dc634cb94a7ae8c0ef9ce128
gbi <plp.github@gmail.com>
2020-11-26 18:05:29 +0200

    install: use the arm64 binary for aarch64 machines
    Enable the install script to use the arm64 binary when uname reports that the machine is aarch64.

--- install.sh
+++ install.sh
@@ -77,6 +77,7 @@ else
     *armv6*)   ARCH="armhf"   ;;
     *arm*)     ARCH="arm"     ;;
     *ppc64le*) ARCH="ppc64le" ;;
+    *aarch64*) ARCH="arm64"   ;;
     *)
       ARCH="386"
       echo -e "\n\033[36mWe don't recognise the $MACHINE architecture; falling back to $ARCH\033[0m"
