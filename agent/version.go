1de6cc843eeeb3834e4ae60508c292626ab9bf30
Jessica Stokes <hello@jessicastokes.net>
2021-02-22 17:13:58 -0800

    Fix the current beta version

--- agent/version.go
+++ agent/version.go
@@ -7,8 +7,10 @@ import "runtime"
 //  go run -ldflags "-X github.com/buildkite/agent/v3/agent.buildVersion abc" *.go --version
 //
 // On CI, the binaries are always build with the buildVersion variable set.
+//
+// Pre-release builds' versions must be in the format `x.y-beta`, `x.y-beta.z` or `x.y-beta.z.a`
 
-var baseVersion string = "3.28.0.pre"
+var baseVersion string = "3.28.0-beta.1"
 var buildVersion string = ""
 
 func Version() string {
