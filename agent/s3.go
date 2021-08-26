3e55ecbf135f4f208fc6c1764f91a873fb1211bc
Keith Duncan <keith.d@buildkite.com>
2021-08-26 12:54:00 +1000

    Elaborate on the credential chain error

--- agent/s3.go
+++ agent/s3.go
@@ -129,9 +129,18 @@ func newS3Client(l logger.Logger, bucket string) (*s3.S3, error) {
 	})
 	if err != nil {
 		if err == credentials.ErrNoValidProvidersFoundInChain {
-			return nil, fmt.Errorf("Could not find a valid authentication strategy to connect to S3. Try setting BUILDKITE_S3_ACCESS_KEY and BUILDKITE_S3_SECRET_KEY")
+			hasProxy := os.Getenv("HTTP_PROXY") != "" || os.Getenv("HTTPS_PROXY") != ""
+			hasNoProxyIdmsException := os.Getenv("NO_PROXY").Contains("169.254.169.254")
+
+			errorTitle := "Could not authenticate with AWS S3 using any of the included credential providers."
+
+			if hasProxy && !hasNoProxyIdmsException {
+				return nil, fmt.Errorf("%s Your HTTP proxy settings do not grant a NO_PROXY=169.254.169.254 exemption for the instance metadata service, instance profile credentials may not be retrievable via your HTTP proxy.", errorTitle)
+			}
+
+			return nil, fmt.Errorf("%s You can authenticate by setting Buildkite environment variables (BUILDKITE_S3_ACCESS_KEY_ID, BUILDKITE_S3_SECRET_ACCESS_KEY), AWS environment variables (AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY), Web Identity environment variables (AWS_ROLE_ARN, AWS_ROLE_SESSION_NAME, AWS_WEB_IDENTITY_TOKEN_FILE), or if running on AWS EC2 ensuring network access to the EC2 Instance Metadata Service to use an instance profile’s IAM Role credentials.", errorTitle)
 		}
-		return nil, fmt.Errorf("Failed to authenticate to bucket `%s` in region `%s` (%s)", bucket, region, err.Error())
+		return nil, fmt.Errorf("Could not s3:ListObjects in your AWS S3 bucket `%s` in region `%s`: (%s)", bucket, region, err.Error())
 	}
 
 	return s3client, nil
