7a8e1ce15edc72ce1bbc16e9c7fde7b07e624809
Keith Pitt <me@keithpitt.com>
2022-05-25 07:57:58 +0800

    Make sure that exit statuses are preserved

--- .buildkite/steps/tests.sh
+++ .buildkite/steps/tests.sh
@@ -6,15 +6,20 @@ echo arch is "$(uname -m)"
 go install gotest.tools/gotestsum@v1.8.0
 
 echo '+++ Running tests'
-gotestsum --junitfile "junit-${OSTYPE}.xml" -- -count=1 "$@" ./...
+gotestsum --junitfile "junit-${OSTYPE}.xml" -- -count=1 "$@" ./... || TEST_EXIT_STATUS=$?
 
 echo '+++ Running integration tests for git-mirrors experiment'
-TEST_EXPERIMENT=git-mirrors gotestsum --junitfile "junit-${OSTYPE}-git-mirrors.xml" -- -count=1 "$@" ./bootstrap/integration
+TEST_EXPERIMENT=git-mirrors gotestsum --junitfile "junit-${OSTYPE}-git-mirrors.xml" -- -count=1 "$@" ./bootstrap/integration || TEST_EXIT_STATUS_WITH_MIRROS=$?
 
 echo '+++ Test Analytics'
-
 if [[ -n "${TEST_ANALYTICS_TOKEN_ENV_KEY-}" ]]; then
   cat "junit-${OSTYPE}.xml" | TEST_ANALYTICS_TOKEN=${!TEST_ANALYTICS_TOKEN_ENV_KEY} bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/collector-junit/main/collect.sh`"
 else
   echo "No TEST_ANALYTICS_TOKEN_ENV_KEY is present, skipping..."
+fi
+
+if [[ $TEST_EXIT_STATUS -ne 0 ]]; then
+  exit $TEST_EXIT_STATUS
+elif [[ $TEST_EXIT_STATUS_WITH_MIRROS -ne 0 ]]; then
+  exit $TEST_EXIT_STATUS_WITH_MIRROS
 fi
