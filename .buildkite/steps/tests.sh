7bb2c44f398647b6f0603c7741840cf4793a7d24
Keith Pitt <me@keithpitt.com>
2022-05-24 18:02:48 +0800

    Send junit results to test analytics

--- .buildkite/steps/tests.sh
+++ .buildkite/steps/tests.sh
@@ -11,3 +11,11 @@ gotestsum --junitfile "junit-${OSTYPE}.xml" -- -count=1 -failfast "$@" ./...
 
 echo '+++ Running integration tests for git-mirrors experiment'
 TEST_EXPERIMENT=git-mirrors gotestsum --junitfile "junit-${OSTYPE}-git-mirrors.xml" -- -count=1 -failfast "$@" ./bootstrap/integration
+
+echo '+++ Test Analytics'
+
+if [[ -n $TEST_ANALYTICS_TOKEN_ENV_KEY ]]; then
+  cat "junit-${OSTYPE}.xml" | TEST_ANALYTICS_TOKEN=${!TEST_ANALYTICS_TOKEN_ENV_KEY} bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/collector-junit/main/collect.sh`"
+else
+  echo "No TEST_ANALYTICS_TOKEN_ENV_KEY is present, skipping..."
+fi
