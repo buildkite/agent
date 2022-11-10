f947ad173507aec6de5f726f07fb49b497682993
Narthana Epa <narthana.epa@gmail.com>
2022-11-10 17:09:12 +1100

    Add table test for 0, 1 and 2 audiences

--- api/oidc_test.go
+++ api/oidc_test.go
@@ -1,6 +1,7 @@
 package api_test
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -10,7 +11,7 @@ import (
 	"github.com/buildkite/agent/v3/logger"
 )
 
-func TestOidcTokenNoAudience(t *testing.T) {
+func TestOidcToken(t *testing.T) {
 	const jobId = "b078e2d2-86e9-4c12-bf3b-612a8058d0a4"
 	const oidcToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.NHVaYe26MbtOYhSKkoKYdFVomg4i8ZJd8_-RU8VNbftc4TSMb4bXP3l3YlNWACwyXPGffz5aXHc6lty1Y2t4SWRqGteragsVdZufDn5BlnJl9pdR_kdVFUsra2rWKEofkZeIC4yWytE58sMIihvo9H1ScmmVwBcQP6XETqYd0aSHp1gOa9RdUPDvoXQ5oqygTqVtxaDr6wUFKrKItgBMzWIdNZ6y7O9E0DhEPTbE9rfBo6KTFsHAZnMg4k68CDp2woYIaXbmYTWcvbzIuHO7_37GT79XdIwkm95QJ7hYC9RiwrV7mesbY4PAahERJawntho0my942XheVLmGwLMBkQ"
 	const accessToken = "llamas"
@@ -52,11 +53,37 @@ func TestOidcTokenNoAudience(t *testing.T) {
 		DebugHTTP: true,
 	})
 
-	if token, resp, err := client.OidcToken(jobId); err != nil {
-		t.Fatalf("OidcToken(%v) error = %# v", jobId, err)
-	} else if token.Token != oidcToken {
-		t.Fatalf("OidcToken(%v) got token = %# v, want %# v", jobId, token, &api.OidcToken{Token: oidcToken})
-	} else if resp.StatusCode != http.StatusOK {
-		t.Fatalf("OidcToken(%v) got StatusCode = %# v, want %# v", jobId, resp.StatusCode, http.StatusOK)
+	for _, testData := range []struct {
+		JobId       string
+		AccessToken string
+		Audience    []string
+		Error       error
+	}{
+		{
+			JobId:       jobId,
+			AccessToken: accessToken,
+			Audience:    []string{},
+		},
+		{
+			JobId:       jobId,
+			AccessToken: accessToken,
+			Audience:    []string{"sts.amazonaws.com"},
+		},
+		{
+			JobId:       jobId,
+			AccessToken: accessToken,
+			Audience:    []string{"sts.amazonaws.com", "buildkite.com"},
+			Error:       api.ErrAudienceTooLong,
+		},
+	} {
+		if token, resp, err := client.OidcToken(testData.JobId, testData.Audience...); err != nil {
+			if !errors.Is(err, testData.Error) {
+				t.Fatalf("OidcToken(%v, %v) got error = %# v, want error = %# v", testData.JobId, testData.Audience, err, testData.Error)
+			}
+		} else if token.Token != oidcToken {
+			t.Fatalf("OidcToken(%v, %v) got token = %# v, want %# v", testData.JobId, testData.Audience, token, &api.OidcToken{Token: oidcToken})
+		} else if resp.StatusCode != http.StatusOK {
+			t.Fatalf("OidcToken(%v, %v) got StatusCode = %# v, want %# v", testData.JobId, testData.Audience, resp.StatusCode, http.StatusOK)
+		}
 	}
 }
