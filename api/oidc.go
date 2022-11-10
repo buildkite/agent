b11ea30dd5352e96e5de46f8960895d62478fa95
Narthana Epa <narthana.epa@gmail.com>
2022-11-10 17:38:07 +1100

    Make OidcTokenRequest a local struct

--- api/oidc.go
+++ api/oidc.go
@@ -7,21 +7,21 @@ import (
 
 var ErrAudienceTooLong = errors.New("the API only supports at most one element in the audience")
 
-type OidcTokenRequest struct {
-	Audience string `json:"audience"`
-}
-
 type OidcToken struct {
 	Token string `json:"token"`
 }
 
 func (c *Client) OidcToken(jobId string, audience ...string) (*OidcToken, *Response, error) {
-	var m *OidcTokenRequest
+	type oidcTokenRequest struct {
+		Audience string `json:"audience"`
+	}
+
+	var m *oidcTokenRequest
 	switch len(audience) {
 	case 0:
 		m = nil
 	case 1:
-		m = &OidcTokenRequest{Audience: audience[0]}
+		m = &oidcTokenRequest{Audience: audience[0]}
 	default:
 		return nil, nil, ErrAudienceTooLong
 	}
