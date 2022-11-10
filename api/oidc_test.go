8753f8af6d58294682ef3b05f53777dcb4d01186
Narthana Epa <narthana.epa@gmail.com>
2022-11-11 00:25:42 +1100

    Test the request body created for Oidc Token API calls

--- api/oidc_test.go
+++ api/oidc_test.go
@@ -1,8 +1,10 @@
 package api_test
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -11,13 +13,14 @@ import (
 	"github.com/buildkite/agent/v3/logger"
 )
 
-func TestOidcToken(t *testing.T) {
-	const jobId = "b078e2d2-86e9-4c12-bf3b-612a8058d0a4"
-	const oidcToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.NHVaYe26MbtOYhSKkoKYdFVomg4i8ZJd8_-RU8VNbftc4TSMb4bXP3l3YlNWACwyXPGffz5aXHc6lty1Y2t4SWRqGteragsVdZufDn5BlnJl9pdR_kdVFUsra2rWKEofkZeIC4yWytE58sMIihvo9H1ScmmVwBcQP6XETqYd0aSHp1gOa9RdUPDvoXQ5oqygTqVtxaDr6wUFKrKItgBMzWIdNZ6y7O9E0DhEPTbE9rfBo6KTFsHAZnMg4k68CDp2woYIaXbmYTWcvbzIuHO7_37GT79XdIwkm95QJ7hYC9RiwrV7mesbY4PAahERJawntho0my942XheVLmGwLMBkQ"
-	const accessToken = "llamas"
+func newOidcTokenServer(
+	t *testing.T,
+	accessToken, oidcToken, path string,
+	expectedBody []byte,
+) *httptest.Server {
+	t.Helper()
 
-	path := fmt.Sprintf("/jobs/%s/oidc/tokens", jobId)
-	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+	return httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		switch req.URL.Path {
 		case path:
 			if got, want := authToken(req), accessToken; got != want {
@@ -28,14 +31,38 @@ func TestOidcToken(t *testing.T) {
 				)
 				return
 			}
-			rw.WriteHeader(http.StatusOK)
-			fmt.Fprint(rw, fmt.Sprintf(`{"token":"%s"}`, oidcToken))
+
+			body, err := io.ReadAll(req.Body)
+			if err != nil {
+				http.Error(
+					rw,
+					fmt.Sprintf(`{"message:"Internal Server Error: %q"}`, err),
+					http.StatusInternalServerError,
+				)
+				return
+			}
+
+			if !bytes.Equal(body, expectedBody) {
+				t.Errorf("wanted = %q, got = %q", expectedBody, body)
+				http.Error(
+					rw,
+					fmt.Sprintf(
+						`{"message:"Bad Request: wanted = %q, got = %q"}`,
+						expectedBody,
+						body,
+					),
+					http.StatusBadRequest,
+				)
+				return
+			}
+
+			io.WriteString(rw, fmt.Sprintf(`{"token":"%s"}`, oidcToken))
 
 		default:
 			http.Error(
 				rw,
 				fmt.Sprintf(
-					`{"message": "not found; method = %q, path = %q"}`,
+					`{"message":"Not Found; method = %q, path = %q"}`,
 					req.Method,
 					req.URL.Path,
 				),
@@ -43,46 +70,67 @@ func TestOidcToken(t *testing.T) {
 			)
 		}
 	}))
-	defer server.Close()
+}
 
-	// Initial client with a registration token
-	client := api.NewClient(logger.Discard, api.Config{
-		UserAgent: "Test",
-		Endpoint:  server.URL,
-		Token:     accessToken,
-		DebugHTTP: true,
-	})
+func TestOidcToken(t *testing.T) {
+	const jobId = "b078e2d2-86e9-4c12-bf3b-612a8058d0a4"
+	const oidcToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.NHVaYe26MbtOYhSKkoKYdFVomg4i8ZJd8_-RU8VNbftc4TSMb4bXP3l3YlNWACwyXPGffz5aXHc6lty1Y2t4SWRqGteragsVdZufDn5BlnJl9pdR_kdVFUsra2rWKEofkZeIC4yWytE58sMIihvo9H1ScmmVwBcQP6XETqYd0aSHp1gOa9RdUPDvoXQ5oqygTqVtxaDr6wUFKrKItgBMzWIdNZ6y7O9E0DhEPTbE9rfBo6KTFsHAZnMg4k68CDp2woYIaXbmYTWcvbzIuHO7_37GT79XdIwkm95QJ7hYC9RiwrV7mesbY4PAahERJawntho0my942XheVLmGwLMBkQ"
+	const accessToken = "llamas"
 
 	for _, testData := range []struct {
-		JobId       string
-		AccessToken string
-		OidcToken   *api.OidcToken
-		Audience    []string
-		Error       error
+		JobId        string
+		AccessToken  string
+		Audience     []string
+		ExpectedBody []byte
+		OidcToken    *api.OidcToken
+		Error        error
 	}{
 		{
-			JobId:       jobId,
-			AccessToken: accessToken,
-			OidcToken:   &api.OidcToken{Token: oidcToken},
-			Audience:    []string{},
+			JobId:        jobId,
+			AccessToken:  accessToken,
+			Audience:     []string{},
+			ExpectedBody: []byte("{}\n"),
+			OidcToken:    &api.OidcToken{Token: oidcToken},
 		},
 		{
 			JobId:       jobId,
 			AccessToken: accessToken,
-			OidcToken:   &api.OidcToken{Token: oidcToken},
 			Audience:    []string{"sts.amazonaws.com"},
+			ExpectedBody: []byte(`{"audience":"sts.amazonaws.com"}
+`),
+			OidcToken: &api.OidcToken{Token: oidcToken},
 		},
 		{
 			JobId:       jobId,
 			AccessToken: accessToken,
 			Audience:    []string{"sts.amazonaws.com", "buildkite.com"},
+			OidcToken:   &api.OidcToken{Token: oidcToken},
 			Error:       api.ErrAudienceTooLong,
 		},
 	} {
+		path := fmt.Sprintf("/jobs/%s/oidc/tokens", testData.JobId)
+
+		server := newOidcTokenServer(
+			t,
+			testData.AccessToken,
+			testData.OidcToken.Token,
+			path,
+			testData.ExpectedBody,
+		)
+		defer server.Close()
+
+		// Initial client with a registration token
+		client := api.NewClient(logger.Discard, api.Config{
+			UserAgent: "Test",
+			Endpoint:  server.URL,
+			Token:     accessToken,
+			DebugHTTP: true,
+		})
+
 		if token, resp, err := client.OidcToken(testData.JobId, testData.Audience...); err != nil {
 			if !errors.Is(err, testData.Error) {
 				t.Fatalf(
-					"OidcToken(%v, %v) got error = %# v, want error = %# v",
+					"OidcToken(%v, %v) got error = %v, want error = %v",
 					testData.JobId,
 					testData.Audience,
 					err,
@@ -90,9 +138,9 @@ func TestOidcToken(t *testing.T) {
 				)
 			}
 		} else if token.Token != oidcToken {
-			t.Fatalf("OidcToken(%v, %v) got token = %# v, want %# v", testData.JobId, testData.Audience, token, testData.OidcToken)
+			t.Fatalf("OidcToken(%v, %v) got token = %v, want %v", testData.JobId, testData.Audience, token, testData.OidcToken)
 		} else if resp.StatusCode != http.StatusOK {
-			t.Fatalf("OidcToken(%v, %v) got StatusCode = %# v, want %# v", testData.JobId, testData.Audience, resp.StatusCode, http.StatusOK)
+			t.Fatalf("OidcToken(%v, %v) got StatusCode = %v, want %v", testData.JobId, testData.Audience, resp.StatusCode, http.StatusOK)
 		}
 	}
 }
