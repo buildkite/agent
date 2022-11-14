e22417c631b3ee0b38d5c11b57a3db5b386165e4
Narthana Epa <narthana.epa@gmail.com>
2022-11-14 13:58:58 +1100

    Change test to have single source of truth for expected audience

--- api/oidc_test.go
+++ api/oidc_test.go
@@ -77,6 +77,7 @@ func TestOIDCToken(t *testing.T) {
 	const jobId = "b078e2d2-86e9-4c12-bf3b-612a8058d0a4"
 	const oidcToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.NHVaYe26MbtOYhSKkoKYdFVomg4i8ZJd8_-RU8VNbftc4TSMb4bXP3l3YlNWACwyXPGffz5aXHc6lty1Y2t4SWRqGteragsVdZufDn5BlnJl9pdR_kdVFUsra2rWKEofkZeIC4yWytE58sMIihvo9H1ScmmVwBcQP6XETqYd0aSHp1gOa9RdUPDvoXQ5oqygTqVtxaDr6wUFKrKItgBMzWIdNZ6y7O9E0DhEPTbE9rfBo6KTFsHAZnMg4k68CDp2woYIaXbmYTWcvbzIuHO7_37GT79XdIwkm95QJ7hYC9RiwrV7mesbY4PAahERJawntho0my942XheVLmGwLMBkQ"
 	const accessToken = "llamas"
+	const audience = "sts.amazonaws.com"
 
 	tests := []struct {
 		OIDCTokenRequest *api.OIDCTokenRequest
@@ -97,11 +98,10 @@ func TestOIDCToken(t *testing.T) {
 			AccessToken: accessToken,
 			OIDCTokenRequest: &api.OIDCTokenRequest{
 				JobId:    jobId,
-				Audience: "sts.amazonaws.com",
+				Audience: audience,
 			},
-			ExpectedBody: []byte(`{"audience":"sts.amazonaws.com"}
-`),
-			OIDCToken: &api.OIDCToken{Token: oidcToken},
+			ExpectedBody: []byte(fmt.Sprintf(`{"audience":%q}`+"\n", audience)),
+			OIDCToken:    &api.OIDCToken{Token: oidcToken},
 		},
 	}
 
