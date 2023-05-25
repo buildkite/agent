73f39e4a48cded7aef4afc6e15ff75d292f1a27d
Josh Deprez <jd@buildkite.com>
2023-05-25 12:23:12 +1000

    Move os.MkdirAll to socket.NewServer
    Unless `job-api` was used previously, there's little chance of the socket directory existing. If the directory doesn't exist, enabling `agent-api` crashes the agent on startup. Using MkdirAll in NewServer ensures the directory exists.

--- jobapi/socket.go
+++ jobapi/socket.go
@@ -15,12 +15,6 @@ func init() {
 // NewSocketPath generates a path to a socket file (without actually creating the file itself) that can be used with the
 // job api.
 func NewSocketPath(base string) (string, error) {
-	path := filepath.Join(base, "job-api")
-	err := os.MkdirAll(path, 0700)
-	if err != nil {
-		return "", fmt.Errorf("creating socket directory: %w", err)
-	}
-
 	sockNum := rand.Int63() % 100_000
-	return filepath.Join(path, fmt.Sprintf("%d-%d.sock", os.Getpid(), sockNum)), nil
+	return filepath.Join(base, "job-api", fmt.Sprintf("%d-%d.sock", os.Getpid(), sockNum)), nil
 }
