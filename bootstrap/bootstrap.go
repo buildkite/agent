60e81fcd7498c27fa5bfea6a46cac43a8b4dad9b
Narthana Epa <narthana.epa@gmail.com>
2023-05-30 15:57:51 +1000

    Downgrade deprecation message from Warning
    The deprecation message isn't something that is actionable by people who
    create builds (builders). It's only relevant if the plugin is depending on the
    deprecated variable name, and the builder would not normally be aware of
    this. Furthermore, it's often the case that the plugin will work fine
    even though the environment variable name is "deprecated", so this does
    not need to be a warning.

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -858,7 +858,7 @@ func (b *Bootstrap) executePluginHook(ctx context.Context, name string, checkout
 		envMap, err := p.ConfigurationToEnvironment()
 		if dnerr := (&plugin.DeprecatedNameErrors{}); err != nil && errors.As(err, &dnerr) {
 			b.shell.Logger.Headerf("Deprecated environment variables for plugin %s", p.Plugin.Name())
-			b.shell.Logger.Warningf(
+			b.shell.Logger.Printf(
 				"The configuration would export environment variables with the following names in a deprecated manner." +
 					"We'll export both the deprecated and the replacement names, but this behaviour may change in future",
 			)
