f6042f589f42485c8ae42a714534183e378931b6
Narthana Epa <narthana.epa@gmail.com>
2023-05-30 16:08:31 +1000

    Update message

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -859,8 +859,10 @@ func (b *Bootstrap) executePluginHook(ctx context.Context, name string, checkout
 		if dnerr := (&plugin.DeprecatedNameErrors{}); err != nil && errors.As(err, &dnerr) {
 			b.shell.Logger.Headerf("Deprecated environment variables for plugin %s", p.Plugin.Name())
 			b.shell.Logger.Printf(
-				"The configuration would export environment variables with the following names in a deprecated manner." +
-					"We'll export both the deprecated and the replacement names, but this behaviour may change in future",
+				"The way that environment variables are derived from the plugin configuration is changing." +
+					"We'll export both the deprecated and the replacement names for now." +
+					"You may be able avoid this by removing consecutive underscore, hypen, or whitespace" +
+					"characters in your plugin configuration.",
 			)
 			for _, err := range dnerr.Errors() {
 				b.shell.Logger.Printf("%s", err.Error())
