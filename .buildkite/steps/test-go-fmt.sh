788fd477e413be29d18284ee81a488992e6b7cba
Ben Moskovitz <ben.m@buildkite.com>
2023-07-26 10:12:10 +1000

    Check that go.mod is tidy in CI

--- .buildkite/steps/test-go-fmt.sh
+++ .buildkite/steps/test-go-fmt.sh
@@ -8,4 +8,10 @@ if [[ $(gofmt -l ./ | head -c 1 | wc -c) != 0 ]]; then
   exit 1
 fi
 
+if [[ $(go mod tidy -v 2> >(wc -c)) != 0 ]]; then # go mod tidy -v outputs to stderr for some reason
+  echo "The go.mod file is out of sync with the source code"
+  echo "Please run \`go mod tidy\` locally, and commit the result."
+  exit 1
+fi
+
 echo "Everything is formatted! 🎉"
