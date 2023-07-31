4746e6a6b14876695cdb21ca17abedd90220c680
Ben Moskovitz <ben.m@buildkite.com>
2023-07-31 10:39:51 +1000

    Noisier go mod tidy error

--- .buildkite/steps/test-go-fmt.sh
+++ .buildkite/steps/test-go-fmt.sh
@@ -8,8 +8,12 @@ if [[ $(gofmt -l ./ | head -c 1 | wc -c) != 0 ]]; then
   exit 1
 fi
 
-if [[ $(go mod tidy -v 2> >(wc -c)) != 0 ]]; then # go mod tidy -v outputs to stderr for some reason
+tidy_output=$(go mod tidy -v 2>&1)
+
+if [[ "${#tidy_output}" -gt 0 ]]; then # go mod tidy -v outputs to stderr for some reason
   echo "The go.mod file is out of sync with the source code"
+  echo "Output of \`go mod tidy -v\`:"
+  echo "$tidy_output"
   echo "Please run \`go mod tidy\` locally, and commit the result."
   exit 1
 fi
