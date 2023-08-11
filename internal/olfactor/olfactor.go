7bf87466cd262dc6c683e8d38bf67ab4a787b667
Narthana Epa <narthana.epa@gmail.com>
2023-08-12 00:00:34 +1000

    Document olfactor

--- internal/olfactor/olfactor.go
+++ internal/olfactor/olfactor.go
@@ -6,11 +6,14 @@ import (
 	"github.com/buildkite/agent/v3/internal/replacer"
 )
 
+// Olfactor may be used for 'sniffing' the output of a command.
 type Olfactor struct {
 	smell string
 	smelt bool
 }
 
+// New returns a writer and an olfactor. Writes to the writer will be forwarded
+// to `dst` and the olfactor will sniff for the given smell.
 func New(dst io.Writer, smell string) (io.Writer, *Olfactor) {
 	d := &Olfactor{
 		smelt: false,
@@ -22,6 +25,7 @@ func New(dst io.Writer, smell string) (io.Writer, *Olfactor) {
 	}), d
 }
 
+// Smelt returns true if the olfactor smelt the smell.
 func (d *Olfactor) Smelt() bool {
 	return d.smelt
 }
