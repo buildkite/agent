655db196eabe6ca021f90c72ccd7d35d24e8b530
Narthana Epa <narthana.epa@gmail.com>
2023-08-13 11:25:30 +1000

    Handle empty string in olfactor

--- internal/olfactor/olfactor.go
+++ internal/olfactor/olfactor.go
@@ -15,10 +15,10 @@ type Olfactor struct {
 // New returns a writer and an olfactor. Writes to the writer will be forwarded
 // to `dst` and the olfactor will sniff for the given smell.
 func New(dst io.Writer, smell string) (io.Writer, *Olfactor) {
-	d := &Olfactor{
-		smelt: false,
-		smell: smell,
+	if smell == "" {
+		return dst, &Olfactor{smell: "", smelt: true}
 	}
+	d := &Olfactor{smelt: false, smell: smell}
 	return replacer.New(dst, []string{d.smell}, func(b []byte) []byte {
 		d.smelt = true
 		return b
