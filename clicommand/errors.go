f10f29492f8c83ec59dbb0d99d1a43c9f44129ed
Narthana Epa <narthana.epa@gmail.com>
2023-09-04 22:03:31 +1000

    Add doc comments in errors.go

--- clicommand/errors.go
+++ clicommand/errors.go
@@ -6,53 +6,62 @@ import (
 	"os"
 )
 
-// ExitError is used to signal to main.go that the command should exit with a
-// the exit code in `code`.
+// ExitError is used to signal that the command should exit with the exit code
+// in `code`. It also wraps an error, which can be used to provide more context.
 type ExitError struct {
 	code  int
 	inner error
 }
 
+// NewExitError returns ExitError with the given code and wrapped error.
 func NewExitError(code int, err error) *ExitError {
 	return &ExitError{code: code, inner: err}
 }
 
+// Code returns the exit code.
 func (e *ExitError) Code() int {
 	return e.code
 }
 
+// Error prints the message of the wrapped error. It ignores the exit code.
 func (e *ExitError) Error() string {
 	return e.inner.Error()
 }
 
+// Unwrap returns the wrapped error.
 func (e *ExitError) Unwrap() error {
 	return e.inner
 }
 
+// Is will return true if the target is an ExitError with the same code.
 func (e *ExitError) Is(target error) bool {
 	terr, ok := target.(*ExitError)
 	return ok && e.code == terr.code
 }
 
-// SilentExitError instructs the main method to not log anything and just exit
-// with status `code`
+// SilentExitError instructs ExitCode to not log anything and just exit
+// with status `code`.
 type SilentExitError struct {
 	code int
 }
 
+// NewSilentExitError returns SilentExitError with the given code.
 func NewSilentExitError(code int) *SilentExitError {
 	return &SilentExitError{code: code}
 }
 
-// Error prints the exit code, but should not be used in main.go
+// Error prints a message with the exit code. It should not be used as the
+// the purpose of this error is to not print anything.
 func (e *SilentExitError) Error() string {
 	return fmt.Sprintf("silently exited status %d", e.code)
 }
 
+// Code returns the exit code.
 func (e *SilentExitError) Code() int {
 	return e.code
 }
 
+// Is will return true if the target is a SilentExitError with the same code.
 func (e *SilentExitError) Is(target error) bool {
 	terr, ok := target.(*SilentExitError)
 	return ok && e.code == terr.code
