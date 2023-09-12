e9d95a9729e489b13d676c8f5472b942abfbffb8
Narthana Epa <narthana.epa@gmail.com>
2023-09-12 22:49:37 +1000

    Clarify and reconcile docs

--- version/version.go
+++ version/version.go
@@ -9,12 +9,6 @@ import (
 	"strings"
 )
 
-// You can overridden buildVersion at compile time by using:
-//
-//  go run -ldflags "-X github.com/buildkite/agent/v3/agent.buildVersion=abc" . --version
-//
-// On CI, the binaries are always build with the buildVersion variable set.
-//
 // Pre-release builds' versions must be in the format `x.y-beta`, `x.y-beta.z` or `x.y-beta.z.a`
 
 //go:embed VERSION
@@ -25,6 +19,16 @@ func Version() string {
 	return strings.TrimSpace(baseVersion)
 }
 
+// BuildNumber returns the build number of the CI pipeline that built the agent.
+// You can override buildVersion at compile time by using the ldflag:
+//
+// "-X github.com/buildkite/agent/v3/version.buildNumber=abc"
+//
+// An easy way to test this is:
+//
+// $ go run -buildvcs=true -ldflags "-X github.com/buildkite/agent/v3/version.buildNumber=abc" . --version
+//
+// On CI, the binaries are always built with the buildVersion variable set.
 func BuildNumber() string {
 	if buildNumber == "" {
 		return "x"
@@ -32,6 +36,9 @@ func BuildNumber() string {
 	return buildNumber
 }
 
+// commitInfo returns a string consisting of the commit hash and whether the the build was made in a
+// `dirty` working directory or not. A dirty working directory is one that has uncommitted changes
+// to files that git would track.
 func commitInfo() string {
 	info, ok := debug.ReadBuildInfo()
 	if !ok {
@@ -53,11 +60,14 @@ func commitInfo() string {
 
 // FullVersion is a SemVer 2.0 compliant version string that includes
 // [build metadata](https://semver.org/#spec-item-10) consisting of the build
-// number (if any), the commit hash, and whether the build was dirty.
+// number (if any), the commit hash, and whether the build was made in a `dirty`
+// working directory or not.
 func FullVersion() string {
 	return fmt.Sprintf("%s+%s.%s", Version(), BuildNumber(), commitInfo())
 }
 
+// UserAgent returns a string suitable for use as a User-Agent header.
+// TODO-vNext: Include some prefix of the commit hash in the User-Agent header.
 func UserAgent() string {
 	return fmt.Sprintf(
 		"buildkite-agent/%s.%s (%s; %s)",
