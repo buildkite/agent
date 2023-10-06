250f9788ecab2c1d41e5c3e5ef145f31150302d5
Ben Moskovitz <ben.m@buildkite.com>
2023-10-06 11:39:13 +1100

    Test that clicommand config is complete
    ie that there is a one-to-one mapping from the cli.Command's `flags` and the config struct's `cli` struct tags
    
    Also, add a test to ensure that any new commands are added to the test above

---
+++ clicommand/commands.go
@@ -0,0 +1,80 @@
+package clicommand
+
+import "github.com/urfave/cli"
+
+var Commands = []cli.Command{
+	AcknowledgementsCommand,
+	AgentStartCommand,
+	AnnotateCommand,
+	{
+		Name:  "annotation",
+		Usage: "Make changes to an annotation on the currently running build",
+		Subcommands: []cli.Command{
+			AnnotationRemoveCommand,
+		},
+	},
+	{
+		Name:  "artifact",
+		Usage: "Upload/download artifacts from Buildkite jobs",
+		Subcommands: []cli.Command{
+			ArtifactUploadCommand,
+			ArtifactDownloadCommand,
+			ArtifactSearchCommand,
+			ArtifactShasumCommand,
+		},
+	},
+	{
+		Name:  "env",
+		Usage: "Process environment subcommands",
+		Subcommands: []cli.Command{
+			EnvDumpCommand,
+			EnvGetCommand,
+			EnvSetCommand,
+			EnvUnsetCommand,
+		},
+	},
+	{
+		Name:  "lock",
+		Usage: "Process lock subcommands",
+		Subcommands: []cli.Command{
+			LockAcquireCommand,
+			LockDoCommand,
+			LockDoneCommand,
+			LockGetCommand,
+			LockReleaseCommand,
+		},
+	},
+	{
+		Name:  "meta-data",
+		Usage: "Get/set data from Buildkite jobs",
+		Subcommands: []cli.Command{
+			MetaDataSetCommand,
+			MetaDataGetCommand,
+			MetaDataExistsCommand,
+			MetaDataKeysCommand,
+		},
+	},
+	{
+		Name:  "oidc",
+		Usage: "Interact with Buildkite OpenID Connect (OIDC)",
+		Subcommands: []cli.Command{
+			OIDCRequestTokenCommand,
+		},
+	},
+	{
+		Name:  "pipeline",
+		Usage: "Make changes to the pipeline of the currently running build",
+		Subcommands: []cli.Command{
+			PipelineUploadCommand,
+		},
+	},
+	{
+		Name:  "step",
+		Usage: "Get or update an attribute of a build step",
+		Subcommands: []cli.Command{
+			StepGetCommand,
+			StepUpdateCommand,
+		},
+	},
+	BootstrapCommand,
+}
