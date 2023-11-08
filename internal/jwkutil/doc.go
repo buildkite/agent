0a311fa47d17e4cea795515d8d894c1dd3ec54bb
Narthana Epa <narthana.epa@gmail.com>
2023-11-08 21:20:49 +1100

    Add package level comments

---
+++ internal/jwkutil/doc.go
@@ -0,0 +1,5 @@
+// Package jwkutil provides utilities for working with JSON Web Keys and JSON
+// Web Key Sets as defined in [RFC 7517].
+//
+// [RFC 7517]: https://tools.ietf.org/html/rfc7517
+package jwkutil
