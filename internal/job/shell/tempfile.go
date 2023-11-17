d666fb1a7a63ca87e6f769f23ffd52d37e77d2d8
Narthana Epa <narthana.epa@gmail.com>
2023-11-17 13:26:10 +1100

    Create better tempfile creation methods

--- internal/job/shell/tempfile.go
+++ internal/job/shell/tempfile.go
@@ -2,6 +2,7 @@ package shell
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
 	"path/filepath"
 	"strings"
@@ -47,4 +48,50 @@ func TempFileWithExtension(filename string) (*os.File, error) {
 	}
 
 	return tempFile, nil
+}
+
+// SystemTempFileWithExtensionAndMode creates a temporary file with the same extension of the
+// provided `filename` and sets its permissions to `perm`. The file will be created in the
+// subdirectory `dir` inside the system temp directory.
+func SystemTempFileWithExtensionAndMode(dir, filename string, perm fs.FileMode) (*os.File, error) {
+	extension := filepath.Ext(filename)
+	basename := strings.TrimSuffix(filename, extension)
+	tmpName := basename
+	if extension != "" {
+		tmpName += "-*-" + extension
+	}
+	tempDir := filepath.Join(os.TempDir(), dir)
+
+	// Best effort ensure tempDir exists
+	if _, err := os.Stat(tempDir); os.IsNotExist(err) {
+		// umask will make perms more reasonable
+		if err := os.MkdirAll(tempDir, 0o777); err != nil {
+			return nil, err
+		}
+	}
+
+	tempFile, err := os.CreateTemp(tempDir, tmpName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create temporary file %q: %w", filename, err)
+	}
+
+	if err := tempFile.Chmod(perm); err != nil {
+		return nil, fmt.Errorf("failed to chmod temporary file %q: %w", tempFile.Name(), err)
+	}
+
+	return tempFile, nil
+}
+
+// ClosedSytemTempFileWithExtensionAndMode creates a temporary file with the same extension of the
+// provided `filename` and sets its permissions to `perm`. The file will be created in the
+// subdirectory `dir` inside the system temp directory. It closes the file after creating it.
+func ClosedTempFileWithExtensionAndMode(dir, filename string, perm fs.FileMode) (string, error) {
+	f, err := SystemTempFileWithExtensionAndMode(dir, filename, perm)
+	if err != nil {
+		return "", err
+	}
+	if err := f.Close(); err != nil {
+		return "", err
+	}
+	return f.Name(), nil
 }
