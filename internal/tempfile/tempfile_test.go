4ecc6452c3a910b0f33f49ea0e097bb16309a306
Narthana Epa <narthana.epa@gmail.com>
2023-11-30 20:47:55 +1100

    Use same test cases for New and NewClosed

--- internal/tempfile/tempfile_test.go
+++ internal/tempfile/tempfile_test.go
@@ -11,75 +11,79 @@ import (
 	"gotest.tools/v3/assert"
 )
 
+type testCase struct {
+	name  string
+	opts  []tempfile.Opts
+	check func(t *testing.T, filename string)
+}
+
+var testCases = []testCase{
+	{
+		name:  "default",
+		opts:  []tempfile.Opts{},
+		check: func(t *testing.T, filename string) {},
+	},
+	{
+		name: "with filename",
+		opts: []tempfile.Opts{
+			tempfile.WithName("foo.txt"),
+		},
+		check: func(t *testing.T, filename string) {},
+	},
+	{
+		name: "with dir",
+		opts: []tempfile.Opts{
+			tempfile.WithDir("foo"),
+		},
+		check: func(t *testing.T, filename string) {
+			assert.Check(t, strings.HasPrefix(filename, filepath.Join(os.TempDir(), "foo")))
+		},
+	},
+	{
+		name: "with perms",
+		opts: []tempfile.Opts{
+			tempfile.WithPerms(0o600),
+		},
+		check: func(t *testing.T, filename string) {
+			if runtime.GOOS == "windows" {
+				t.Skip("Windows doesn't support or need checking if chmod worked")
+			}
+
+			fi, err := os.Stat(filename)
+			assert.NilError(t, err, "os.Stat(%q) = %s", filename, err)
+			assert.Check(t, fi.Mode().Perm() == os.FileMode(0o600))
+		},
+	},
+	{
+		name: "with filename and keep extension",
+		opts: []tempfile.Opts{
+			tempfile.WithName("foo.txt"),
+			tempfile.KeepingExtension(),
+		},
+		check: func(t *testing.T, filename string) {
+			assert.Check(t, filepath.Ext(filename) == ".txt")
+		},
+	},
+	{
+		name: "without filename and keep extension",
+		opts: []tempfile.Opts{
+			tempfile.KeepingExtension(),
+		},
+		check: func(t *testing.T, filename string) {},
+	},
+}
+
 func TestNew(t *testing.T) {
 	t.Parallel()
 
-	for _, test := range []struct {
-		name  string
-		opts  []tempfile.Opts
-		check func(t *testing.T, f *os.File)
-	}{
-		{
-			name:  "default",
-			opts:  []tempfile.Opts{},
-			check: func(t *testing.T, f *os.File) {},
-		},
-		{
-			name: "with filename",
-			opts: []tempfile.Opts{
-				tempfile.WithName("foo.txt"),
-			},
-			check: func(t *testing.T, f *os.File) {},
-		},
-		{
-			name: "with dir",
-			opts: []tempfile.Opts{
-				tempfile.WithDir("foo"),
-			},
-			check: func(t *testing.T, f *os.File) {
-				assert.Check(t, strings.HasPrefix(f.Name(), filepath.Join(os.TempDir(), "foo")))
-			},
-		},
-		{
-			name: "with perms",
-			opts: []tempfile.Opts{
-				tempfile.WithPerms(0o600),
-			},
-			check: func(t *testing.T, f *os.File) {
-				if runtime.GOOS == "windows" {
-					t.Skip("Windows doesn't support or need checking if chmod worked")
-				}
+	for _, tc := range testCases {
+		tc := tc
 
-				fi, err := os.Stat(f.Name())
-				assert.NilError(t, err, "os.Stat(%q) = %s", f.Name(), err)
-				assert.Check(t, fi.Mode().Perm() == os.FileMode(0o600))
-			},
-		},
-		{
-			name: "with filename and keep extension",
-			opts: []tempfile.Opts{
-				tempfile.WithName("foo.txt"),
-				tempfile.KeepingExtension(),
-			},
-			check: func(t *testing.T, f *os.File) {
-				assert.Check(t, filepath.Ext(f.Name()) == ".txt")
-			},
-		},
-		{
-			name: "without filename and keep extension",
-			opts: []tempfile.Opts{
-				tempfile.KeepingExtension(),
-			},
-			check: func(t *testing.T, f *os.File) {},
-		},
-	} {
-		test := test
-
-		t.Run(test.name, func(t *testing.T) {
+		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 
-			f, err := tempfile.New(test.opts...)
-			assert.NilError(t, err, `New(%v) = %v`, test.opts, err)
+			f, err := tempfile.New(tc.opts...)
+			assert.NilError(t, err, `New(%v) = %v`, tc.opts, err)
 
 			t.Cleanup(func() {
 				assert.Check(t, f.Close() == nil, "failed to close file: %s", f.Name())
@@ -87,7 +91,7 @@ func TestNew(t *testing.T) {
 			})
 
 			assert.Check(t, strings.HasPrefix(f.Name(), os.TempDir()))
-			test.check(t, f)
+			tc.check(t, f.Name())
 		})
 	}
 }
@@ -95,79 +99,21 @@ func TestNew(t *testing.T) {
 func TestNewClosed(t *testing.T) {
 	t.Parallel()
 
-	for _, test := range []struct {
-		name  string
-		opts  []tempfile.Opts
-		check func(t *testing.T, filename string)
-	}{
-		{
-			name:  "default",
-			opts:  []tempfile.Opts{},
-			check: func(t *testing.T, filename string) {},
-		},
-		{
-			name: "with filename",
-			opts: []tempfile.Opts{
-				tempfile.WithName("foo.txt"),
-			},
-			check: func(t *testing.T, filename string) {},
-		},
-		{
-			name: "with dir",
-			opts: []tempfile.Opts{
-				tempfile.WithDir("foo"),
-			},
-			check: func(t *testing.T, filename string) {
-				assert.Check(t, strings.HasPrefix(filename, filepath.Join(os.TempDir(), "foo")))
-			},
-		},
-		{
-			name: "with perms",
-			opts: []tempfile.Opts{
-				tempfile.WithPerms(0o600),
-			},
-			check: func(t *testing.T, filename string) {
-				if runtime.GOOS == "windows" {
-					t.Skip("Windows doesn't support or need checking if chmod worked")
-				}
+	for _, tc := range testCases {
+		tc := tc
 
-				fi, err := os.Stat(filename)
-				assert.NilError(t, err, "os.Stat(%q) = %s", filename, err)
-				assert.Check(t, fi.Mode().Perm() == os.FileMode(0o600))
-			},
-		},
-		{
-			name: "with filename and keep extension",
-			opts: []tempfile.Opts{
-				tempfile.WithName("foo.txt"),
-				tempfile.KeepingExtension(),
-			},
-			check: func(t *testing.T, filename string) {
-				assert.Check(t, filepath.Ext(filename) == ".txt")
-			},
-		},
-		{
-			name: "without filename and keep extension",
-			opts: []tempfile.Opts{
-				tempfile.KeepingExtension(),
-			},
-			check: func(t *testing.T, filename string) {},
-		},
-	} {
-		test := test
-
-		t.Run(test.name, func(t *testing.T) {
+		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 
-			filename, err := tempfile.NewClosed(test.opts...)
-			assert.NilError(t, err, `New(%v) = %v`, test.opts, err)
+			filename, err := tempfile.NewClosed(tc.opts...)
+			assert.NilError(t, err, `NewClosed(%v) = %v`, tc.opts, err)
 
 			t.Cleanup(func() {
 				assert.Check(t, os.Remove(filename) == nil, "failed to remove file: %s", filename)
 			})
 
 			assert.Check(t, strings.HasPrefix(filename, os.TempDir()))
-			test.check(t, filename)
+			tc.check(t, filename)
 		})
 	}
 }
