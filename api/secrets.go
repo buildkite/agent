293857468b2c9a9777af010ce02c497cbd7e2bfb
Narthana Epa <narthana.epa@gmail.com>
2024-02-20 21:53:21 +1100

    Fix path needs jobID

--- api/secrets.go
+++ api/secrets.go
@@ -3,10 +3,12 @@ package api
 import (
 	"context"
 	"net/url"
+	"path"
 )
 
 type GetSecretRequest struct {
-	Key string
+	Key   string
+	JobID string
 }
 
 type Secret struct {
@@ -16,7 +18,7 @@ type Secret struct {
 }
 
 func (c *Client) GetSecret(ctx context.Context, req *GetSecretRequest) (*Secret, *Response, error) {
-	u := url.URL{Path: "secrets"}
+	u := url.URL{Path: path.Join("jobs", req.JobID, "secrets")}
 	u.Query().Add("key", req.Key)
 
 	httpReq, err := c.newRequest(ctx, "GET", railsPathEscape(u.String()), nil)
