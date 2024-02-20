5f8cf27e1ae4a5f7130762a1c5a4a88a595c6115
Narthana Epa <narthana.epa@gmail.com>
2024-02-20 22:59:01 +1100

    Fix escapes and query strings

--- api/secrets.go
+++ api/secrets.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"context"
-	"net/url"
 	"path"
 )
 
@@ -12,19 +11,20 @@ type GetSecretRequest struct {
 }
 
 type Secret struct {
-	Key   string `json:"name"`
+	Key   string `json:"key"`
 	Value string `json:"value"`
 	UUID  string `json:"uuid"`
 }
 
 func (c *Client) GetSecret(ctx context.Context, req *GetSecretRequest) (*Secret, *Response, error) {
-	u := url.URL{Path: path.Join("jobs", req.JobID, "secrets")}
-	u.Query().Add("key", req.Key)
-
-	httpReq, err := c.newRequest(ctx, "GET", railsPathEscape(u.String()), nil)
+	httpReq, err := c.newRequest(ctx, "GET", path.Join("jobs", req.JobID, "secrets"), nil)
 	if err != nil {
 		return nil, nil, err
 	}
+
+	q := httpReq.URL.Query()
+	q.Add("key", req.Key)
+	httpReq.URL.RawQuery = q.Encode()
 
 	secret := &Secret{}
 	resp, err := c.doRequest(httpReq, secret)
