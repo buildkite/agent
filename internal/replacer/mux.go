dc69b3fd7b33c066e79e4233a3242ba68f39e6a2
Narthana Epa <narthana.epa@gmail.com>
2024-02-28 22:39:17 +1100

    Add some methods to replacer.Mux

--- internal/replacer/mux.go
+++ internal/replacer/mux.go
@@ -4,13 +4,45 @@ import (
 	"errors"
 )
 
+type unit struct{}
+
 // Mux contains multiple replacers
-type Mux map[*Replacer]struct{}
+type Mux map[*Replacer]unit
+
+// NewMux returns a new mux with the given replacers.
+func NewMux(rs ...*Replacer) *Mux {
+	m := Mux{}
+	m.Append(rs...)
+	return &m
+}
 
 // Reset resets all replacers with new needles (secrets).
 func (mux Mux) Reset(needles []string) {
 	for r := range mux {
 		r.Reset(needles)
+	}
+}
+
+// Add adds needles to all replacers in the mux.
+func (mux Mux) Add(needles ...string) {
+	for r := range mux {
+		r.Add(needles...)
+	}
+}
+
+// Flush flushes all replacers in the mux.
+func (mux Mux) Flush() error {
+	errs := make([]error, 0, len(mux))
+	for r := range mux {
+		errs = append(errs, r.Flush())
+	}
+	return errors.Join(errs...)
+}
+
+// Append appends the given replacers to the mux.
+func (mux Mux) Append(rs ...*Replacer) {
+	for _, r := range rs {
+		mux[r] = unit{}
 	}
 }
 
