b2ebe5c965cccc0b35f78fadf465818826acb070
Ruoqing He <ruoqing@iscas.ac.cn>
2024-07-08 15:16:13 +0800

    Introduce `riscv64` architecture
    Add `riscv64` DEB and RPM packaging logic.
    
    Signed-off-by: Ruoqing He <ruoqing@iscas.ac.cn>

--- install.sh
+++ install.sh
@@ -65,6 +65,7 @@ else
     *aarch64*) ARCH="arm64"   ;;
     *mips64*) ARCH="mips64le" ;;
     *s390x*)   ARCH="s390x"   ;;
+    *riscv64*) ARCH="riscv64" ;;
     *)
       ARCH="386"
       echo -e "\n\033[36mWe don't recognise the $MACHINE architecture; falling back to $ARCH\033[0m"
