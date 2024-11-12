71958f8d20249bdf53fc3c0651cd2e11e1249568
Josh Deprez <jd@buildkite.com>
2024-11-12 14:19:36 +1100

    Add nilaway check

--- .buildkite/steps/check-code-committed.sh
+++ .buildkite/steps/check-code-committed.sh
@@ -26,12 +26,27 @@ echo --- :go: Generating code
 go generate ./...
 if ! git diff --no-ext-diff --exit-code; then
   echo ^^^ +++
-  echo :x: Generated code was not commited.
+  echo ":x: Generated code was not commited."
   echo "Run"
   echo "  go generate ./..."
   echo "and make a commit."
 
   exit 1
+fi
+
+echo --- :go: Checking for potential nil panics
+if ! command -v nilaway >/dev/null; then
+    # Probably ran outside the usual build container.
+	go install go.uber.org/nilaway/cmd/nilaway@latest
+	NILAWAY="$(go env GOPATH)/bin/nilaway"
+else
+	NILAWAY="$(command -v nilaway)"
+fi
+nilaway_results="$("${NILAWAY}" ./...)"
+if [[ "${nilaway_results:-}"  != "" ]]; then
+    echo "^^^ +++"
+    echo ":x: Nilaway found potential problems:"
+    echo "${nilaway_results}"
 fi
 
 echo +++ Everything is clean and tidy! 🎉
