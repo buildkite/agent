buildkite/agent

commit 77fc7dc04462183d29d44217a18d13fa710fabd7

Handle pause actions

agent/agent_worker.go
@@ -87,9 +87,8 @@ type AgentWorker struct {
 	cancelSig process.Signal
 
 	// Stop controls
-	stopMutex sync.Mutex
-	stop      chan struct{}
-	stopping  bool
+	stopOnce sync.Once // prevents double-closing the channel
+	stop     chan struct{}
 
 	// The index of this agent worker
 	spawnIndex int
@@ -102,9 +101,9 @@ type AgentWorker struct {
 	agentStdout io.Writer
 
 	// Are we doing something right now?
+	stateMtx     sync.Mutex
 	state        agentWorkerState
 	currentJobID string
-	stateMtx     sync.Mutex
 }
 
 type agentWorkerState string
@@ -229,8 +228,10 @@ func (a *AgentWorker) Start(ctx context.Context, idleMonitor *IdleMonitor) error
 
 	go a.runHeartbeatLoop(heartbeatCtx)
 
-	// If the agent is booted in acquisition mode, then we don't need to
-	// bother about starting the ping loop.
+	// If the agent is booted in acquisition mode, acquire that particular job
+	// before running the ping loop.
+	// (Why run a ping loop at all? To find out if the agent is paused, which
+	// affects whether it terminates after the job.)
 	if a.agentConfiguration.AcquireJob != "" {
 		// When in acquisition mode, there can't be any agents, so
 		// there's really no point in letting the idle monitor know
@@ -238,7 +239,9 @@ func (a *AgentWorker) Start(ctx context.Context, idleMonitor *IdleMonitor) error
 		// measure.
 		idleMonitor.MarkBusy(a.agent.UUID)
 
-		return a.AcquireAndRunJob(ctx, a.agentConfiguration.AcquireJob)
+		if err := a.AcquireAndRunJob(ctx, a.agentConfiguration.AcquireJob); err != nil {
+			a.logger.Error("Failed to acquire and run job: %v", err)
+		}
 	}
 
 	return a.runPingLoop(ctx, idleMonitor)
@@ -296,7 +299,9 @@ func (a *AgentWorker) runPingLoop(ctx context.Context, idleMonitor *IdleMonitor)
 	first <- struct{}{}
 
 	lastActionTime := time.Now()
-	a.logger.Info("Waiting for work...")
+	a.logger.Info("Waiting for instructions...")
+
+	wasPaused := false
 
 	// Continue this loop until the closing of the stop channel signals termination
 	for {
@@ -321,22 +326,43 @@ func (a *AgentWorker) runPingLoop(ctx context.Context, idleMonitor *IdleMonitor)
 			return nil
 		}
 
-		a.stopMutex.Lock()
-		stopping := a.stopping
-		a.stopMutex.Unlock()
-		if stopping {
-			return nil
-		}
-
-		setStat("📡 Pinging Buildkite for work")
-		job, err := a.Ping(ctx)
+		setStat("📡 Pinging Buildkite for instructions")
+		job, action, err := a.Ping(ctx)
 		if err != nil {
 			if errors.Is(err, &errUnrecoverable{}) {
 				a.logger.Error("%v", err)
 			} else {
 				a.logger.Warn("%v", err)
 			}
-		} else if job != nil {
+		}
+
+		switch action {
+		case "disconnect":
+			a.Stop(false)
+			return nil
+
+		case "pause":
+			// An agent is not dispatched any jobs while it is paused, but the
+			// paused agent is expected to remain alive and pinging for
+			// instructions.
+			// *This includes acquire-job and disconnect-after-idle-timeout.*
+			wasPaused = true
+			continue
+		}
+
+		// At this point, action was neither "disconnect" nor "pause".
+		if wasPaused {
+			a.logger.Info("Agent has resumed after being paused")
+			wasPaused = false
+		}
+
+		// Exit after acquire-job.
+		if a.agentConfiguration.AcquireJob != "" {
+			return nil
+		}
+
+		// Note that Ping only returns a job if err == nil.
+		if job != nil {
 			// Let other agents know this agent is now busy and
 			// not to idle terminate
 			idleMonitor.MarkBusy(a.agent.UUID)
@@ -395,15 +421,12 @@ func (a *AgentWorker) runPingLoop(ctx context.Context, idleMonitor *IdleMonitor)
 // Stops the agent from accepting new work and cancels any current work it's
 // running
 func (a *AgentWorker) Stop(graceful bool) {
-	// Only allow one stop to run at a time (because we're playing with
-	// channels)
-	a.stopMutex.Lock()
-	defer a.stopMutex.Unlock()
-
 	if graceful {
-		if a.stopping {
+		select {
+		case <-a.stop:
 			a.logger.Warn("Agent is already gracefully stopping...")
-		} else {
+
+		default:
 			// If we have a job, tell the user that we'll wait for
 			// it to finish before disconnecting
 			if a.jobRunner != nil {
@@ -429,18 +452,11 @@ func (a *AgentWorker) Stop(graceful bool) {
 		}
 	}
 
-	// We don't need to do the below operations again since we've already
-	// done them before
-	if a.stopping {
-		return
-	}
-
-	// Use the closure of the stop channel as a signal to the main run loop in Start()
-	// to stop looping and terminate
-	close(a.stop)
-
-	// Mark the agent as stopping
-	a.stopping = true
+	a.stopOnce.Do(func() {
+		// Use the closure of the stop channel as a signal to the main run loop in Start()
+		// to stop looping and terminate
+		close(a.stop)
+	})
 }
 
 // Connects the agent to the Buildkite Agent API, retrying up to 10 times if it
@@ -491,7 +507,7 @@ func (a *AgentWorker) Heartbeat(ctx context.Context) error {
 
 // Performs a ping that checks Buildkite for a job or action to take
 // Returns a job, or nil if none is found
-func (a *AgentWorker) Ping(ctx context.Context) (*api.Job, error) {
+func (a *AgentWorker) Ping(ctx context.Context) (job *api.Job, action string, err error) {
 	ping, resp, pingErr := a.apiClient.Ping(ctx)
 	// wait a minute, where's my if err != nil block? TL;DR look for pingErr ~20 lines down
 	// the api client returns an error if the response code isn't a 2xx, but there's still information in resp and ping
@@ -504,11 +520,7 @@ func (a *AgentWorker) Ping(ctx context.Context) (*api.Job, error) {
 			a.logger.Info(ping.Message)
 		}
 
-		// Should the agent disconnect?
-		if ping.Action == "disconnect" {
-			a.Stop(false)
-			return nil, nil
-		}
+		action = ping.Action
 	}
 
 	if pingErr != nil {
@@ -517,7 +529,7 @@ func (a *AgentWorker) Ping(ctx context.Context) (*api.Job, error) {
 		// responses with non-retryable statuses
 		if resp != nil && !api.IsRetryableStatus(resp) {
 			a.Stop(false)
-			return nil, &errUnrecoverable{action: "Ping", response: resp, err: pingErr}
+			return nil, action, &errUnrecoverable{action: "Ping", response: resp, err: pingErr}
 		}
 
 		// Get the last ping time to the nearest microsecond
@@ -527,9 +539,9 @@ func (a *AgentWorker) Ping(ctx context.Context) (*api.Job, error) {
 		// If a ping fails, we don't really care, because it'll
 		// ping again after the interval.
 		if a.stats.lastPing.IsZero() {
-			return nil, fmt.Errorf("Failed to ping: %w (No successful ping yet)", pingErr)
+			return nil, action, fmt.Errorf("Failed to ping: %w (No successful ping yet)", pingErr)
 		} else {
-			return nil, fmt.Errorf("Failed to ping: %w (Last successful was %v ago)", pingErr, time.Since(a.stats.lastPing))
+			return nil, action, fmt.Errorf("Failed to ping: %w (Last successful was %v ago)", pingErr, time.Since(a.stats.lastPing))
 		}
 	}
 
@@ -556,11 +568,12 @@ func (a *AgentWorker) Ping(ctx context.Context) (*api.Job, error) {
 	}
 
 	// If we don't have a job, there's nothing to do!
-	if ping.Job == nil {
-		return nil, nil
+	// If we're paused, job should be nil, but in case it isn't, ignore it.
+	if ping.Job == nil || action == "pause" {
+		return nil, action, nil
 	}
 
-	return ping.Job, nil
+	return ping.Job, action, nil
 }
 
 // AcquireAndRunJob attempts to acquire a job an run it. It will retry at after the
@@ -682,45 +695,4 @@ func (a *AgentWorker) healthHandler() http.HandlerFunc {
 			}
 		}
 	}
-}
-
-// This monitor has a 3rd implicit state we will call "initializing" that all agents start in
-// Agents can transition to busy and/or idle but always start in the "initializing" state
-/*
-//                -> Busy
-//              /     ^
-// Initializing       |
-//              \     v
-//                -> Idle
-*/
-// This (intentionally?) ensures the DisconnectAfterIdleTimeout doesn't fire before agents have had a chance to run a job
-type IdleMonitor struct {
-	sync.Mutex
-	totalAgents int
-	idle        map[string]struct{}
-}
-
-func NewIdleMonitor(totalAgents int) *IdleMonitor {
-	return &IdleMonitor{
-		totalAgents: totalAgents,
-		idle:        map[string]struct{}{},
-	}
-}
-
-func (i *IdleMonitor) Idle() bool {
-	i.Lock()
-	defer i.Unlock()
-	return len(i.idle) == i.totalAgents
-}
-
-func (i *IdleMonitor) MarkIdle(agentUUID string) {
-	i.Lock()
-	defer i.Unlock()
-	i.idle[agentUUID] = struct{}{}
-}
-
-func (i *IdleMonitor) MarkBusy(agentUUID string) {
-	i.Lock()
-	defer i.Unlock()
-	delete(i.idle, agentUUID)
 }

agent/agent_worker_test.go
@@ -2,11 +2,14 @@ package agent
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"math"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"path/filepath"
 	"regexp"
 	"strconv"
 	"testing"
@@ -15,6 +18,8 @@ import (
 	"github.com/buildkite/agent/v3/api"
 	"github.com/buildkite/agent/v3/core"
 	"github.com/buildkite/agent/v3/logger"
+	"github.com/buildkite/agent/v3/metrics"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -121,9 +126,6 @@ func TestDisconnectRetry(t *testing.T) {
 }
 
 func TestAcquireJobReturnsWrappedError_WhenServerResponds422(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
 	jobID := "some-uuid"
 
 	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
@@ -154,16 +156,13 @@ func TestAcquireJobReturnsWrappedError_WhenServerResponds422(t *testing.T) {
 		agentConfiguration: AgentConfiguration{},
 	}
 
-	err := worker.AcquireAndRunJob(ctx, jobID)
+	err := worker.AcquireAndRunJob(t.Context(), jobID)
 	if !errors.Is(err, core.ErrJobAcquisitionRejected) {
 		t.Fatalf("expected worker.AcquireAndRunJob(%q) = core.ErrJobAcquisitionRejected, got %v", jobID, err)
 	}
 }
 
 func TestAcquireAndRunJobWaiting(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
 	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		switch req.URL.Path {
 		case "/jobs/waitinguuid/acquire":
@@ -210,7 +209,7 @@ func TestAcquireAndRunJobWaiting(t *testing.T) {
 		agentConfiguration: AgentConfiguration{},
 	}
 
-	err := worker.AcquireAndRunJob(ctx, "waitinguuid")
+	err := worker.AcquireAndRunJob(t.Context(), "waitinguuid")
 	assert.ErrorContains(t, err, "423")
 
 	if errors.Is(err, core.ErrJobAcquisitionRejected) {
@@ -223,4 +222,147 @@ func TestAcquireAndRunJobWaiting(t *testing.T) {
 		expectedSleeps = append(expectedSleeps, time.Duration(d)*time.Second)
 	}
 	assert.Equal(t, expectedSleeps, retrySleeps)
+}
+
+func TestAgentWorker_Start_AcquireJob_Pause_Unpause(t *testing.T) {
+	jobUUID := uuid.New().String()
+	jobToken := uuid.New().String()
+	jobAcquired := false
+	jobStarted := false
+	jobFinished := false
+
+	pingCount := 0
+
+	// This could almost be made into a reusable fake...
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		switch req.URL.Path {
+		case fmt.Sprintf("/jobs/%s/acquire", jobUUID):
+			if jobAcquired {
+				http.Error(rw, `{"message":"job already acquired"}`, http.StatusUnprocessableEntity)
+				return
+			}
+
+			if req.Header.Get("X-Buildkite-Lock-Acquire-Job") != "1" {
+				http.Error(rw, "Expected X-Buildkite-Lock-Acquire-Job to be set to 1", http.StatusUnprocessableEntity)
+				return
+			}
+
+			jobAcquired = true
+
+			resp := api.Job{
+				ID: jobUUID,
+				Env: map[string]string{
+					"BUILDKITE_COMMAND": "echo echo",
+				},
+				ChunksMaxSizeBytes: 1024,
+				Token:              jobToken,
+			}
+			if err := json.NewEncoder(rw).Encode(resp); err != nil {
+				t.Errorf("json.NewEncoder(http.ResponseWriter).Encode(%v) = %v", resp, err)
+			}
+
+		case fmt.Sprintf("/jobs/%s/start", jobUUID):
+			if !jobAcquired {
+				http.Error(rw, `{"message":"job not yet assigned"}`, http.StatusUnprocessableEntity)
+				return
+			}
+
+			jobStarted = true
+
+			rw.Write([]byte("{}"))
+
+		case fmt.Sprintf("/jobs/%s/finish", jobUUID):
+			if !jobStarted {
+				http.Error(rw, `{"message":"job not yet started"}`, http.StatusUnprocessableEntity)
+				return
+			}
+
+			jobFinished = true
+			rw.Write([]byte("{}"))
+
+		case fmt.Sprintf("/jobs/%s/chunks", jobUUID):
+			// Log chunks are not the focus of this test
+			rw.WriteHeader(http.StatusOK)
+
+		case "/ping":
+			pingCount++
+			resp := api.Ping{
+				Action:  "pause",
+				Message: "Agent has been paused",
+			}
+			if pingCount > 1 {
+				resp = api.Ping{
+					Action: "idle",
+				}
+			}
+			if err := json.NewEncoder(rw).Encode(resp); err != nil {
+				t.Errorf("json.NewEncoder(http.ResponseWriter).Encode(%v) = %v", resp, err)
+			}
+
+		case "/heartbeat":
+			var hb api.Heartbeat
+			if err := json.NewDecoder(req.Body).Decode(&hb); err != nil {
+				http.Error(rw, fmt.Sprintf(`{"message":%q}`, err), http.StatusBadRequest)
+				return
+			}
+			hb.ReceivedAt = time.Now().Format(time.RFC3339)
+			if err := json.NewEncoder(rw).Encode(hb); err != nil {
+				t.Errorf("json.NewEncoder(http.ResponseWriter).Encode(%v) = %v", hb, err)
+			}
+
+		default:
+			http.Error(rw, "Not found", http.StatusNotFound)
+		}
+	}))
+	defer server.Close()
+
+	apiClient := api.NewClient(logger.Discard, api.Config{
+		Endpoint: server.URL,
+		Token:    "llamas",
+	})
+
+	l := logger.NewConsoleLogger(logger.NewTestPrinter(t), func(int) {})
+
+	worker := NewAgentWorker(
+		l,
+		&api.AgentRegisterResponse{
+			UUID:              uuid.New().String(),
+			Name:              "agent-1",
+			AccessToken:       "alpacas",
+			Endpoint:          server.URL,
+			PingInterval:      1,
+			JobStatusInterval: 1,
+			HeartbeatInterval: 10,
+		},
+		metrics.NewCollector(logger.Discard, metrics.CollectorConfig{}),
+		apiClient,
+		AgentWorkerConfig{
+			SpawnIndex: 1,
+			AgentConfiguration: AgentConfiguration{
+				BootstrapScript: "./dummy_bootstrap.sh",
+				BuildPath:       filepath.Join(os.TempDir(), t.Name(), "build"),
+				HooksPath:       filepath.Join(os.TempDir(), t.Name(), "hooks"),
+				AcquireJob:      jobUUID,
+			},
+		},
+	)
+
+	idleMonitor := NewIdleMonitor(1)
+
+	if err := worker.Start(t.Context(), idleMonitor); err != nil {
+		t.Errorf("worker.Start() = %v", err)
+	}
+
+	if got, want := pingCount, 2; got != want {
+		t.Errorf("pingCount = %d, want %d", got, want)
+	}
+	if !jobAcquired {
+		t.Errorf("jobAcquired = %t, want true", jobAcquired)
+	}
+	if !jobStarted {
+		t.Errorf("jobStarted = %t, want true", jobStarted)
+	}
+	if !jobFinished {
+		t.Errorf("jobFinished = %t, want true", jobFinished)
+	}
 }

agent/dummy_bootstrap.sh
@@ -1,0 +1,1 @@
+# An empty script that exits 0

agent/idle_monitor.go
@@ -1,0 +1,44 @@
+package agent
+
+import "sync"
+
+// This monitor has a 3rd implicit state we will call "initializing" that all agents start in
+// Agents can transition to busy and/or idle but always start in the "initializing" state
+/*
+//                -> Busy
+//              /     ^
+// Initializing       |
+//              \     v
+//                -> Idle
+*/
+// This (intentionally?) ensures the DisconnectAfterIdleTimeout doesn't fire before agents have had a chance to run a job
+type IdleMonitor struct {
+	sync.Mutex
+	totalAgents int
+	idle        map[string]struct{}
+}
+
+func NewIdleMonitor(totalAgents int) *IdleMonitor {
+	return &IdleMonitor{
+		totalAgents: totalAgents,
+		idle:        map[string]struct{}{},
+	}
+}
+
+func (i *IdleMonitor) Idle() bool {
+	i.Lock()
+	defer i.Unlock()
+	return len(i.idle) == i.totalAgents
+}
+
+func (i *IdleMonitor) MarkIdle(agentUUID string) {
+	i.Lock()
+	defer i.Unlock()
+	i.idle[agentUUID] = struct{}{}
+}
+
+func (i *IdleMonitor) MarkBusy(agentUUID string) {
+	i.Lock()
+	defer i.Unlock()
+	delete(i.idle, agentUUID)
+}

go.mod
@@ -31,6 +31,7 @@ require (
 	github.com/gofrs/flock v0.12.1
 	github.com/google/go-cmp v0.7.0
 	github.com/google/go-querystring v1.1.0
+	github.com/google/uuid v1.6.0
 	github.com/gowebpki/jcs v1.0.1
 	github.com/lestrrat-go/jwx/v2 v2.1.3
 	github.com/mattn/go-zglob v0.0.6
@@ -113,7 +114,6 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/s2a-go v0.1.9 // indirect
-	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.4 // indirect
 	github.com/googleapis/gax-go/v2 v2.14.1 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.25.1 // indirect

logger/log.go
@@ -12,6 +12,7 @@ import (
 	"runtime"
 	"strings"
 	"sync"
+	"testing"
 	"time"
 
 	"github.com/buildkite/agent/v3/version"
@@ -265,4 +266,19 @@ var Discard = &ConsoleLogger{
 	printer: &TextPrinter{
 		Writer: io.Discard,
 	},
+}
+
+// TestPrinter is a log printer than calls the Logf method of a [testing.T]
+// or [testing.B].
+type TestPrinter struct {
+	tb testing.TB
+}
+
+func NewTestPrinter(tb testing.TB) TestPrinter {
+	return TestPrinter{tb: tb}
+}
+
+func (tp TestPrinter) Print(level Level, msg string, fields Fields) {
+	now := time.Now().Format(DateFormat)
+	tp.tb.Logf("%s %s %s %v", now, level, msg, fields)
 }

commit 8d62e534b3a59b7a08de459b5a136fbdfdb4c360

Fix AquireJob to return early and trigger a sentinal error for rejection
This change
https://github.com/buildkite/agent/commit/77fc7dc04462183d29d44217a18d13fa710fabd7
introduced a regression which resulted in acquire job not triggering a
exit code 27 for jobs when acquire job API calls are rejected by
buildkite.

agent/agent_worker.go
@@ -243,6 +243,12 @@ func (a *AgentWorker) Start(ctx context.Context, idleMonitor *IdleMonitor) error
 		idleMonitor.MarkBusy(a.agent.UUID)
 
 		if err := a.AcquireAndRunJob(ctx, a.agentConfiguration.AcquireJob); err != nil {
+			// If the job acquisition was rejected, we can exit with an error
+			// so that supervisor knows that the job was not acquired due to the job being rejected.
+			if errors.Is(err, core.ErrJobAcquisitionRejected) {
+				return fmt.Errorf("Failed to acquire job %q: %w", a.agentConfiguration.AcquireJob, err)
+			}
+
 			a.logger.Error("Failed to acquire and run job: %v", err)
 		}
 	}