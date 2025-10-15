3569db553c5aaa2d09874dee991bb20df8f807f8
Tom Watt <tom@buildkite.com>
2025-10-15 14:00:48 +0100

    docs: Add examples for step update commands for priority and notify attributes

--- clicommand/step_update.go
+++ clicommand/step_update.go
@@ -37,7 +37,10 @@ Example:
     $ buildkite-agent step update "label" "New Label"
     $ buildkite-agent step update "label" " (add to end of label)" --append
     $ buildkite-agent step update "label" < ./tmp/some-new-label
-    $ ./script/label-generator | buildkite-agent step update "label"`
+    $ ./script/label-generator | buildkite-agent step update "label"
+	$ buildkite-agent step update "priority" 10 --step "my-step-key"
+	$ buildkite-agent step update "notify" '[{"github_commit_status": {"context": "my-context"}}]' --append
+`
 
 type StepUpdateConfig struct {
 	GlobalConfig
