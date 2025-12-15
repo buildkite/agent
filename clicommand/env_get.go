9b908c3732bebe736066e5df4796ca290b98f763
Giles Gas <gilesgas@gmail.com>
2025-12-15 18:29:15 +1100

    Update clicommand/env_get.go

--- clicommand/env_get.go
+++ clicommand/env_get.go
@@ -12,7 +12,7 @@ import (
 
 const envClientErrMessage = `Could not create Job API client: %w
 This command can only be used from hooks or plugins running under a job executor
-where the Job API (available in agent v3.64.0 and later) is available.`
+where the agent's job API is available (in version v3.64.0 and later of the Buildkite Agent).`
 
 const envGetHelpDescription = `Usage:
 
