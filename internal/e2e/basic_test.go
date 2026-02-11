84330a5f41c244542bfb7b433e4070b83e8e4cbc
Josh Deprez <jd@buildkite.com>
2026-02-11 16:39:47 +1100

    Add E2E tests for new ping modes

--- internal/e2e/basic_test.go
+++ internal/e2e/basic_test.go
@@ -3,8 +3,10 @@
 package e2e
 
 import (
+	"context"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestBasicE2E(t *testing.T) {
@@ -13,7 +15,59 @@ func TestBasicE2E(t *testing.T) {
 
 	tc.startAgent()
 	build := tc.triggerBuild()
-	state := tc.waitForBuild(ctx, build)
+
+	// It should take much less time than 1 minute to successfully run the job.
+	waitCtx, canc := context.WithTimeout(ctx, 1*time.Minute)
+	defer canc()
+
+	state := tc.waitForBuild(waitCtx, build)
+	if got, want := state, "passed"; got != want {
+		t.Errorf("Build state = %q, want %q", got, want)
+	}
+
+	logs := tc.fetchLogs(ctx, build)
+	if !strings.Contains(logs, "hello world") {
+		t.Errorf("tc.fetchLogs(ctx, build %q) logs as follows, did not contain 'hello world'\n%s", build.ID, logs)
+	}
+}
+
+func TestBasicE2E_PingOnly(t *testing.T) {
+	ctx := t.Context()
+	tc := newTestCase(t, "basic_e2e.yaml")
+
+	tc.startAgent("--ping-mode=ping-only")
+	build := tc.triggerBuild()
+
+	// It should take much less time than 1 minute to successfully run the job.
+	waitCtx, canc := context.WithTimeout(ctx, 1*time.Minute)
+	defer canc()
+
+	state := tc.waitForBuild(waitCtx, build)
+	if got, want := state, "passed"; got != want {
+		t.Errorf("Build state = %q, want %q", got, want)
+	}
+
+	logs := tc.fetchLogs(ctx, build)
+	if !strings.Contains(logs, "hello world") {
+		t.Errorf("tc.fetchLogs(ctx, build %q) logs as follows, did not contain 'hello world'\n%s", build.ID, logs)
+	}
+}
+
+func TestBasicE2E_StreamOnly(t *testing.T) {
+	ctx := t.Context()
+	tc := newTestCase(t, "basic_e2e.yaml")
+
+	tc.startAgent(
+		"--ping-mode=stream-only",
+		"--endpoint=https://agent-edge.buildkite.com/v3",
+	)
+	build := tc.triggerBuild()
+
+	// It should take much less time than 1 minute to successfully run the job.
+	waitCtx, canc := context.WithTimeout(ctx, 1*time.Minute)
+	defer canc()
+
+	state := tc.waitForBuild(waitCtx, build)
 	if got, want := state, "passed"; got != want {
 		t.Errorf("Build state = %q, want %q", got, want)
 	}
