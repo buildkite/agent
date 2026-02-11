9116080e1f4b586aee47dddf44ca05f3ef402c83
Josh Deprez <jd@buildkite.com>
2026-02-11 16:39:47 +1100

    Add more comments to explain baton implementation

--- agent/baton.go
+++ agent/baton.go
@@ -10,14 +10,15 @@ type baton struct {
 	acquire map[string]chan struct{}
 }
 
-// newBaton creates a new baton for sharing among (non-zero) values of T.
+// newBaton creates a new baton for sharing among actors, each identified
+// by a non-empty string.
 func newBaton() *baton {
 	return &baton{
 		acquire: make(map[string]chan struct{}),
 	}
 }
 
-// HeldBy reports if the argument holds the baton.
+// HeldBy reports if the actor specified by the argument holds the baton.
 func (b *baton) HeldBy(by string) bool {
 	b.mu.Lock()
 	defer b.mu.Unlock()
@@ -30,11 +31,15 @@ func (b *baton) Acquire(by string) <-chan struct{} {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 
+	// If there's an existing channel for this actor, reuse it.
 	ch := b.acquire[by]
 	if ch == nil {
 		ch = make(chan struct{})
 	}
 
+	// If nothing holds the baton currently, assign it to the caller.
+	// The caller won't be receiving on the channel until after we
+	// return it, so make the channel receivable by closing it.
 	if b.holder == "" {
 		b.holder = by
 		close(ch)
@@ -42,6 +47,8 @@ func (b *baton) Acquire(by string) <-chan struct{} {
 		return ch
 	}
 
+	// Something holds the baton, so record that this actor is
+	// waiting for the baton.
 	b.acquire[by] = ch
 	return ch
 }
@@ -51,22 +58,29 @@ func (b *baton) Release(by string) {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 
+	// Only release if its the same actor, to prevent bugs due
+	// to double-releasing.
 	if b.holder != by {
 		return
 	}
 
-	// Attempt to pass the baton.
+	// Attempt to pass the baton to anything still waiting for it.
 	for a, ch := range b.acquire {
 		delete(b.acquire, a)
 		select {
 		case ch <- struct{}{}:
-			// This acquirer has acquired the baton.
+			// We were able to send a value to the channel,
+			// so this actor was still waiting to receive.
+			// Therefore this actor has acquired the baton.
 			b.holder = a
 			return
 		default:
-			// This acquirer has stopped waiting, try another.
+			// This actor has stopped waiting to receive,
+			// so try another.
 		}
 	}
-	// Nothing was waiting, nothing now holds the baton.
+
+	// Nothing was still waiting on its channel,
+	// so now nothing holds the baton.
 	b.holder = ""
 }
