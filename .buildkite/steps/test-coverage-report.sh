03796d18c41e57afcac45b28b6dda991fb1553aa
Josh Deprez <jd@buildkite.com>
2026-02-11 16:39:47 +1100

    Fix coverage reports missing many tests

--- .buildkite/steps/test-coverage-report.sh
+++ .buildkite/steps/test-coverage-report.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 echo 'Producing coverage report'
-go tool covdata textfmt -i "coverage" -o cover.out
+go tool covdata textfmt -i "$1" -o cover.out
 go tool cover -html cover.out -o cover.html
