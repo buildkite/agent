e81a234b15c9afc0b282572510a0f1d4c671c696
Josh Deprez <jd@buildkite.com>
2026-03-26 17:04:55 +1100

    chore: refactor process internals, better mutex coverage
    - Split Run into setup, start, startWithPTY, startWithoutPTY, copyPTYToStdout,
      complete, onContextCancel.
    - Use CommandContext and Command.Cancel to handle context cancellation
    - Fix missing mutex coverage of some fields

--- internal/process/process.go
+++ internal/process/process.go
@@ -142,12 +142,66 @@ func (p *Process) WaitStatus() WaitStatus {
 
 // Run the command and block until it finishes
 func (p *Process) Run(ctx context.Context) error {
+	// Setup and start the command.
+	cleanup, err := p.start(ctx)
+	if err != nil {
+		return err
+	}
+	defer cleanup()
+
+	p.logger.Info("[Process] Process is running with PID: %d", p.pid())
+
+	// Wait until the process has finished. The returned error is nil if the
+	// command runs, has no problems copying stdin, stdout, and stderr, and
+	// exits with a zero exit status.
+	waitResult := p.command.Wait()
+
+	// Log and record the result
+	return p.complete(waitResult)
+}
+
+// start sets up the command and starts it, then closes p.started. It returns a
+// cleanup function.
+func (p *Process) start(ctx context.Context) (func(), error) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	// Create the command, etc
+	if err := p.setup(ctx); err != nil {
+		return nil, err
+	}
+
+	// Run in a PTY or not?
+	start := p.startWithoutPTY
+	if p.conf.PTY {
+		start = p.startWithPTY
+	}
+
+	// Start!
+	cleanup, err := start(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := p.postStart(); err != nil {
+		p.logger.Error("[Process] postStart failed: %v", err)
+	}
+	// Signal waiting consumers in Started() by closing the started channel
+	close(p.started)
+	return cleanup, nil
+}
+
+// setup sets up the command. It should be called under p.mu.
+func (p *Process) setup(ctx context.Context) error {
 	if p.command != nil {
 		return errors.New("process is already running")
 	}
 
-	// Create a command
-	p.command = exec.Command(p.conf.Path, p.conf.Args...)
+	// Create a command. Upon context cancellation, call p.onContextCancel,
+	// which calls p.Interrupt, and if the process continues past the signal
+	// grace period, p.Terminate.
+	p.command = exec.CommandContext(ctx, p.conf.Path, p.conf.Args...)
+	p.command.Cancel = p.onContextCancel
 
 	// Setup the process to create a process group if supported
 	p.setupProcessGroup()
@@ -172,128 +226,114 @@ func (p *Process) Run(ctx context.Context) error {
 	// take precedence over the agent
 	currentEnv := os.Environ()
 	p.command.Env = append(currentEnv, p.conf.Env...)
+	return nil
+}
 
-	var waitGroup sync.WaitGroup
+// startWithPTY starts the process in a PTY, and a goroutine for copying the PTY
+// to stdout. The cleanup function waits for the copy to finish and closes the
+// PTY handle.
+func (p *Process) startWithPTY(ctx context.Context) (func(), error) {
+	p.logger.Debug("[Process] Running with a PTY")
 
-	// Toggle between running in a pty
-	if p.conf.PTY {
-		p.logger.Debug("[Process] Running with a PTY")
+	// Commands like tput expect a TERM value for a PTY
+	p.command.Env = append(p.command.Env, "TERM="+termType)
 
-		// Commands like tput expect a TERM value for a PTY
-		p.command.Env = append(p.command.Env, "TERM="+termType)
+	rawPTY := experiments.IsEnabled(ctx, experiments.PTYRaw)
 
-		rawPTY := experiments.IsEnabled(ctx, experiments.PTYRaw)
-
-		pty, err := StartPTY(p.command, rawPTY)
-		if err != nil {
-			return fmt.Errorf("error starting pty: %w", err)
-		}
-
-		afterPTYStartHook()
-
-		// Make sure to close the pty at the end.
-		defer func() { _ = pty.Close() }()
-
-		if rawPTY {
-			p.logger.Debug("[Process] Setting raw mode for PTY %s (fd:%d)", pty.Name(), pty.Fd())
-		}
-
-		// Signal waiting consumers in Started() by closing the started channel
-		close(p.started)
-
-		waitGroup.Go(func() {
-			p.logger.Debug("[Process] Starting to copy PTY to the buffer")
-
-			// Copy the pty to our writer. This will block until it EOFs or something breaks.
-			_, err = io.Copy(p.conf.Stdout, pty)
-			switch {
-			case errors.Is(err, syscall.EIO):
-				// We can safely ignore syscall.EIO errors, at least on linux
-				// because it's just the PTY telling us that it closed
-				// successfully.
-				//
-				// See: https://github.com/buildkite/agent/pull/34#issuecomment-46080419
-				//
-				// We will still log the error to aid debugging on other platforms.
-				p.logger.Debug("[Process] PTY has finished being copied to the buffer: %v", err)
-
-			case err == nil:
-				p.logger.Debug("[Process] PTY has finished being copied to the buffer")
-
-			default:
-				p.logger.Error("[Process] PTY output copy failed with error: %T: %v", err, err)
-			}
-		})
-	} else {
-		p.logger.Debug("[Process] Running without a PTY")
-
-		p.command.Stdin = p.conf.Stdin
-		p.command.Stdout = p.conf.Stdout
-		p.command.Stderr = p.conf.Stderr
-
-		if err := p.command.Start(); err != nil {
-			return fmt.Errorf("error starting command: %w", err)
-		}
-
-		if err := p.postStart(); err != nil {
-			p.logger.Error("[Process] postStart failed: %v", err)
-		}
-
-		// Signal waiting consumers in Started() by closing the started channel
-		close(p.started)
+	pty, err := StartPTY(p.command, rawPTY)
+	if err != nil {
+		return nil, fmt.Errorf("error starting pty: %w", err)
 	}
 
-	// When the context finishes, terminate the process
-	go func() {
-		if ctx == nil {
-			return
-		}
+	afterPTYStartHook()
+
+	if rawPTY {
+		p.logger.Debug("[Process] Setting raw mode for PTY %s (fd:%d)", pty.Name(), pty.Fd())
+	}
+
+	// Copy and close the PTY, if it exists.
+	copyDone := make(chan struct{})
+	go p.copyPTYToStdout(pty, copyDone)
+
+	return func() {
+		// Sometimes (in docker containers) io.Copy never seems to finish. This is a
+		// mega hack around it. If it doesn't finish after 10 seconds, just continue.
+		p.logger.Debug("[Process] Waiting for routines to finish")
 
 		select {
-		case <-p.Done(): // process exited of it's own accord
-			return
-		case <-ctx.Done():
-			p.logger.Debug("[Process] Context done, terminating. pid=%d", p.pid())
-			if err := p.Interrupt(); err != nil {
-				p.logger.Warn("[Process] Failed termination: %v", err)
-			}
-
-			select {
-			case <-p.Done(): // process exited after being interrupted
-				return
-			case <-time.After(p.conf.SignalGracePeriod):
-				p.logger.Warn("[Process] Has not terminated in time, killing. pid=%d", p.pid())
-				if err := p.Terminate(); err != nil {
-					p.logger.Error("[Process] error sending SIGKILL: %s", err)
-					return
-				}
-			}
+		case <-time.After(10 * time.Second):
+			p.logger.Debug("[Process] Timed out waiting for PTY->stdout copy")
+		case <-copyDone:
+			// it's done
 		}
-	}()
 
-	p.logger.Info("[Process] Process is running with PID: %d", p.pid())
+		pty.Close() //nolint:errcheck // Best-effort cleanup
+	}, nil
+}
 
-	// Wait until the process has finished. The returned error is nil if the
-	// command runs, has no problems copying stdin, stdout, and stderr, and
-	// exits with a zero exit status.
-	p.waitResult = p.command.Wait()
+// startWithoutPTY starts the process without using a PTY. The cleanup function
+// is a no-op.
+func (p *Process) startWithoutPTY(context.Context) (func(), error) {
+	p.logger.Debug("[Process] Running without a PTY")
 
+	p.command.Stdin = p.conf.Stdin
+	p.command.Stdout = p.conf.Stdout
+	p.command.Stderr = p.conf.Stderr
+
+	if err := p.command.Start(); err != nil {
+		return nil, fmt.Errorf("error starting command: %w", err)
+	}
+	return func() {}, nil
+}
+
+// copyPTYToStdout copies pty to p.conf.Stdout. It should be a new goroutine.
+func (p *Process) copyPTYToStdout(pty *os.File, copyDone chan<- struct{}) {
+	defer close(copyDone)
+	p.logger.Debug("[Process] Starting to copy PTY to the buffer")
+
+	// Copy the pty to our writer. This will block until it EOFs or something breaks.
+	_, err := io.Copy(p.conf.Stdout, pty)
+	switch {
+	case errors.Is(err, syscall.EIO):
+		// We can safely ignore syscall.EIO errors, at least on linux
+		// because it's just the PTY telling us that it closed
+		// successfully.
+		//
+		// See: https://github.com/buildkite/agent/pull/34#issuecomment-46080419
+		//
+		// We will still log the error to aid debugging on other platforms.
+		p.logger.Debug("[Process] PTY has finished being copied to the buffer: %v", err)
+
+	case err == nil:
+		p.logger.Debug("[Process] PTY has finished being copied to the buffer")
+
+	default:
+		p.logger.Error("[Process] PTY output copy failed with error: %T: %v", err, err)
+	}
+}
+
+// complete stores the waitResult, wait status, logs completion, and closes
+// p.done.
+func (p *Process) complete(waitResult error) error {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	p.waitResult = waitResult
 	// Signal waiting consumers in Done() by closing the done channel
 	close(p.done)
 
 	// Convert the wait result into a native WaitStatus
-	if p.waitResult != nil {
-		exitErr := new(exec.ExitError)
-		if !errors.As(p.waitResult, &exitErr) {
-			return fmt.Errorf("unexpected waitResult error type %[1]T: %[1]w", p.waitResult)
+	if waitResult != nil {
+		var exitErr *exec.ExitError
+		if !errors.As(waitResult, &exitErr) {
+			return fmt.Errorf("unexpected waitResult error type %[1]T: %[1]w", waitResult)
 		}
 
-		switch ws := exitErr.Sys().(type) {
-		case syscall.WaitStatus: // posix
-			p.status = ws
-		default: // not-posix
+		waitStatus, isWS := exitErr.Sys().(syscall.WaitStatus)
+		if !isWS {
 			return ErrNotWaitStatus
 		}
+		p.status = waitStatus
 	}
 
 	// Find the exit status or terminating signal of the script
@@ -303,16 +343,40 @@ func (p *Process) Run(ctx context.Context) error {
 	}
 	p.logger.Info("Process with PID: %d finished with Exit Status: %d, Signal: %s",
 		p.pid(), p.status.ExitStatus(), exitSignal)
+	return nil
+}
 
-	// Sometimes (in docker containers) io.Copy never seems to finish. This is a mega
-	// hack around it. If it doesn't finish after 1 second, just continue.
-	p.logger.Debug("[Process] Waiting for routines to finish")
-	err := timeoutWait(&waitGroup)
-	if err != nil {
-		p.logger.Debug("[Process] Timed out waiting for wait group: (%T: %v)", err, err)
+// onContextCancel interrupts the process, waits for the process to exit for the
+// signal grace period, then terminates the process. It is called by the
+// Command.Cancel mechanism when the context for p.command is cancelled.
+func (p *Process) onContextCancel() error {
+	p.logger.Debug("[Process] Context done, terminating. pid=%d", p.pid())
+	if err := p.Interrupt(); err != nil {
+		p.logger.Warn("[Process] Failed termination: %v", err)
 	}
 
-	return nil
+	// We could almost use Command.WaitDelay to implement the signal grace
+	// period, but we want to kill *all* child processes, not just the primary
+	// process. exec.Command seems to lack a way to override its behaviour for
+	// when WaitDelay is reached. Hence, we have a separate goroutine:
+	go func() {
+		// Wait for signal grace period or the process to be done
+		select {
+		case <-p.Done(): // process exited after being interrupted above
+			return
+		case <-time.After(p.conf.SignalGracePeriod):
+			// continue below
+		}
+
+		p.logger.Warn("[Process] Has not terminated in time, killing. pid=%d", p.pid())
+		if err := p.Terminate(); err != nil {
+			// Oh Well, At Least We Tried™
+			p.logger.Error("[Process] error sending SIGKILL: %s", err)
+		}
+	}()
+
+	// Suppress "context canceled" by returning this.
+	return os.ErrProcessDone
 }
 
 // Done returns a channel that is closed when the process finishes
@@ -335,7 +399,8 @@ func (p *Process) Started() <-chan struct{} {
 	return d
 }
 
-// Interrupt the process on platforms that support it, terminate otherwise
+// Interrupt interrupts the process on platforms that support it, and terminates
+// otherwise.
 func (p *Process) Interrupt() error {
 	if p == nil {
 		return nil
@@ -349,7 +414,9 @@ func (p *Process) Interrupt() error {
 		return nil
 	}
 
-	// interrupt the process (ctrl-c or SIGINT)
+	// interrupt the process ~~(ctrl-c or SIGINT)~~
+	// Actually, this sends SIGTERM (*nixes) or Ctrl-Break (Windows).
+	// One day we might change that to SIGINT/Ctrl-C.
 	if err := p.interruptProcessGroup(); err != nil {
 		//  No process or process group can be found corresponding to that specified by pid.
 		if errors.Is(err, syscall.ESRCH) {
@@ -381,22 +448,4 @@ func (p *Process) Terminate() error {
 	}
 
 	return p.terminateProcessGroup()
-}
-
-func timeoutWait(waitGroup *sync.WaitGroup) error {
-	// Make a chanel that we'll use as a timeout
-	ch := make(chan struct{})
-
-	// Start waiting for the routines to finish
-	go func() {
-		waitGroup.Wait()
-		close(ch)
-	}()
-
-	select {
-	case <-ch:
-		return nil
-	case <-time.After(10 * time.Second):
-		return errors.New("Timeout")
-	}
 }
