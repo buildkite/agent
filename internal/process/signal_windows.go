c2348b4a0fbc03b6a8afdccf834b3d2e05d7f7be
Josh Deprez <jd@buildkite.com>
2026-03-26 17:04:55 +1100

    chore: cleanup on aisle signal_windows.go

--- internal/process/signal_windows.go
+++ internal/process/signal_windows.go
@@ -18,6 +18,32 @@ import (
 
 // See https://docs.microsoft.com/en-us/windows/console/generateconsolectrlevent
 
+// What's the deal with CTRL_C_EVENT ?
+//
+// Unlike CTRL_BREAK_EVENT, it can only be sent to the *current console window*.
+// So, naïvely, we can only use it to self-signal. There's a workaround...
+//
+// Firstly we would need to create the process with CREATE_NEW_CONSOLE, spawning
+// another black console rectangle on the screen for each process. We _could_
+// hide it immediately (GetConsoleWindow(pid), ShowWindow(hWnd, SW_HIDE)) but
+// the window would flash briefly. Probably no big issue. (Also GetConsoleWindow
+// is deprecated.)
+//
+// But then, when we want to interrupt, we have this Rube-Cronenbergian set of
+// Win32 calls:
+//
+// 1. detach from the current console (FreeConsole)
+// 2. attach to the console of this process (AttachConsole)
+// 3. add the control handler (SetConsoleCtrlHandler(NULL, TRUE))
+// 4. send the CTRL_C_EVENT
+// 5. detach from this process's console
+// 6. attach back to the original console
+// 7. remove the control handler (SetConsoleCtrlHandler(NULL, FALSE))
+//
+// If you do this in the wrong way, with the wrong amount of time between each
+// call, it doesn't work or you end up sending Ctrl-C to your own console.
+// It's a PITA.
+
 func (p *Process) setupProcessGroup() {
 	if p.conf.PTY {
 		return
@@ -43,11 +69,13 @@ func newJobObject() (uintptr, error) {
 			LimitFlags: windows.JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE,
 		},
 	}
-	if _, err := windows.SetInformationJobObject(
+	_, err = windows.SetInformationJobObject(
 		handle,
 		windows.JobObjectExtendedLimitInformation,
 		uintptr(unsafe.Pointer(&info)),
-		uint32(unsafe.Sizeof(info))); err != nil {
+		uint32(unsafe.Sizeof(info)),
+	)
+	if err != nil {
 		return 0, err
 	}
 
@@ -57,7 +85,7 @@ func newJobObject() (uintptr, error) {
 func (p *Process) postStart() error {
 	// convert the pid into a windows process handle. We need particular permissions on the handle
 	// for AssignProcessToJobObject to accept it
-	pid := uint32(p.command.Process.Pid)
+	pid := uint32(p.pid())
 	processPerms := uint32(windows.PROCESS_QUERY_LIMITED_INFORMATION | windows.PROCESS_SET_QUOTA | windows.PROCESS_TERMINATE)
 	processHandle, err := windows.OpenProcess(processPerms, false, pid)
 	if err != nil {
@@ -65,11 +93,7 @@ func (p *Process) postStart() error {
 	}
 	defer windows.CloseHandle(processHandle)
 
-	err = windows.AssignProcessToJobObject(windows.Handle(p.winJobHandle), processHandle)
-	if err != nil {
-		return err
-	}
-	return nil
+	return windows.AssignProcessToJobObject(windows.Handle(p.winJobHandle), processHandle)
 }
 
 func (p *Process) terminateProcessGroup() error {
@@ -78,21 +102,15 @@ func (p *Process) terminateProcessGroup() error {
 }
 
 func (p *Process) interruptProcessGroup() error {
-	// Sends a CTRL-BREAK signal to the process group id, which is the same as the process PID
-	// For some reason I cannot fathom, this returns "Incorrect function" in docker for windows
-	err := windows.GenerateConsoleCtrlEvent(windows.CTRL_BREAK_EVENT, uint32(p.pid()))
-	if err != nil {
-		return err
-	}
-	return nil
+	// Send Ctrl-Break to the process group id, which is the same as the process PID
+	// This may return "Incorrect function" in docker for windows
+	return windows.GenerateConsoleCtrlEvent(windows.CTRL_BREAK_EVENT, uint32(p.pid()))
 }
 
 func GetPgid(pid int) (int, error) {
-	return 0, errors.New("Not implemented on Windows")
+	return 0, errors.New("not implemented on Windows")
 }
 
 // SignalString returns the name of the given signal.
 // For example, SignalString(syscall.Signal(15)) // "terminated"
-func SignalString(s syscall.Signal) string {
-	return fmt.Sprintf("%v", s)
-}
+func SignalString(s syscall.Signal) string { return fmt.Sprint(s) }
