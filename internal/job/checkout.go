65f98e02fa12dee6f966f8bd2b86f01837f6771f
Josh Deprez <jd@buildkite.com>
2026-04-08 10:47:05 +1000

    Promote or delete experiments

--- internal/job/checkout.go
+++ internal/job/checkout.go
@@ -11,7 +11,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/buildkite/agent/v4/internal/experiments"
 	"github.com/buildkite/agent/v4/internal/osutil"
 	"github.com/buildkite/agent/v4/internal/self"
 	"github.com/buildkite/agent/v4/internal/shell"
@@ -1057,10 +1056,7 @@ func (e *Executor) defaultCheckoutPhase(ctx context.Context) (retErr error) {
 	}
 
 	// resolve BUILDKITE_COMMIT based on the local git repo
-	if experiments.IsEnabled(ctx, experiments.ResolveCommitAfterCheckout) {
-		e.shell.Commentf("Using resolve-commit-after-checkout experiment 🧪")
-		e.resolveCommit(ctx)
-	}
+	e.resolveCommit(ctx)
 
 	return nil
 }
