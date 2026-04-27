01d44b8b41fa67d9f159d5f9f402313c437a5780
Sneha <sneha@buildkite.com>
2026-04-27 16:38:57 +1000

    test: add test case that contains a new line and single quotes in the value

--- clicommand/secret_get_test.go
+++ clicommand/secret_get_test.go
@@ -10,7 +10,7 @@ import (
 	"testing"
 
 	"github.com/buildkite/agent/v3/logger"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func newSecretGetTestServer(t *testing.T, secrets map[string]string) *httptest.Server {
@@ -45,7 +45,7 @@ func TestSecretGet(t *testing.T) {
 		defer server.Close()
 		var out bytes.Buffer
 		err := secretGet(context.Background(), baseSecretGetConfig(server.URL, []string{}, "default"), &out, logger.NewBuffer())
-		assert.EqualError(t, err, "at least one secret key must be provided")
+		require.EqualError(t, err, "at least one secret key must be provided")
 	})
 
 	t.Run("error on invalid format", func(t *testing.T) {
@@ -54,7 +54,7 @@ func TestSecretGet(t *testing.T) {
 		defer server.Close()
 		var out bytes.Buffer
 		err := secretGet(context.Background(), baseSecretGetConfig(server.URL, []string{"deploy_key"}, "xml"), &out, logger.NewBuffer())
-		assert.EqualError(t, err, `invalid format "xml": must be one of 'default', 'json', or 'env'`)
+		require.EqualError(t, err, `invalid format "xml": must be one of 'default', 'json', or 'env'`)
 	})
 
 	t.Run("single key default format prints value", func(t *testing.T) {
@@ -63,8 +63,8 @@ func TestSecretGet(t *testing.T) {
 		defer server.Close()
 		var out bytes.Buffer
 		err := secretGet(context.Background(), baseSecretGetConfig(server.URL, []string{"deploy_key"}, "default"), &out, logger.NewBuffer())
-		assert.NoError(t, err)
-		assert.Equal(t, "shhsecret\n", out.String())
+		require.NoError(t, err)
+		require.Equal(t, "shhsecret\n", out.String())
 	})
 
 	t.Run("multiple keys default format outputs json", func(t *testing.T) {
@@ -76,12 +76,12 @@ func TestSecretGet(t *testing.T) {
 		defer server.Close()
 		var out bytes.Buffer
 		err := secretGet(context.Background(), baseSecretGetConfig(server.URL, []string{"deploy_key", "github_api_key"}, "default"), &out, logger.NewBuffer())
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		var result map[string]string
-		// unmarshalling and asserting the result to ensure the output is valid JSON and has the expected keys and values
-		assert.NoError(t, json.Unmarshal(out.Bytes(), &result))
-		assert.Equal(t, map[string]string{
+		// unmarshalling and requiring the result to ensure the output is valid JSON and has the expected keys and values
+		require.NoError(t, json.Unmarshal(out.Bytes(), &result))
+		require.Equal(t, map[string]string{
 			"deploy_key":     "secret1",
 			"github_api_key": "secret2",
 		}, result)
@@ -93,11 +93,11 @@ func TestSecretGet(t *testing.T) {
 		defer server.Close()
 		var out bytes.Buffer
 		err := secretGet(context.Background(), baseSecretGetConfig(server.URL, []string{"deploy_key"}, "json"), &out, logger.NewBuffer())
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		var result map[string]string
-		assert.NoError(t, json.Unmarshal(out.Bytes(), &result))
-		assert.Equal(t, map[string]string{"deploy_key": "supersecret"}, result)
+		require.NoError(t, json.Unmarshal(out.Bytes(), &result))
+		require.Equal(t, map[string]string{"deploy_key": "supersecret"}, result)
 	})
 
 	t.Run("env format - sorts keys, uppercases and shellQuotes", func(t *testing.T) {
@@ -110,8 +110,22 @@ func TestSecretGet(t *testing.T) {
 
 		var out bytes.Buffer
 		err := secretGet(context.Background(), baseSecretGetConfig(server.URL, []string{"deploy_key", "github_api_key"}, "env"), &out, logger.NewBuffer())
-		assert.NoError(t, err)
-		assert.Equal(t, "DEPLOY_KEY='secret1'\nGITHUB_API_KEY='secret2'\n", out.String())
+		require.NoError(t, err)
+		require.Equal(t, "DEPLOY_KEY='secret1'\nGITHUB_API_KEY='secret2'\n", out.String())
+	})
+
+	t.Run("env format - secret contains newline and single quote, returns valid shell syntax", func(t *testing.T) {
+		t.Parallel()
+		server := newSecretGetTestServer(t, map[string]string{
+			"deploy_key":     "'sec\\nret1'",
+			"github_api_key": "se'c'ret2",
+		})
+		defer server.Close()
+
+		var out bytes.Buffer
+		err := secretGet(context.Background(), baseSecretGetConfig(server.URL, []string{"deploy_key", "github_api_key"}, "env"), &out, logger.NewBuffer())
+		require.NoError(t, err)
+		require.Equal(t, "DEPLOY_KEY=''\\''sec\\nret1'\\'''\nGITHUB_API_KEY='se'\\''c'\\''ret2'\n", out.String())
 	})
 
 	t.Run("api error returns error with details", func(t *testing.T) {
@@ -123,8 +137,8 @@ func TestSecretGet(t *testing.T) {
 
 		var out bytes.Buffer
 		err := secretGet(context.Background(), baseSecretGetConfig(server.URL, []string{"missing_key"}, "default"), &out, logger.NewBuffer())
-		assert.Error(t, err)
-		assert.Contains(t, err.Error(), "Failed to fetch some secrets:")
-		assert.Contains(t, err.Error(), "missing_key")
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "Failed to fetch some secrets:")
+		require.Contains(t, err.Error(), "missing_key")
 	})
 }
