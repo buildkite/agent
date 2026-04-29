56613c0cb93db04f4026df7de8ca3b5aa3fcfb8d
Ben Moskovitz <ben.m@buildkite.com>
2026-04-29 14:40:28 +1000

    Rip out opentracing tracing backend

--- agent/agent_configuration.go
+++ agent/agent_configuration.go
@@ -75,7 +75,6 @@ type AgentConfiguration struct {
 	TracingBackend               string
 	TracingServiceName           string
 	TracingPropagateTraceparent  bool
-	TraceContextEncoding         string
 	DisableWarningsFor           []string
 	AllowMultipartArtifactUpload bool
 	ArtifactUploadConcurrency    int
--- clicommand/agent_start.go
+++ clicommand/agent_start.go
@@ -205,7 +205,6 @@ type AgentStartConfig struct {
 	StrictSingleHooks               bool          `cli:"strict-single-hooks"`
 	KubernetesExec                  bool          `cli:"kubernetes-exec"`
 	KubernetesContainerStartTimeout time.Duration `cli:"kubernetes-container-start-timeout"`
-	TraceContextEncoding            string        `cli:"trace-context-encoding"`
 	NoMultipartArtifactUpload       bool          `cli:"no-multipart-artifact-upload"`
 	ArtifactUploadConcurrency       int           `cli:"artifact-upload-concurrency"`
 
@@ -239,10 +238,6 @@ func (asc AgentStartConfig) Features(ctx context.Context) []string {
 
 	if asc.AcquireJob != "" {
 		features = append(features, "acquire-job")
-	}
-
-	if asc.TracingBackend == tracetools.BackendDatadog {
-		features = append(features, "datadog-tracing")
 	}
 
 	if asc.TracingBackend == tracetools.BackendOpenTelemetry {
@@ -682,7 +677,7 @@ var AgentStartCommand = cli.Command{
 		cancelCleanupTimeoutFlag,
 		cli.StringFlag{
 			Name:   "tracing-backend",
-			Usage:  `Enable tracing for build jobs by specifying a backend, "datadog" or "opentelemetry"`,
+			Usage:  `Enable tracing for build jobs by specifying a backend. Currently only "opentelemetry" (or empty) is supported`,
 			EnvVar: "BUILDKITE_TRACING_BACKEND",
 			Value:  "",
 		},
@@ -772,7 +767,6 @@ var AgentStartCommand = cli.Command{
 		// Other shared flags
 		RedactedVars,
 		StrictSingleHooksFlag,
-		TraceContextEncodingFlag,
 		NoMultipartArtifactUploadFlag,
 		AgentArtifactUploadConcurrencyFlag,
 	),
@@ -906,10 +900,6 @@ var AgentStartCommand = cli.Command{
 			if err != nil {
 				return fmt.Errorf("failed to parse gcp labels timeout: %w", err)
 			}
-		}
-
-		if _, err := tracetools.ParseEncoding(cfg.TraceContextEncoding); err != nil {
-			return fmt.Errorf("while parsing trace context encoding: %v", err)
 		}
 
 		mc := metrics.NewCollector(l, metrics.CollectorConfig{
@@ -1060,7 +1050,6 @@ var AgentStartCommand = cli.Command{
 			TracingBackend:                  cfg.TracingBackend,
 			TracingServiceName:              cfg.TracingServiceName,
 			TracingPropagateTraceparent:     cfg.TracingPropagateTraceparent,
-			TraceContextEncoding:            cfg.TraceContextEncoding,
 			AllowMultipartArtifactUpload:    !cfg.NoMultipartArtifactUpload,
 			ArtifactUploadConcurrency:       cfg.ArtifactUploadConcurrency,
 			KubernetesExec:                  cfg.KubernetesExec,
