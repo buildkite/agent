2fb5fcfff91ec9982a5862eefb998756388dd78c
Ben Moskovitz <ben.m@buildkite.com>
2026-05-01 15:52:37 +1000

    Always enable traceparent propagation from the backend if otel tracing is enabled

--- agent/agent_configuration.go
+++ agent/agent_configuration.go
@@ -74,8 +74,7 @@ type AgentConfiguration struct {
 	RedactedVars                 []string
 	AcquireJob                   string
 	OpenTelemetryTracing         bool
-	TracingServiceName           string
-	TracingPropagateTraceparent  bool
+	TelemetryServiceName         string
 	DisableWarningsFor           []string
 	AllowMultipartArtifactUpload bool
 	ArtifactUploadConcurrency    int
