b2fdb589e27c59feefd2e7fe627591375046ffc2
Josh Deprez <jd@buildkite.com>
2026-05-20 13:54:57 +1000

    Release oldstable from v3, edge from v4

--- .buildkite/steps/github-release.sh
+++ .buildkite/steps/github-release.sh
@@ -10,10 +10,16 @@ dry_run() {
 }
 
 echo '--- Getting credentials from SSM'
-GH_TOKEN=$(aws ssm get-parameter --name /pipelines/agent/GITHUB_RELEASE_ACCESS_TOKEN --with-decryption --output text --query Parameter.Value --region us-east-1)
+GH_TOKEN="$(aws ssm get-parameter \
+  --name /pipelines/agent/GITHUB_RELEASE_ACCESS_TOKEN \
+  --with-decryption \
+  --output text \
+  --query Parameter.Value \
+  --region us-east-1 \
+)"
 export GH_TOKEN
 
-if [[ "$GH_TOKEN" == "" ]]; then
+if [[ "${GH_TOKEN}" == "" ]]; then
   echo "Error: Missing \$GH_TOKEN"
   exit 1
 fi
@@ -27,38 +33,38 @@ gh --version
 
 echo '--- Getting agent version from build meta data'
 
-export FULL_AGENT_VERSION=$(buildkite-agent meta-data get "agent-version-full")
-export AGENT_VERSION=$(buildkite-agent meta-data get "agent-version")
-export BUILD_VERSION=$(buildkite-agent meta-data get "agent-version-build")
-export IS_PRERELEASE=$(buildkite-agent meta-data get "agent-is-prerelease")
+export FULL_AGENT_VERSION="$(buildkite-agent meta-data get "agent-version-full")"
+export AGENT_VERSION="$(buildkite-agent meta-data get "agent-version")"
+export BUILD_VERSION="$(buildkite-agent meta-data get "agent-version-build")"
+export IS_PRERELEASE="$(buildkite-agent meta-data get "agent-is-prerelease")"
 
-echo "Full agent version: $FULL_AGENT_VERSION"
-echo "Agent version: $AGENT_VERSION"
-echo "Build version: $BUILD_VERSION"
-echo "Is prerelease?: $IS_PRERELEASE"
+echo "Full agent version: ${FULL_AGENT_VERSION}"
+echo "Agent version: ${AGENT_VERSION}"
+echo "Build version: ${BUILD_VERSION}"
+echo "Is prerelease?: ${IS_PRERELEASE}"
 
-if [[ "$CODENAME" == "unstable" && "$IS_PRERELEASE" == "0" ]] ; then
-  echo "Skipping github release, will happen in stable pipeline"
+if [[ "${CODENAME}" == "unstable" && "${IS_PRERELEASE}" == "0" ]] ; then
+  echo "Skipping github release, will happen in stable/oldstable pipeline"
   exit 0
 fi
 
-if [[ "$CODENAME" == "stable" && "$IS_PRERELEASE" == "1" ]] ; then
+if [[ ("${CODENAME}" == "stable" || "${CODENAME}" == "oldstable") && "${IS_PRERELEASE}" == "1" ]] ; then
   echo "Skipping github release, should have happened in unstable pipeline"
   exit 0
 fi
 
 echo '--- Downloading releases'
 
-artifacts_build=$(buildkite-agent meta-data get "agent-artifacts-build")
+artifacts_build="$(buildkite-agent meta-data get "agent-artifacts-build")"
 
 rm -rf releases
 mkdir -p releases
-buildkite-agent artifact download --build "$artifacts_build" "releases/*" .
+buildkite-agent artifact download --build "${artifacts_build}" "releases/*" .
 
-echo "Version is $FULL_AGENT_VERSION"
+echo "Version is ${FULL_AGENT_VERSION}"
 
 release_args=(
-  "v$AGENT_VERSION"
+  "v${AGENT_VERSION}"
   releases/*
   --repo buildkite/agent
   --target "$(git rev-parse HEAD)"
@@ -66,18 +72,26 @@ release_args=(
   --fail-on-no-commits
 )
 
-if [[ "$IS_PRERELEASE" == "1" ]]; then
-  echo "--- 🚀 $AGENT_VERSION (prerelease)"
+if [[ "${IS_PRERELEASE}" == "1" ]]; then
+  echo "--- 🚀 ${AGENT_VERSION} (prerelease)"
 
   buildkite-agent meta-data set github_release_type "prerelease"
-  buildkite-agent meta-data set github_release_version "$AGENT_VERSION"
 
   release_args+=(--prerelease)
-else
-  echo "--- 🚀 $AGENT_VERSION"
+
+elif [[ "${CODENAME}" == "oldstable" ]]; then
+  echo "--- 🚀 ${AGENT_VERSION} (oldstable)"
 
   buildkite-agent meta-data set github_release_type "stable"
-  buildkite-agent meta-data set github_release_version "$AGENT_VERSION"
+
+  release_args+=(--latest=false)
+
+else
+  echo "--- 🚀 ${AGENT_VERSION}"
+
+  buildkite-agent meta-data set github_release_type "stable"
 fi
+
+buildkite-agent meta-data set github_release_version "${AGENT_VERSION}"
 
 dry_run gh release create "${release_args[@]}"
